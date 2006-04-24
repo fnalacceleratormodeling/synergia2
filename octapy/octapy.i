c47cc144c9c0c9daa14c1744787883207cd4fcff
James Amundson <amundson@fnal.gov>
2006-04-24 18:21:48 +0000

    Initial adaptation to Synergia2.
    git-svn-id: svn+ssh://pcac2.fnal.gov/home3/svnuser/repo/panal1/synergia2/branches/proto1@3114 3a3b969a-5101-0410-98b0-c812aed1c8b7

---
+++ octapy/octapy.i
@@ -0,0 +1,7 @@
+%module octapy
+%{
+#include "octave_class.h"
+%}
+
+%include std_string.i
+%include "octave_class.h"
---
+++ octapy/octave_class.h
@@ -0,0 +1,26 @@
+#ifndef OCTAVE_CLASS_H__
+#define OCTAVE_CLASS_H__
+#undef HAVE_STAT
+#undef _POSIX_C_SOURCE
+#include <Python.h>
+#include <string>
+#ifdef NUMARRAY
+#include <numarray/libnumarray.h>
+#include <numarray/arrayobject.h>
+#else
+#include <Numeric/arrayobject.h>
+#endif
+class Octave
+{
+ public:
+  Octave();
+  int execute(std::string string);
+  PyObject * top_level_vars();
+  PyObject * get_value(std::string name);
+  void set_value(std::string name, PyObject *value);
+  ~Octave();
+};
+
+
+#endif
+
