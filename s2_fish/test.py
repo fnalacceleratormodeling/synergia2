d771b26729943b25bd5a3cd0f2898e0a9f4c05a3
James Amundson <amundson@fnal.gov>
2006-12-05 22:47:17 +0000

    first real tests
    git-svn-id: svn+ssh://pcac2.fnal.gov/home3/svnuser/repo/panal1/synergia2/branches/proto1@3163 3a3b969a-5101-0410-98b0-c812aed1c8b7

--- s2_fish/test.py
+++ s2_fish/test.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 
 from s2_fish import *
+import unittest
+class Test_Scalar_Field(unittest.TestCase):
+    def test_01_construct1(self):
+        sf = Scalar_Field()
 
-f = Scalar_Field()
-f.set_num_points(int3(16,16,16))
-i3 = int3(1,2,3)
-print "i3[2] =",i3.get(2)
+    def test_02_contruct2(self):
+        sf = Scalar_Field(int3(10,10,10),double3(1.0,1.0,1.0),
+                          double3(0.0,0.0,0.0))
+
+    def test_03_num_points1(self):
+        sf = Scalar_Field()
+        sf.set_num_points(int3(7,17,37))
+        num = sf.get_num_points()
+        self.assertEqual(num.get(0),7)
+        self.assertEqual(num.get(1),17)
+        self.assertEqual(num.get(2),37)
+
+if __name__ == '__main__':
+    scalar_field_suite = unittest.TestLoader().loadTestsFromTestCase(Test_Scalar_Field)
+    unittest.TextTestRunner(verbosity=2).run(scalar_field_suite)
