0e27ed54c9524a7944a82c7b8a648828d06d4033
James Amundson <amundson@fnal.gov>
2007-05-25 18:43:48 +0000

    (temporarily) using time module, not mpi_wtime
    git-svn-id: svn+ssh://pcac2.fnal.gov/home3/svnuser/repo/synergia2/branches/proto1@3233 3a3b969a-5101-0410-98b0-c812aed1c8b7

---
+++ s2_fish/mytimer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import time
+
+t0 = 0.0
+
+def mytimer(message):
+    global t0
+    if t0 = 0.0:
+        t0 = time.time()
+    t1 = time.time()
+    print "%s: %g",t1-t0
+    t0 = time.time()
