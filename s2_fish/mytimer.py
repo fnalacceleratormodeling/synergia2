1c088e8e2928e2bd1f3e921e348301fb2477e5ba
James Amundson <amundson@fnal.gov>
2007-05-25 18:45:42 +0000

    bug fix
    git-svn-id: svn+ssh://pcac2.fnal.gov/home3/svnuser/repo/synergia2/branches/proto1@3234 3a3b969a-5101-0410-98b0-c812aed1c8b7

--- s2_fish/mytimer.py
+++ s2_fish/mytimer.py
@@ -6,8 +6,8 @@ t0 = 0.0
 
 def mytimer(message):
     global t0
-    if t0 = 0.0:
+    if t0 == 0.0:
         t0 = time.time()
     t1 = time.time()
-    print "%s: %g",t1-t0
+    print "%s: %g"%(message,t1-t0)
     t0 = time.time()
