911b77833d8ee1e660f17d9f8f482264f80571c0
James Amundson <amundson@fnal.gov>
2008-01-24 22:21:22 +0000

    move CHEF information into FindCHEF.cmake. bug fix for gsl linking
    git-svn-id: svn+ssh://pcac2.fnal.gov/home3/svnuser/repo/synergia2/branches/proto1@3504 3a3b969a-5101-0410-98b0-c812aed1c8b7

---
+++ CMake/FindCHEF.cmake
@@ -0,0 +1,7 @@
+# jfa: work is left to be done here...
+
+set(CHEF_PREFIX /home2/amundson/work/synergia2-base/install)
+set(CHEF_INCLUDE_DIRS ${CHEF_PREFIX}/include)
+set(CHEF_LIBRARY_DIRS ${CHEF_PREFIX}/lib)
+
+set(CHEF_LIBS -lphysics_toolkit -lbeamline -lbmlfactory -lglib-2.0 -lbasic_toolkit -lmxyzptlk)
