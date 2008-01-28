188e4cdfd73e8bc00d2d808ce25d035808cc2aa7
James Amundson <amundson@fnal.gov>
2008-01-28 21:13:09 +0000

    remove hardcoded path
    git-svn-id: svn+ssh://pcac2.fnal.gov/home3/svnuser/repo/synergia2/branches/proto2@3515 3a3b969a-5101-0410-98b0-c812aed1c8b7

--- CMake/FindCHEF.cmake
+++ CMake/FindCHEF.cmake
@@ -1,6 +1,6 @@
 # jfa: work is left to be done here...
 
-set(CHEF_PREFIX /home2/amundson/work/synergia2-base/install)
+set(CHEF_PREFIX xxx)
 set(CHEF_INCLUDE_DIRS ${CHEF_PREFIX}/include)
 set(CHEF_LIBRARY_DIRS ${CHEF_PREFIX}/lib)
 
