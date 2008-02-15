1d7569aab03fa03153f374de16a8d7406727d331
James Amundson <amundson@fnal.gov>
2008-02-15 21:24:41 +0000

    fix hardcoded mass in currently unused segment of code
    git-svn-id: svn+ssh://pcac2.fnal.gov/home3/svnuser/repo/synergia2/branches/proto2@3570 3a3b969a-5101-0410-98b0-c812aed1c8b7

--- s2_fish/electric_field.cc
+++ s2_fish/electric_field.cc
@@ -128,10 +128,7 @@ apply_phi_kick(Real_scalar_field &phi, int axis, double tau,
     const  double c = 2.99792458e8;
     const  double pi = 4.0 * atan(1.0);
 
-    // GACK! THIS IS HARDCODED FOR PROTONS! FIXME FIXME FIXME FIXME
-    // FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME
-    // FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME
-    double mass = 0.5110034e6; // should I come from macro_bunch_store?
+    double mass = mbs.mass * 1.0e9;
     double eps0 = 1.0 / (4 * pi * c * c * 1.0e-7); // using c^2 = 1/(eps0 * mu0)
     double Brho = gamma * beta * mass / c;
     double perveance0 = mbs.total_current / (2 * pi * eps0 * Brho * gamma * gamma*\
