f7d5aca2724a55419c79ad3fdec7297c20fdf3ad
James Amundson <amundson@fnal.gov>
2008-02-22 20:47:02 +0000

    add periodic test; many debugging statements commented out but not removed
    git-svn-id: svn+ssh://pcac2.fnal.gov/home3/svnuser/repo/synergia2/branches/proto2@3583 3a3b969a-5101-0410-98b0-c812aed1c8b7

--- s2_fish/test_solver_fftw.py
+++ s2_fish/test_solver_fftw.py
@@ -25,6 +25,21 @@ def potential_uniform_sphere(Q,r0,rvec):
         retval = Q/(4*pi*r)
     return retval
 
+def potential_uniform_sphere_periodic(Q,r0,rvec,period):
+    #~ return potential_uniform_sphere(Q,r0,rvec)
+    retval = 0.0
+    num_images = 20
+    image_vec = [0.0,0.0,0.0]
+    image_vec[0] = rvec[0]
+    image_vec[1] = rvec[1]
+    #~ print "begin"
+    for image in range(-num_images,num_images+1):
+        image_vec[2] = rvec[2] - image*period
+        retval += potential_uniform_sphere(Q,r0,image_vec)
+        #~ print potential_uniform_sphere(Q,r0,image_vec)
+    #~ print "end"
+    return retval
+
 def E_field_uniform_sphere(Q,r0,rvec,axis):
     """ The electric field in the axis direction due to a uniform
     sphere of radius r0 and charge Q, centered at the origin."""
@@ -36,13 +51,31 @@ def E_field_uniform_sphere(Q,r0,rvec,axis):
         dphi_dr = -Q/(4*pi*r**2)
     return dphi_dr*rhat[axis]
 
-def compare_on_axis(axis,shape,size,offset,phi,Q,r0):
+def E_field_uniform_sphere_periodic(Q,r0,rvec,period,axis):
+    #~ return potential_uniform_sphere(Q,r0,rvec)
+    retval = 0.0
+    num_images = 10
+    image_vec = [0.0,0.0,0.0]
+    image_vec[0] = rvec[0]
+    image_vec[1] = rvec[1]
+    #~ print "begin"
+    for image in range(-num_images,num_images+1):
+        image_vec[2] = rvec[2] - image*period
+        retval += E_field_uniform_sphere(Q,r0,image_vec,axis)
+        #~ print E_field_uniform_sphere(Q,r0,image_vec,axis)
+    #~ print "end"
+    return retval
+
+def compare_on_axis(axis,shape,size,offset,phi,Q,r0,periodic=False):
     r = numarray.arrayrange(shape[axis])*size[axis]/(shape[axis]-1) \
         - 0.5*size[axis] + offset[axis]
     phi_r = numarray.zeros(shape[axis],numarray.Float)
     exact = numarray.zeros(shape[axis],numarray.Float)
     max_err = 0.0
     sum_err = 0.0
+    rfile = open("r.dat","w")
+    phifile = open("phi.dat","w")
+    exactfile = open("exact.dat","w")
     for i in range(0,shape[axis]):
         rvec = [0.0,0.0,0.0]
         ri = r[i]
@@ -50,21 +83,33 @@ def compare_on_axis(axis,shape,size,offset,phi,Q,r0):
             ri = 0.5*size[axis] + offset[axis]- 1.0e-10
         rvec[axis] = ri
         phi_r[i] = phi.get_val(rvec)
-        exact[i] = potential_uniform_sphere(Q,r0,rvec)
+        if periodic:
+            exact[i] = potential_uniform_sphere_periodic(Q,r0,rvec,size[2])
+        else:
+            exact[i] = potential_uniform_sphere(Q,r0,rvec)
         abs_err = abs(1.0 - phi_r[i]/exact[i])
         if abs_err > max_err:
             max_err = abs_err
         sum_err += abs_err
+        rfile.write("%g\n" % r[i])
+        phifile.write("%g\n" % phi_r[i])
+        exactfile.write("%g\n" % exact[i])
+    rfile.close()
+    phifile.close()
+    exactfile.close()
     mean_err = sum_err/shape[axis]
     return r,phi_r,exact,max_err,mean_err
 
-def compare_E_on_axis(axis,shape,size,offset,E,Q,r0,E_axis):
+def compare_E_on_axis(axis,shape,size,offset,E,Q,r0,E_axis,periodic=False):
     r = numarray.arrayrange(shape[axis])*size[axis]/(shape[axis]-1) \
         - 0.5*size[axis] + offset[axis]
     Er = numarray.zeros(shape[axis],numarray.Float)
     exact = numarray.zeros(shape[axis],numarray.Float)
     max_err = 0.0
     sum_err = 0.0
+    rfile = open("er.dat","w")
+    efile = open("e.dat","w")
+    exactfile = open("eexact.dat","w")
     for i in range(0,shape[axis]):
         rvec = [0.0,0.0,0.0]
         ri = r[i]
@@ -72,7 +117,10 @@ def compare_E_on_axis(axis,shape,size,offset,E,Q,r0,E_axis):
             ri = 0.5*size[axis] + offset[axis]- 1.0e-10
         rvec[axis] = ri
         Er[i] = E.get_val(rvec)
-        exact[i] = E_field_uniform_sphere(Q,r0,rvec,E_axis)
+        if periodic:
+            exact[i] = E_field_uniform_sphere_periodic(Q,r0,rvec,size[2],E_axis)
+        else:
+            exact[i] = E_field_uniform_sphere(Q,r0,rvec,E_axis)
         if (exact[i] == 0.0):
             abs_err = abs(Er[i] - exact[i])
         else:
@@ -80,6 +128,12 @@ def compare_E_on_axis(axis,shape,size,offset,E,Q,r0,E_axis):
         if abs_err > max_err:
             max_err = abs_err
         sum_err += abs_err
+        rfile.write("%g\n" % r[i])
+        efile.write("%g\n" % Er[i])
+        exactfile.write("%g\n" % exact[i])
+    rfile.close()
+    efile.close()
+    exactfile.close()
     mean_err = sum_err/shape[axis]
     return r,Er,exact,max_err,mean_err
 
@@ -187,8 +241,27 @@ class Test_solver_fftw_open(unittest.TestCase):
             apply_E_n_kick(E,E_axis,1.0,mb.get_store())
 
 class Test_solver_fftw_open_periodic(unittest.TestCase):    
-    def test_01_rough_grid(self):
+    #~ def test_01_rough_grid(self):
+        #~ shape = (16,16,16)
+        #~ size = (2.0,2.0,2.0)
+        #~ offset = (0.0,0.0,0.0)
+        #~ sf = Real_scalar_field(shape,size,offset)
+        #~ mb = Macro_bunch(physics_constants.PH_NORM_mp,1)
+        #~ Q = 100000
+        #~ r0 = 0.2
+        #~ mb.init_sphere(Q,r0)
+        #~ total_charge = deposit_charge_cic(sf,mb.get_store(),1)
+        #~ fftwh = Fftw_helper(shape)
+        #~ phi = solver_fftw_open(sf,fftwh,1)
+        #~ for axis in range(0,3):
+            #~ r,phi_r,exact,max_err,mean_err = compare_on_axis(axis,shape,size,
+                                                             #~ offset,phi,Q,r0,periodic=True)
+            #~ self.failIf(max_err>0.07)
+            #~ self.failIf(mean_err>0.02)
+
+    def test_02_rough_grid_E(self):
         shape = (16,16,16)
+        shape = (64,64,64)
         size = (2.0,2.0,2.0)
         offset = (0.0,0.0,0.0)
         sf = Real_scalar_field(shape,size,offset)
@@ -200,15 +273,21 @@ class Test_solver_fftw_open_periodic(unittest.TestCase):
         fftwh = Fftw_helper(shape)
         phi = solver_fftw_open(sf,fftwh,1)
         for axis in range(0,3):
-            r,phi_r,exact,max_err,mean_err = compare_on_axis(axis,shape,size,
-                                                             offset,phi,Q,r0)
-            self.failIf(max_err>0.07)
-            self.failIf(mean_err>0.02)
+            E = calculate_E_n(phi,axis)
+            r,E_r,exact,max_err,mean_err = compare_E_on_axis(axis,shape,size,
+                                                             offset,E,Q,r0,periodic=True,E_axis=axis)
+            print "axis:",axis,"max_err:",max_err,"mean_err:",mean_err
+            #~ self.failIf(max_err>0.07)
+            #~ self.failIf(mean_err>0.02)
 
 if __name__ == '__main__':
     unsuccessful = 0
     solver_suite = unittest.TestLoader().loadTestsFromTestCase(Test_solver_fftw_open)
     retval = unittest.TextTestRunner(verbosity=2).run(solver_suite)
+    if not retval.wasSuccessful():
+        unsuccessful = 1
+    solver_suite_periodic = unittest.TestLoader().loadTestsFromTestCase(Test_solver_fftw_open_periodic)
+    retval = unittest.TextTestRunner(verbosity=2).run(solver_suite_periodic)
     if not retval.wasSuccessful():
         unsuccessful = 1
 
