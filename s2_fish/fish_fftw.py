00f4e2f6ade995817e5412e3918dee31b174d31a
James Amundson <amundson@fnal.gov>
2008-02-22 20:47:20 +0000

    periodicity bug fixed; many debugging statements included
    git-svn-id: svn+ssh://pcac2.fnal.gov/home3/svnuser/repo/synergia2/branches/proto2@3586 3a3b969a-5101-0410-98b0-c812aed1c8b7

--- s2_fish/fish_fftw.py
+++ s2_fish/fish_fftw.py
@@ -15,11 +15,24 @@ import sys
 from mpi4py import MPI
 import Numeric
 import MLab
+import math
 
 fftwhs = {}
+counter = 0   
+
+def get_longitudinal_period_size(mbunch):
+    mbs = mbunch.get_store()
+    ref_particle = mbs.get_ref_particle()
+    units = mbs.get_units()
+    gamma = -ref_particle[5]
+    beta = math.sqrt(1.0-1.0/gamma**2)
+    size = 2.0*math.pi*gamma*beta/units[0]
+    return size
 
 def apply_space_charge_kick(shape,size,offset,mbunch,tau,
         periodic=True,aperture=None):
+    global counter
+    counter += 1
     show_timings=1
     mytimer("misc asck1")
     key = str(shape)
@@ -32,19 +45,25 @@ def apply_space_charge_kick(shape,size,offset,mbunch,tau,
         constraints.apply_longitudinal_periodicity(mbunch.get_store())
         mytimer("apply periodicity")
     mbunch.convert_to_fixedt()
+    mbunch.write_particles("bunch_fixedt_%02d.dat" % counter)
     mytimer("convert")
     if (size == None) or (offset == None):
         means, stds = synergia.get_spatial_means_stds(mbunch)
         if size == None:
             n_sigma = 8.0
-            size = tuple(n_sigma*stds)
+            size = list(n_sigma*stds)
         if offset == None:
-            offset = tuple(means)
+            offset = list(means)
+    if periodic:
+        size[2] = get_longitudinal_period_size(mbunch)
+        offset[2] = 0
     mytimer("diagnostics")
     rho = Real_scalar_field(shape,size,offset)
-    total_charge = deposit_charge_cic(rho,mbunch.get_store(),0)
+    total_charge = deposit_charge_cic(rho,mbunch.get_store(),periodic)
+    rho.write_to_file("rho_%02d.dat"%counter)
     mytimer("deposit")
     phi = solver_fft_open(rho,fftwhs[key],periodic)
+    phi.write_to_file("phi_%02d.dat"%counter)
     mytimer("solve")
     full_kick(phi,tau,mbunch.get_store())
     mytimer("full kick")
