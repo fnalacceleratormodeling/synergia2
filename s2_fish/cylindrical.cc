b55cacdcfc1c217adfd49918b6fcb596ae286d08
James Amundson <amundson@fnal.gov>
2008-03-13 16:12:18 +0000

    start cylindrical solver
    git-svn-id: svn+ssh://pcac2.fnal.gov/home3/svnuser/repo/synergia2/branches/proto2@3608 3a3b969a-5101-0410-98b0-c812aed1c8b7

---
+++ s2_fish/cylindrical.cc
@@ -0,0 +1,25 @@
+#include "cylindrical.h"
+#include "math_constants.h"
+
+void 
+get_cylindrical_coords(Macro_bunch_store &mbs, Array_2d<double> &coords)
+{
+    for (int n = 0; n < mbs.local_num; ++n) {
+        double x = mbs.local_particles(0,n);
+        double y = mbs.local_particles(2,n);        
+        double r = sqrt(x*x + y*y);
+        double theta;
+        if (x>0.0) {
+            if(y>0.0) {
+                theta = asin(y/r);
+            } else {
+                theta = 2*pi + asin(y/r);
+            }
+        } else {
+            theta = pi - asin(y/r);
+        }
+        coords(0,n) = r;
+        coords(1,n) = theta;
+        coords(2,n) = mbs.local_particles(4,n); // z
+    }
+}
---
+++ s2_fish/cylindrical.h
@@ -0,0 +1,9 @@
+#ifndef HAVE_CYLINDRICAL_H
+#define HAVE_CYLINDRICAL_H
+
+#include "macro_bunch_store.h"
+#include "array_nd/array_2d.h"
+
+void get_cylindrical_coords(Macro_bunch_store &mbs, Array_2d<double> &coords);
+
+#endif // HAVE_CYLINDRICAL_H
