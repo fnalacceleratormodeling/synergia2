5baf3fe4e165a0365b5d4c7abb3aa40395e9a1de
Paul Lebrun <lebrun@fnal.gov>
2008-07-01 18:37:23 +0000

    Testing potoential 3D sym
    git-svn-id: svn+ssh://pcac2.fnal.gov/home3/svnuser/repo/synergia2/branches/proto2@3747 3a3b969a-5101-0410-98b0-c812aed1c8b7

--- examples/ECloud/StudyPotential.py
+++ examples/ECloud/StudyPotential.py
@@ -25,14 +25,14 @@ from mpi4py import MPI
  
 if ( __name__ == '__main__'):
 
-    kinetic_energy = 8.0  # Injection energy..Irrelevant, it seems..  
+    kinetic_energy = 1.0  # Injection energy..Irrelevant, it seems..  
     mass = synergia.PH_NORM_mp # mass of the proton, the bunch contains proton. 
     # Set the charge of the bunch.  Unspecified units 
     charge = 2.0  # unspecified units 
-    current = 10.0 # unspecified units, but does NOT change the potential value. 
+    current = 5.0 # unspecified units, but does NOT change the potential value. 
     initial_phase = 0.0
 #    scaling_frequency = 10221.05558e6 # Angular frequency 
-    scaleDef = 2.0 # Arbitrary Ratio of beam width to Synergia length or "Angular Frequency" 
+    scaleDef = 2.5 # Arbitrary Ratio of beam width to Synergia length or "Angular Frequency" 
     scaling_frequency = scaleDef*synergia.PH_MKS_c/(2.0*math.pi) # Angular frequency
     part_per_cell = 2 # Result varies little changing this...  as we many cells.. 
     kicks_per_line = 10 # Number of iteration on space charge field kick (irrelevant ? )  
@@ -44,7 +44,7 @@ if ( __name__ == '__main__'):
     print "num_particles =",num_particles
     print "We will use a", solver, "solver"
     
-    xwidth=0.0020/scaleDef   # I hope my beam width is 3.2 mm  
+    xwidth=0.0050/scaleDef   # I hope my beam width is 3.2 mm  
     ywidth=xwidth   # same 
     xpwidth=0.0049608  # Irrelevant, we won't tranport the beam 
     ypwidth=0.00768  # same... 
@@ -52,8 +52,6 @@ if ( __name__ == '__main__'):
     yOffset = 0.0
     rx=0.0 # No offset.   
     dpop = 1.0e-20 # irrelevant as well 
-#    bunchLength = xwidth # 3D spherical bunch !... in meters 
-    bunchLength = 0.5 # realistic bunch length now..  
 #
 # Now define the problem in Synergia.  The channel.mad file is from
 #  synergia2/examples/channel.  The detail of this beam line are irrelevant, 
@@ -70,7 +68,7 @@ if ( __name__ == '__main__'):
     
     pz = beam_parameters.get_gamma() * beam_parameters.get_beta() * beam_parameters.mass_GeV
     beam_parameters.x_params(sigma = xwidth, lam = xpwidth * pz,r = -rx)
-    beam_parameters.y_params(sigma = ywidth, lam = ypwidth * pz,r = rx)
+    beam_parameters.y_params(sigma = xwidth, lam = xpwidth * pz,r = rx)
     sigma_z_meters = beam_parameters.get_beta()*synergia.PH_MKS_c/scaling_frequency/math.pi
     # No, set the bunch length to
     sigma_z_meters = xwidth # Make sure we have a 3D isotropic charge. 
@@ -83,10 +81,8 @@ if ( __name__ == '__main__'):
     means,sigs = synergia.get_spatial_means_stds(bunch)
     sigXNow = sigs[0]
     sigYNow = sigs[1]
-    bunchLength = sigs[2] 
     print "Beam Gamma", beam_parameters.get_gamma()
-    print " Bunch sigmas, before propagation, x =  ", \
-          sigXNow, " y = ", sigYNow, " z ", bunchLength
+    print " Bunch sigmas, before propagation, x =  ", sigXNow, 
     print " Are these in meters, at this stage ???"  
     
     unitsBunch = bunch.get_store().get_units()
@@ -104,7 +100,8 @@ if ( __name__ == '__main__'):
 # set the offset.. ?? Keep it centered     
     rho = Real_scalar_field(griddim,sizeNow,(0.0,0.0,0.0))
 # drop the bunch charge onto the grid..    
-    total_charge = deposit_charge_cic(rho,bunch.get_store(),0)
+#    total_charge = deposit_charge_cic(rho,bunch.get_store(),0)
+    total_charge = deposit_charge_ngp(rho,bunch.get_store())
     print " Total Charge ", total_charge # which units? 
     print " Total Current ", bunch.total_current # Which units?
     print " mass  ", bunch.mass # This is in GeV 
@@ -118,7 +115,8 @@ if ( __name__ == '__main__'):
       rr = iSig*sigXNow # at given radius rr .. In what units? 
       loc = numpy.array([rr, rr, rr],'d')
       vv = phi.get_val(loc)/total_charge # Normalize to the total charge (units?) 
+      vv *= 8.98755e9*4.0*math.pi
 #      vvPrime = phi.get_deriv(loc,0)/total_charge # Not available in Python..  
-      print " V per Impact Charge ", rr , " = ", vv, " V*r/Q " , vv*rr 
+      print " V for 1 cb  ", rr , " = ", vv*1.0e-9, " V*r " , vv*rr*1.0e-9
       iSig = iSig+0.5
     
