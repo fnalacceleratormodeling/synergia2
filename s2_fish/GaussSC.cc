df44a6b950095ab4948305de678abedf4a1d3b21
Alexandru Macridin <macridin@fnal.gov>
2009-03-18 13:04:42 -0500

    clean up

--- s2_fish/GaussSC.cc
+++ s2_fish/GaussSC.cc
@@ -11,28 +11,30 @@ apply_BasErs_kick(Macro_bunch_store &mbs, double sigmaX, double sigmaY, double t
 
 {
 
-    const double epsilon_0 = PH_MKS_eps0;
+    double gamma = -1 * mbs.ref_particle(5);
+    double beta = sqrt(gamma * gamma - 1.0) / gamma;
+    const  double c = PH_MKS_c;
+    const  double pi = M_PI;
+    const double eps0 = PH_MKS_eps0;
 
     double sigma[2];
     sigma[0] = sigmaX;
     sigma[1] = sigmaY;
     BasErs_field *myfield = new BasErs_field(sigma);
 
+ 
     for (int ipart = 0; ipart < mbs.local_num; ipart++) {
         // Get the field at the particle (x,y) location
         std::vector<double> Efield(3);
-
         Efield = myfield->NormalizedEField(mbs.local_particles(0, ipart), mbs.local_particles(2, ipart));
-
         //std::cout << " Ex = " << Efield[0] << " Ey = "<< Efield[1] << " Ez = " << Efield[2] << std::endl;
 
         // Now x and y kick
+
+ 	
+
         for (int n_axis = 0; n_axis < 2; n_axis++) {
-            double gamma = -1 * mbs.ref_particle(5);
-            double beta = sqrt(gamma * gamma - 1.0) / gamma;
-            const  double c = PH_MKS_c;
-            const  double pi = M_PI;
-            double eps0 = PH_MKS_eps0;
+           
 //**************** old attempt **********************************
 /*          double mass = mbs.mass * 1.0e9;          
             double Brho = gamma * beta * mass/ c;
@@ -41,7 +43,6 @@ apply_BasErs_kick(Macro_bunch_store &mbs, double sigmaX, double sigmaY, double t
                                 beta * c * beta * c);
             double LengthScale = mbs.units(2 * n_axis);
             double factor = pi * perveance0 * gamma * beta * beta / LengthScale * 4.0 * pi;
-            factor = perveance0* beta*gamma*mbs.mass;//0.93827231;
 
 
 
@@ -57,7 +58,7 @@ apply_BasErs_kick(Macro_bunch_store &mbs, double sigmaX, double sigmaY, double t
 //**************************************************************
 
 
-/*       A;ex: please check it, although I am almost sure it is right.....
+/*       Alex: please check it, although I am almost sure it is right.....
           In the bunch frame,  
 
                     (Delta p) = q*E* (Delta t) =factor*Efiled*tau
@@ -93,17 +94,14 @@ apply_BasErs_kick(Macro_bunch_store &mbs, double sigmaX, double sigmaY, double t
 //        1/gamma is due to the charge density transformation between the frames
 //        rho_charge (in the acc frame)=  rho_charge (in the bunch frame)*gamma
 //        remember that the current I=lambda*beta*c is defined in the acc frame 
+//   
 
-	  factor = factor/(gamma * gamma);
-	
+
+	  factor = factor/(gamma * gamma);	
 
           int index = 2 * n_axis + 1; // for n_axis = (0,1,2) Cartesian coordinate x,y,z,
             // in particle store indexing, px,py,pz = (1,3,5)
           double kick = Efield[n_axis] * tau * factor ; // the SC kick with the 2D geometric field contribution
-
-            //if (ipart < 5) {
-            //std::cout << " our kick " << kick << "the 3d kick " << kick2 << " ID " << mbs.local_particles(6,ipart) << std::endl;}
-
             // update the data structure
             mbs.local_particles(index, ipart) += kick;
            
