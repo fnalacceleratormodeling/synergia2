0c5c1f34e00a0837a6fd6f0143060763577759bd
James Amundson <amundson@fnal.gov>
2009-09-29 11:36:41 -0500

    add parallel_utils library

---
+++ utils/parallel_utils.cc
@@ -0,0 +1,60 @@
+#include "parallel_utils.h"
+#include <cmath>
+
+int
+mpi_get_rank(MPI_Comm &comm)
+{
+    int error,rank;
+    error = MPI_Comm_rank(comm,&rank);
+    if (error != MPI_SUCCESS) {
+        throw std::runtime_error("MPI error in MPI_Comm_rank");
+    }
+    return rank;
+}
+
+int
+mpi_get_size(MPI_Comm &comm)
+{
+    int error,size;
+    error = MPI_Comm_size(comm,&size);
+    if (error != MPI_SUCCESS) {
+        throw std::runtime_error("MPI error in MPI_Comm_size");
+    }
+    return size;
+}
+
+void
+decompose_1d_raw(int processors, int length, std::vector<int > &offsets,
+        std::vector<int > &counts)
+{
+    int min_counts = length / processors;
+    int remainder = fmod(length, processors);
+    int offset = 0;
+    for (int i = 0; i < processors; ++i) {
+        int count = min_counts;
+        if (i >= (processors - remainder)) {
+            count += 1;
+        }
+        offsets.at(i) = offset;
+        counts.at(i) = count;
+        offset += count;
+    }
+}
+
+void
+decompose_1d(MPI_Comm &comm, int length, std::vector<int> & offsets,
+        std::vector<int> &counts)
+{
+    int size = mpi_get_size(comm);
+    decompose_1d_raw(size,length,offsets,counts);
+}
+
+int
+decompose_1d_local(MPI_Comm &comm, int length)
+{
+    int size = mpi_get_size(comm);
+    int rank = mpi_get_rank(comm);
+    std::vector<int> offsets(size),counts(size);
+    decompose_1d_raw(size,length,offsets,counts);
+    return counts[rank];
+}
---
+++ utils/parallel_utils.h
@@ -0,0 +1,24 @@
+#ifndef PARALLEL_UTILS_H_
+#define PARALLEL_UTILS_H_
+
+#include "mpi.h"
+#include <vector>
+#include <stdexcept>
+
+int
+mpi_get_rank(MPI_Comm &comm);
+
+int
+mpi_get_size(MPI_Comm &comm);
+
+void
+decompose_1d_raw(int processors, int length, std::vector<int > &offsets,
+        std::vector<int > &counts);
+void
+decompose_1d(MPI_Comm &comm, int length, std::vector<int> & offsets,
+        std::vector<int> &counts);
+
+int
+decompose_1d_local(MPI_Comm &comm, int length);
+
+#endif /* PARALLEL_UTILS_H_ */
