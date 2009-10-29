3baac33eff57e279e7282fbc13a2ece4b2929b01
James Amundson <amundson@fnal.gov>
2009-10-29 14:37:45 -0500

    documentation for utils module

--- utils/commxx.h
+++ utils/commxx.h
@@ -3,16 +3,30 @@
 
 #include "mpi.h"
 
+/// Commxx is a thin wrapper around MPI communicator (MPI_Comm) objects.
+/// This is a C++-only class.
+///
+/// Python: The equivalent functionality is provided by mpi4py Comm objects.
+/// mpi4py Comm objects may be passed from python anywhere a Commxx object is
+/// expected.
 class Commxx
 {
 private:
     MPI_Comm comm;
 public:
+    /// Construct a Commxx object from an MPI_Comm communicator
+    /// @param comm is an MPI communicator, e.g., MPI_COMM_WORLD
     Commxx(MPI_Comm comm);
+
+    /// Get communicator rank.
     int
     get_rank() const;
+
+    /// Get communicator size
     int
     get_size() const;
+
+    /// Extract the MPI_comm object wrapped by the Commxx instance.
     MPI_Comm
     get() const;
 };
--- utils/parallel_utils.h
+++ utils/parallel_utils.h
@@ -5,14 +5,29 @@
 #include <vector>
 #include "commxx.h"
 
+/// Perform a one-dimensional decomposition. In cases where the decomposition
+/// is uneven, i.e., processors is not an integral multiple of length, the
+/// extra items are assigned to the higher-numbered processors.
+/// @param processors is the number of processors.
+/// @param length is the length of the vector to decompose.
+/// @param offsets is a vector of length processors that will be filled with
+/// the vector offsets on each processor.
+/// @param counts is a vector of length processors that will be filled with
+/// the number of counts assigned to each processor.
 void
 decompose_1d_raw(int processors, int length, std::vector<int > &offsets,
         std::vector<int > &counts);
 
+/// See decompose_1d_raw. The number of processors is extracted from the
+/// Commxx object.
 void
 decompose_1d(Commxx comm, int length, std::vector<int > &offsets,
         std::vector<int > &counts);
 
+/// See decompose_1d_raw. In this simpler version, the number of processors is
+/// extracted from the Commxx object and the number of counts assigned to the
+/// current processor is returned. The lengths on other processors and offsets are
+/// discarded.
 int
 decompose_1d_local(Commxx comm, int length);
 
