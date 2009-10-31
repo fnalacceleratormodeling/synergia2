1c80736dfb1ed8079451854c143ee100a084f143
James Amundson <amundson@fnal.gov>
2009-10-31 18:26:50 -0500

    move Fixed_t_z_converter classes to separate files

--- components/bunch/bunch.cc
+++ components/bunch/bunch.cc
@@ -3,52 +3,6 @@
 #include <stdexcept>
 #include <cmath>
 
-void
-Fixed_t_z_zeroth::fixed_t_to_fixed_z(Bunch &bunch)
-{
-    // This routine just copied/translated from older branch.
-    // Check/fix me please.
-    double gamma_ref = bunch.get_reference_particle().get_gamma();
-    double mass = bunch.get_mass();
-    MArray2d_ref particles = bunch.get_local_particles();
-    for (int part = 0; part < bunch.get_local_num(); ++part) {
-        double f_x = particles[part][Bunch::xp] / mass;
-        double f_y = particles[part][Bunch::yp] / mass;
-        double gamma = gamma_ref - particles[part][Bunch::tp] / mass;
-        double beta = sqrt(1.0 - (1 + f_x * f_x + f_y * f_y) / (gamma * gamma));
-        particles[part][Bunch::z] /= -gamma * beta;
-    }
-}
-
-void
-Fixed_t_z_zeroth::fixed_z_to_fixed_t(Bunch &bunch)
-{
-    // This routine just copied/translated from older branch.
-    // Check/fix me please.
-    double gamma_ref = bunch.get_reference_particle().get_gamma();
-    double mass = bunch.get_mass();
-    MArray2d_ref particles = bunch.get_local_particles();
-    for (int part = 0; part < bunch.get_local_num(); ++part) {
-        double f_x = particles[part][Bunch::xp] / mass;
-        double f_y = particles[part][Bunch::yp] / mass;
-        double gamma = gamma_ref - particles[part][Bunch::tp] / mass;
-        double beta = sqrt(1.0 - (1 + f_x * f_x + f_y * f_y) / (gamma * gamma));
-        particles[part][Bunch::z] *= -gamma * beta;
-    }
-}
-
-void
-Fixed_t_z_ballistic::fixed_t_to_fixed_z(Bunch &bunch)
-{
-    std::cout << "stub: ballistic fixed_t_to_fixed_z\n";
-}
-
-void
-Fixed_t_z_ballistic::fixed_z_to_fixed_t(Bunch &bunch)
-{
-    std::cout << "stub: ballistic fixed_z_to_fixed_t\n";
-}
-
 const int Bunch::x;
 const int Bunch::xp;
 const int Bunch::y;
--- components/bunch/bunch.h
+++ components/bunch/bunch.h
@@ -5,62 +5,7 @@
 #include "utils/multi_array_typedefs.h"
 #include "components/foundation/reference_particle.h"
 #include "utils/commxx.h"
-
-class Bunch;
-
-/// Fixed_t_z_converter is a virtual base class for converters
-/// between the bunch fixed-z representation and the bunch
-/// fixed-t representation
-class Fixed_t_z_converter
-{
-public:
-    /// Convert from the fixed-t state to the fixed-z state.
-    virtual void
-    fixed_t_to_fixed_z(Bunch &bunch) = 0;
-
-    /// Convert from the fixed-z state to the fixed-t state.
-    virtual void
-    fixed_z_to_fixed_t(Bunch &bunch) = 0;
-
-    virtual
-    ~Fixed_t_z_converter()
-    {
-    }
-    ;
-};
-
-/// Fixed_t_z_zeroth implements a fixed-t-fixed-z converter using
-/// the simplest approximation: longitudinal coordinates are transformed,
-/// but transverse coordinates are unaffected.
-class Fixed_t_z_zeroth : public Fixed_t_z_converter
-{
-public:
-    /// Convert from the fixed-t state to the fixed-z state.
-    void
-    fixed_t_to_fixed_z(Bunch &bunch);
-
-    /// Convert from the fixed-z state to the fixed-t state.
-    void
-    fixed_z_to_fixed_t(Bunch &bunch);
-};
-
-/// Fixed_t_z_zeroth implements a fixed-t-fixed-z converter using
-/// the ballistic approximation: longitudinal coordinates are transformed,
-/// then transverse coordinates are transformed using the ballistic
-/// approximation, i.e., as though the particles were traveling independently
-/// in free space.
-/// UNIMPLEMENTED
-class Fixed_t_z_ballistic : public Fixed_t_z_converter
-{
-public:
-    /// Convert from the fixed-t state to the fixed-z state.
-    void
-    fixed_t_to_fixed_z(Bunch &bunch);
-
-    /// Convert from the fixed-z state to the fixed-t state.
-    void
-    fixed_z_to_fixed_t(Bunch &bunch);
-};
+#include "components/bunch/fixed_t_z_converter.h"
 
 /// Bunch represents a macroparticle bunch distributed across the processors
 /// in a communicator.
