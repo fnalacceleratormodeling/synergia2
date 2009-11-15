917f79ba5544848199b5cb74ad2be079752c6d29
James Amundson <amundson@fnal.gov>
2009-11-14 22:54:52 -0600

    populate_6d passes one test

--- components/bunch/populate.cc
+++ components/bunch/populate.cc
@@ -1,24 +1,46 @@
 #include "populate.h"
 #include "diagnostics.h"
 
-#include "utils/eigen2/Eigen/Core"
-#include "utils/eigen2/Eigen/LU"
+#include "utils/eigen2/Eigen/Eigen"
+#include "utils/eigen2/Eigen/Cholesky"
 
 // import most common Eigen types
 USING_PART_OF_NAMESPACE_EIGEN
+
+#include "utils/multi_array_print.h"
 
 void
 adjust_moments(Bunch &bunch, Const_MArray1d_ref means,
         Const_MArray2d_ref covariances)
 {
-    MArray2d scratch(boost::extents[bunch.get_local_num()][6]);
-    Diagnostics_full2 diagnostics(bunch,0.0);
-//    MArray1d actual_means(diagnostics.get_mean());
-//    MArray2d actual_covs(diagnostics.get_mom2());
-    Matrix<double, 6, 6 > C(diagnostics.get_mom2().origin());
-//    Matrix<double, 6,6 > G(C.llt().compute());
+    Diagnostics_full2 diagnostics(bunch, 0.0);
+    Matrix<double, 6, 6 > C(covariances.origin());
+    Matrix<double, 6, 6 > X(diagnostics.get_mom2().origin());
+    Matrix<double, 6, 6 > A = C.llt().matrixL() * X.llt().matrixL().inverse();
 
+    // The suffix 7 is used in the following section to indicate that we
+    // are using the 7-dimensional data structure with the real 6-dimensional
+    // data plus the id (seventh dimension).
+    Matrix<double, 7, 7 > A7;
+    A7 << A, MatrixXd::Zero(6, 1), MatrixXd::Zero(1, 6), MatrixXd::Identity(1,
+            1);
+    MArray2d_ref rho(bunch.get_local_particles());
+    Const_MArray1d_ref rhobar(diagnostics.get_mean());
+    for (int part = 0; part < bunch.get_local_num(); ++part) {
+        for (int i = 0; i < 6; ++i) {
+            rho[part][i] -= rhobar[i];
+        }
+    }
 
+    Eigen::Map<Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor > >
+            rho7(bunch.get_local_particles().origin(), bunch.get_local_num(), 7);
+    rho7 *= A7.transpose();
+
+    for (int part = 0; part < bunch.get_local_num(); ++part) {
+        for (int i = 0; i < 6; ++i) {
+            rho[part][i] += means[i];
+        }
+    }
 }
 
 void
---
+++ components/bunch/tests/test_populate.cc
@@ -0,0 +1,84 @@
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+#include "components/bunch/populate.h"
+#include "components/bunch/diagnostics.h"
+#include "utils/boost_test_mpi_fixture.h"
+#include "utils/multi_array_typedefs.h"
+#include "utils/multi_array_print.h"
+BOOST_GLOBAL_FIXTURE(MPI_fixture)
+;
+
+const double tolerance = 1.0e-12;
+
+const double mass = 100.0;
+const double total_energy = 125.0;
+const int proton_charge = 1;
+const int total_num = 1000;
+const double real_num = 2.0e12;
+const double default_s = 123.4;
+
+void
+compare_multi_array(Const_MArray1d_ref const& a, Const_MArray1d_ref const& b,
+        double tolerance)
+{
+    BOOST_CHECK_EQUAL(a.shape()[0],b.shape()[0]);
+    for (int i = 0; i < a.shape()[0]; ++i) {
+        if (a[i] == 0.0) {
+            BOOST_CHECK_SMALL(b[i], tolerance);
+        } else {
+            BOOST_CHECK_CLOSE(a[i], b[i], tolerance);
+        }
+    }
+}
+
+void
+compare_multi_array(Const_MArray2d_ref const& a, Const_MArray2d_ref const& b,
+        double tolerance)
+{
+    BOOST_CHECK_EQUAL(a.shape()[0],b.shape()[0]);
+    BOOST_CHECK_EQUAL(a.shape()[1],b.shape()[1]);
+    for (int i = 0; i < a.shape()[0]; ++i) {
+        for (int j = 0; j < a.shape()[1]; ++j) {
+            if (a[i][j] == 0.0) {
+                BOOST_CHECK_SMALL(b[i][j], tolerance);
+            } else {
+                BOOST_CHECK_CLOSE(a[i][j], b[i][j], tolerance);
+            }
+        }
+    }
+}
+
+struct Fixture
+{
+    Fixture() :
+        reference_particle(mass, total_energy), comm(MPI_COMM_WORLD), bunch(
+                reference_particle, proton_charge, total_num, real_num, comm),
+                distribution(0, comm), s(default_s)
+    {
+        BOOST_TEST_MESSAGE("setup fixture");
+    }
+    ~Fixture()
+    {
+        BOOST_TEST_MESSAGE("teardown fixture");
+    }
+
+    Reference_particle reference_particle;
+    Commxx comm;
+    Bunch bunch;
+    Random_distribution distribution;
+    double s;
+};
+
+BOOST_FIXTURE_TEST_CASE(populate_6d_diagonal, Fixture)
+{
+    MArray2d covariances(boost::extents[6][6]);
+    MArray1d means(boost::extents[6]);
+    for (int i = 0; i < 6; ++i) {
+        means[i] = i * 0.1;
+        covariances[i][i] = (i + 1) * (i + 1);
+    }
+    populate_6d(distribution, bunch, means, covariances);
+    Diagnostics_full2 diagnostics(bunch, s);
+    compare_multi_array(means, diagnostics.get_mean(), tolerance);
+    compare_multi_array(covariances, diagnostics.get_mom2(), tolerance);
+}
