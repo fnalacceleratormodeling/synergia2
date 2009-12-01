7e544a60b35f8ffd791e842ad14d0e057eeac71f
James Amundson <amundson@fnal.gov>
2009-12-01 11:34:20 -0600

    wip on plot-results.py

--- examples/fodo/plot-results.py
+++ examples/fodo/plot-results.py
@@ -1,27 +1,10 @@
 #!/usr/bin/env python
 
-import tables
 from matplotlib import pyplot
+from diagnostics_file import Diagnostics_file
 
-class Diagnostics:
-    def __init__(self,filename):
-        d = tables.openFile(filename)
-#        print dir(d.root)
-        self.s = d.root.s.read()
-        self.emitx = d.root.emitx.read()
-        self.emitxy = d.root.emitxy.read()
-        self.emitxyz = d.root.emitxyz.read()
-        self.emity = d.root.emity.read()
-        self.emitz = d.root.emitz.read()
-        self.mean = d.root.mean.read()
-        self.n = d.root.n.read()
-        self.s = d.root.s.read()
-        self.std = d.root.std.read()
-        self.units = d.root.units.read()
-        d.close()
-
-last = Diagnostics("fodo.h5")
-zero_current = Diagnostics("fodo-np0.h5")
+last = Diagnostics_file("fodo.h5")
+zero_current = Diagnostics_file("fodo-np0.h5")
 pyplot.plot(last.s,last.std[:,0])
 pyplot.plot(zero_current.s,zero_current.std[:,0])
 pyplot.show()
