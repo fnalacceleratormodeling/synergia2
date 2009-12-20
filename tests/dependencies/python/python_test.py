965648d0e3ddad24519860857f1e76117c722e3d
James Amundson <amundson@fnal.gov>
2009-12-20 14:23:04 -0600

    Add printout of "python_test complete" to end of python test.

--- tests/dependencies/python/python_test.py
+++ tests/dependencies/python/python_test.py
@@ -5,3 +5,5 @@ from python_test_options import opts
 if __name__ == "__main__":
     print "hello world from python_test.py"
     print "the value of x is",opts.get("x")
+    print "python_test complete"
+
