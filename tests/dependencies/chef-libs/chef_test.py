885ecacfea1294e63ddc81943d7c7de8a3703d64
James Amundson <amundson@fnal.gov>
2010-01-14 19:12:52 -0600

    test chef dependency; currently only imports

---
+++ tests/dependencies/chef-libs/chef_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from chef_test_options import opts
+
+from bmlfactory import *
+from basic_toolkit import *
+from mxyzptlk import *
+from beamline import *
+from physics_toolkit import *
+
+if __name__ == "__main__":
+    print "hello world from chef_test.py"
+    print "chef_test complete"
+
+
