7e871f7e7c30601bb400c0728aec6acffd684259
James Amundson <amundson@fnal.gov>
2010-03-10 11:14:42 -0600

    first stab at a set of physical constants

---
+++ components/foundation/physical_constants.h
@@ -0,0 +1,10 @@
+#ifndef PHYSICAL_CONSTANTS_H_
+#define PHYSICAL_CONSTANTS_H_
+
+namespace constants {
+    const double mp = 0.93827203; // Mass of proton [GeV/c^2]
+    const double me = 0.51099892e-3; // Mass of electron [GeV/c^2]
+    const double mmu = 0.105658369; // Mass of muon [GeV/c^2]
+}
+
+#endif /* PHYSICAL_CONSTANTS_H_ */
