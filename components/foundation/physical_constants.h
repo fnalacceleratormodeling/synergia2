eb0909b1e19abfec0e42169249c976af6b84ca46
James Amundson <amundson@fnal.gov>
2010-03-31 13:52:45 -0500

    add particle charges to physical constants

--- components/foundation/physical_constants.h
+++ components/foundation/physical_constants.h
@@ -5,6 +5,13 @@ namespace constants {
     const double mp = 0.93827203; // Mass of proton [GeV/c^2]
     const double me = 0.51099892e-3; // Mass of electron [GeV/c^2]
     const double mmu = 0.105658369; // Mass of muon [GeV/c^2]
+
+    const int proton_charge = 1;
+    const int antiproton_charge = -1;
+    const int electron_charge = -1;
+    const int positron_charge = 1;
+    const int muon_charge = -1;
+    const int antimuon_charge = 1;
 }
 
 #endif /* PHYSICAL_CONSTANTS_H_ */
