add41fcbe9e0523cd668858c7a7ffa95a82375b6
James Amundson <amundson@fnal.gov>
2010-03-31 13:59:06 -0500

    dummy implementation of propagator::propagate

--- components/simulation/propagator.cc
+++ components/simulation/propagator.cc
@@ -8,8 +8,9 @@ Propagator::construct()
             != stepper.get_steps().end(); ++s_it) {
         for (Operators::const_iterator o_it = (*s_it)->get_operators().begin(); o_it
                 != (*s_it)->get_operators().end(); ++o_it) {
-            for(Lattice_element_slices::const_iterator les_it = (*o_it)->get_slices().begin();
-                    les_it != (*o_it)->get_slices().end(); ++les_it) {
+            for (Lattice_element_slices::const_iterator les_it =
+                    (*o_it)->get_slices().begin(); les_it
+                    != (*o_it)->get_slices().end(); ++les_it) {
                 all_slices.push_back(*les_it);
             }
         }
@@ -27,7 +28,23 @@ void
 Propagator::propagate(Bunch & bunch, int num_turns, bool diagnostics_per_step,
         bool diagnostics_per_turn)
 {
-
+    for (int turn = 0; turn < num_turns; ++turn) {
+        for (Steps::const_iterator step_it = stepper.get_steps().begin(); step_it
+                != stepper.get_steps().end(); ++step_it) {
+            std::cout << "jfa: step\n";
+            for (Operators::const_iterator op_it = (*step_it)->get_operators().begin(); op_it
+                    != (*step_it)->get_operators().end(); ++op_it) {
+                std::cout << "jfa: apply operator " << (*op_it)->get_name()
+                        << std::endl;
+            }
+            if (diagnostics_per_step) {
+                std::cout << "jfa: per-step diagnostics\n";
+            }
+        }
+        if (diagnostics_per_turn) {
+            std::cout << "jfa: per-turn diagnostics\n";
+        }
+    }
 }
 
 Chef_lattice &
--- components/simulation/tests/test_propagator.cc
+++ components/simulation/tests/test_propagator.cc
@@ -3,9 +3,15 @@
 #include "components/simulation/propagator.h"
 #include "components/foundation/physical_constants.h"
 #include "components/lattice/chef_utils.h"
+#include "components/bunch/bunch.h"
+#include "utils/boost_test_mpi_fixture.h"
+BOOST_GLOBAL_FIXTURE(MPI_fixture)
+;
 
 const std::string name("foo");
 const double mass = constants::mp;
+const double real_num = 1.0e11;
+const int total_num = 1000;
 const double total_energy = 125.0;
 const double tolerance = 1.0e-12;
 const double quad_length = 0.2;
@@ -34,13 +40,40 @@ BOOST_AUTO_TEST_CASE(construct)
     Collective_operator_sptr space_charge(new Collective_operator(
             "space_charge"));
 
-    Split_operator_stepper stepper7(lattice, 7, space_charge);
-//    stepper7.print();
+    Split_operator_stepper stepper(lattice, 7, space_charge);
 
-    Propagator propagator(stepper7);
-//    std::cout << "original\n";
-//    print_chef_beamline(propagator.get_chef_lattice().get_beamline_sptr());
-//    std::cout << "sliced\n";
-//    print_chef_beamline(propagator.get_chef_lattice().get_sliced_beamline_sptr());
+    Propagator propagator(stepper);
+}
+
+BOOST_AUTO_TEST_CASE(propagate)
+{
+
+    Four_momentum four_momentum(mass, total_energy);
+    Reference_particle reference_particle(four_momentum);
+    Lattice_element f("quadrupole", "f");
+    f.set_double_attribute("l", quad_length);
+    Lattice_element o("drift", "o");
+    o.set_double_attribute("l", drift_length);
+    Lattice_element d("quadrupole", "d");
+    d.set_double_attribute("l", quad_length);
+
+    Lattice lattice(name);
+    lattice.append(f);
+    lattice.append(o);
+    lattice.append(d);
+    lattice.append(o);
+    lattice.set_reference_particle(reference_particle);
+
+    Collective_operator_sptr space_charge(new Collective_operator(
+            "space_charge"));
+
+    Split_operator_stepper stepper(lattice, 4, space_charge);
+    Propagator propagator(stepper);
+
+    Commxx comm(MPI_COMM_WORLD);
+    Bunch bunch(reference_particle, constants::proton_charge, total_num, real_num, comm);
+
+    int num_turns = 4;
+    propagator.propagate(bunch, num_turns, true, true);
 }
 
