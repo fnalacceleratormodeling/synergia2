c804f6dce9ba54a5ddb3d3cccc53124e599bb0aa
James Amundson <amundson@fnal.gov>
2010-04-01 13:39:12 -0500

    add Fast_mapping_term, Fast_mapping

---
+++ components/simulation/fast_mapping.cc
@@ -0,0 +1,262 @@
+#include "fast_mapping.h"
+#include "components/lattice/chef_utils.h"
+
+#define MANUAL_LOOP_UNROLL shockingly_yes
+
+std::string
+read_line_ignoring_comments(std::ifstream &file)
+{
+    std::string line;
+    getline(file, line);
+    if (line.size() == 0) {
+        return line;
+    }
+    while (((line[0] == '#') || (line.size() == 0))) {
+        getline(file, line);
+    }
+    return line;
+}
+
+Fast_mapping_term::Fast_mapping_term(int order)
+{
+    i = new int[order + 1];
+    this->order = order;
+}
+
+Fast_mapping_term::Fast_mapping_term(Fast_mapping_term const& t)
+{
+    the_coeff = t.the_coeff;
+    order = t.order;
+    i = new int[order + 1];
+    for (int j = 0; j < order + 1; ++j) {
+        i[j] = t.i[j];
+    }
+}
+
+inline double &
+Fast_mapping_term::coeff()
+{
+    return the_coeff;
+}
+
+inline double const&
+Fast_mapping_term::coeff() const
+{
+    return the_coeff;
+}
+
+inline int &
+Fast_mapping_term::index(int which)
+{
+    return i[which];
+}
+
+inline
+int const&
+Fast_mapping_term::index(int which) const
+{
+    return i[which];
+}
+
+void
+Fast_mapping_term::write_to_stream(std::ofstream& stream) const
+{
+    for (int j = 0; j < order + 1; ++j) {
+        stream << i[j] << " ";
+    }
+    stream.precision(17);
+    stream << the_coeff << std::endl;
+}
+
+void
+Fast_mapping_term::read_from_stream(std::ifstream& stream)
+{
+    std::stringstream sstream(read_line_ignoring_comments(stream));
+    for (int j = 0; j < order + 1; ++j) {
+        sstream >> i[j];
+    }
+    sstream >> the_coeff;
+}
+
+Fast_mapping_term::~Fast_mapping_term()
+{
+    delete[] i;
+}
+
+void
+Fast_mapping::init(int order)
+{
+    terms.resize(6);
+    this->order = order;
+    for (int comp_index = 0; comp_index < 6; ++comp_index) {
+        terms.at(comp_index).resize(order + 1);
+    }
+}
+
+Fast_mapping::Fast_mapping()
+{
+    init(0);
+}
+
+inline double
+quickpow(double x, int i)
+{
+    double retval = 1.0;
+    while (i--) {
+        retval *= x;
+    }
+    return retval;
+}
+
+Fast_mapping::Fast_mapping(Reference_particle const& reference_particle,
+        Mapping & chef_mapping)
+{
+    std::vector<double > u = chef_unit_conversion(reference_particle);
+    order = chef_mapping.Weight();
+    init(order);
+    for (int i = 0; i < 6; ++i) {
+        int chef_i = chef_index(i);
+        int nterm = 0;
+        Jet__environment_ptr env = chef_mapping.Env();
+        for (Jet::iterator jet_it = chef_mapping(chef_i).begin(); jet_it
+                != chef_mapping(chef_i).end(); ++jet_it) {
+            if (jet_it->coefficient() == 0.0) {
+                //ignore zero coefficients
+            } else {
+                int term_order = jet_it->exponents(env).Sum();
+                Fast_mapping_term tmp_term(term_order);
+                tmp_term.coeff() = jet_it->coefficient() * u[i];
+                int which = 0;
+                for (int index = 0; index < 6; ++index) {
+                    int chef_i2 = chef_index(index);
+                    int expt = jet_it->exponents(env)(chef_i2);
+                    for (int count = 0; count < expt; ++count) {
+                        tmp_term.index(which) = index;
+                        ++which;
+                    }
+                    tmp_term.coeff() *= 1.0 / quickpow(u[index], expt);
+                }
+                terms.at(i).at(term_order).push_back(tmp_term);
+            }
+        }
+    }
+}
+
+void
+Fast_mapping::apply(Bunch & bunch)
+{
+    double temp[6];
+    int local_num = bunch.get_local_num();
+    MArray2d_ref particles = bunch.get_local_particles();
+    for (int part = 0; part < local_num; ++part) {
+        for (int i = 0; i < 6; ++i) {
+            temp[i] = 0.0;
+            double term;
+            Fast_mapping_terms::const_iterator telem;
+            for (telem = terms[i][0].begin(); telem != terms[i][0].end(); ++telem) {
+                temp[i] += telem->coeff();
+            }
+            for (telem = terms[i][1].begin(); telem != terms[i][1].end(); ++telem) {
+                term = telem->coeff();
+                term *= particles[part][telem->index(0)];
+                temp[i] += term;
+            }
+            if (order > 1) {
+                for (telem = terms[i][2].begin(); telem != terms[i][2].end(); ++telem) {
+                    term = telem->coeff();
+                    term *= particles[part][telem->index(0)];
+                    term *= particles[part][telem->index(1)];
+                    temp[i] += term;
+                }
+                if (order > 2) {
+                    for (telem = terms[i][3].begin(); telem
+                            != terms[i][3].end(); ++telem) {
+                        term = telem->coeff();
+                        term *= particles[part][telem->index(0)];
+                        term *= particles[part][telem->index(1)];
+                        term *= particles[part][telem->index(2)];
+                        temp[i] += term;
+                    }
+                    if (order > 3) {
+                        for (int suborder = 4; suborder <= order; ++suborder) {
+                            for (telem = terms[i][suborder].begin(); telem
+                                    != terms[i][suborder].end(); ++telem) {
+                                term = telem->coeff();
+                                for (int j = 0; j < suborder; ++j) {
+                                    term *= particles[part][telem->index(j)];
+                                }
+                                temp[i] += term;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+#ifdef MANUAL_LOOP_UNROLL
+        particles[part][0] = temp[0];
+        particles[part][1] = temp[1];
+        particles[part][2] = temp[2];
+        particles[part][3] = temp[3];
+        particles[part][4] = temp[4];
+        particles[part][5] = temp[5];
+#else
+        for (int i = 0; i < 6; ++i) {
+            particles[ part][i] = temp[i];
+        }
+#endif
+    }
+}
+
+void
+Fast_mapping::write_to_file(std::string const& filename)
+{
+    std::ofstream file(filename.c_str());
+    file << "# Synergia Fast_mapping\n";
+    file << "# file format=1.0\n";
+    file << "# order:\n";
+    file << order << std::endl;
+    for (int index = 0; index < 6; ++index) {
+        for (int order = 0; order <= this->order; ++order) {
+            file << "# index=" << index << ", order=" << order << std::endl;
+            file << "# num:\n";
+            file << terms[index][order].size() << std::endl;
+            if (terms[index][order].size() > 0) {
+                file << "# terms:\n";
+                Fast_mapping_terms::const_iterator telem;
+                for (telem = terms[index][order].begin(); telem
+                        != terms[index][order].end(); ++telem) {
+                    telem->write_to_stream(file);
+                }
+            }
+        }
+    }
+    file << "end_fast_mapping\n";
+    file.close();
+}
+
+void
+Fast_mapping::read_from_file(std::string const& filename)
+{
+    std::ifstream file(filename.c_str());
+    std::stringstream stream(read_line_ignoring_comments(file));
+    stream >> order;
+    init(order);
+    int num_terms_read;
+    for (int index = 0; index < 6; ++index) {
+        for (int order = 0; order <= this->order; ++order) {
+            stream.str(read_line_ignoring_comments(file));
+            stream.clear();
+            stream >> num_terms_read;
+            for (int term_index = 0; term_index < num_terms_read; ++term_index) {
+                Fast_mapping_term tmp_term(order);
+                tmp_term.read_from_stream(file);
+                terms.at(index).at(order).push_back(tmp_term);
+            }
+        }
+    }
+    if (read_line_ignoring_comments(file) != "end_fast_mapping") {
+        throw std::runtime_error("Fast_mapping::read_from_file(" + filename
+                + ") found a truncated file.");
+    }
+    file.close();
+}
---
+++ components/simulation/fast_mapping.h
@@ -0,0 +1,120 @@
+#ifndef FAST_MAPPING_H_
+#define FAST_MAPPING_H_
+
+#include "components/foundation/reference_particle.h"
+#include "components/bunch/bunch.h"
+#include "mxyzptlk/Mapping.h"
+#include "components/lattice/chef_utils.h"
+
+#include <vector>
+#include <list>
+#include <fstream>
+#include <string>
+
+// A Fast_mapping_term represents one term in a polynomial expansion of order
+// "order". It contains a coefficient "coeff" and a c-style vector "i" of length
+// "order" containing the vector indices of the dependent variable.
+//
+// Examples:
+//   3.14 * p_1
+//   => Fast_mapping_term fmt(1); fmt.coeff = 3.14; fmt.i[0] = 1;
+//
+//   1.7724 * p_0^3 (i.e., p_0 cubed)
+//   => Fast_mapping_term fmt(3); fmt.coeff = 1.7724;
+//          fmt.i[0] = 0; fmt.i[1] = 0; fmt.i[2] = 0;
+//
+//   2.2 * p_0*p_2*p_4
+//   => Fast_mapping_term fmt(3); fmt.coeff = 2.2;
+//          fmt.i[0] = 0; fmt.i[1] = 2; fmt.i[2] = 4;
+//
+class Fast_mapping_term
+{
+private:
+    double the_coeff;
+    int *i;
+    int order;
+public:
+    Fast_mapping_term(int order);
+    Fast_mapping_term(Fast_mapping_term const& fast_mapping_term);
+    inline double &
+    coeff();
+    inline double const&
+    coeff() const;
+    inline int &
+    index(int which);
+    inline int const&
+    index(int which) const;
+    void
+    write_to_stream(std::ofstream & stream) const;
+    void
+    read_from_stream(std::ifstream & stream);
+    ~Fast_mapping_term();
+};
+
+typedef std::list<Fast_mapping_term > Fast_mapping_terms;
+
+// Fast_mapping is a sparse container for a collection of arbitrary-order
+// terms in a polynomial expansion taking the six-dimensional phase space
+// variable g to a new variable f:
+//
+// f^{i} = C_{0}^{i} + C_{1}^{ij} g^{j} + \
+//         C_{2}^{ijk} g^{j}g^{k} + \
+//         C_{3}^{ijkl}g^{j}g^{k}g^{l} + ...
+//
+// Only the non-zero terms are stored.
+// The "terms" member contains a vector of vectors of lists of
+// "Fast_mapping_terms", each of which corresponds to one term in the
+// fully expanded version of the above polynomial. The details of the
+// storage are designed for optimized application of the mappings, not
+// not necessarily for clarity. The structure is described below.
+//
+// The outermost vector in terms runs from 0 to 5, corresponding to the
+// components x px y py t pt of the output vector, $i$ in the equation
+// above.
+// For each $i$, There is a vector running from 0 to N+1, where N is the
+// highest order term present. Each component of this vector contains
+// a list of all the non-zero terms at that order. The Fast_mapping_term's
+// themselves are defined near the definition of the class.
+//
+// Example, using only a single value of $i$:
+//
+// f^1 = 1.1 + 2.2*g^1 - 3.0*g^2 + 4.0*g^4*g^5
+//
+// Fast_mapping_term tmp_term0(0);
+// tmp_term0.coeff = 1.1;
+// terms.at(1).at(0).push_back(tmp_term0);
+//
+// Fast_mapping_term tmp_term1(1);
+// tmp_term1.coeff = 2.2;
+// tmp_term1.i[0] = 1;
+// terms.at(1).at(1).push_back(tmp_term1);
+// tmp_term1.coeff = 3.0;
+// tmp_term1.i[0] = 2;
+// terms.at(1).at(1).push_back(tmp_term1);
+//
+// Fast_mapping_term tmp_term2(2);
+// tmp_term2.coeff = 4.0;
+// tmp_term2.i[0] = 4;
+// tmp_term2.i[1] = 5;
+// terms.at(1).at(2).push_back(tmp_term2);
+//
+class Fast_mapping
+{
+private:
+    std::vector<std::vector<Fast_mapping_terms > > terms;
+    int order;
+    void
+    init(int order);
+public:
+    Fast_mapping();
+    Fast_mapping(Reference_particle const& reference_particle,
+            Mapping & chef_mapping);
+    void
+    apply(Bunch & bunch);
+    void
+    write_to_file(std::string const& filename);
+    void
+    read_from_file(std::string const& filename);
+};
+
+#endif /* FAST_MAPPING_H_ */
