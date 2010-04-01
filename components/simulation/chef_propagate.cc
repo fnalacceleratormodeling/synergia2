4ac9c79bf1f7658e4cd0856e4894839583adf7b9
James Amundson <amundson@fnal.gov>
2010-04-01 16:40:19 -0500

    add chef_propagate

---
+++ components/simulation/chef_propagate.cc
@@ -0,0 +1,35 @@
+#include "chef_propagate.h"
+#include "components/lattice/chef_utils.h"
+
+// jfa: This routine is incorrect when passing through an accelerating element.
+// Please fix it.
+void
+chef_propagate(Bunch & bunch, Chef_elements & chef_elements)
+{
+    std::vector<double > u = chef_unit_conversion(
+            bunch.get_reference_particle());
+    int local_num = bunch.get_local_num();
+    MArray2d_ref particles = bunch.get_local_particles();
+
+    Vector chef_state(6);
+    Particle particle(reference_particle_to_chef_particle(
+            bunch.get_reference_particle()));
+    for (int part = 0; part < local_num; ++part) {
+        for (int synergia_index = 0; synergia_index < 6; ++synergia_index) {
+            int chef_idx = get_chef_index(synergia_index);
+            chef_state[chef_idx] = particles[part][synergia_index]
+                    / u[synergia_index];
+        }
+        particle.State() = chef_state;
+        for (Chef_elements::iterator it = chef_elements.begin(); it
+                != chef_elements.end(); ++it) {
+            (*it)->propagate(particle);
+        }
+        chef_state = particle.State();
+        for (int synergia_index = 0; synergia_index < 6; ++synergia_index) {
+            int chef_idx = get_chef_index(synergia_index);
+            particles[part][synergia_index] = chef_state[chef_idx]
+                    * u[synergia_index];
+        }
+    }
+}
---
+++ components/simulation/chef_propagate.h
@@ -0,0 +1,10 @@
+#ifndef CHEF_PROPAGATE_H_
+#define CHEF_PROPAGATE_H_
+
+#include "components/bunch/bunch.h"
+#include "components/lattice/chef_lattice.h"
+
+void
+chef_propagate(Bunch & bunch, Chef_elements & chef_elements);
+
+#endif /* CHEF_PROPAGATE_H_ */
