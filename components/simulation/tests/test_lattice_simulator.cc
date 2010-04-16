e8e0f1bd07043ffa85070f0bc2cb4817b7469458
James Amundson <amundson@fnal.gov>
2010-04-16 16:52:20 -0500

    another Lattice_simulator test

--- components/simulation/tests/test_lattice_simulator.cc
+++ components/simulation/tests/test_lattice_simulator.cc
@@ -14,3 +14,21 @@ BOOST_FIXTURE_TEST_CASE(construct, Lattice_fixture)
     Lattice_simulator lattice_simulator(lattice_sptr, map_order);
 }
 
+BOOST_FIXTURE_TEST_CASE(construct_sliced_chef_beamline, Lattice_fixture)
+{
+    Lattice_simulator lattice_simulator(lattice_sptr, map_order);
+    Lattice_element_slices slices;
+    for (Lattice_elements::const_iterator it =
+            lattice_sptr->get_elements().begin(); it
+            != lattice_sptr->get_elements().end(); ++it) {
+        double length = (*it)->get_length();
+        Lattice_element_slice_sptr first_half(new Lattice_element_slice(*(*it), 0.0,
+                0.5 * length));
+        Lattice_element_slice_sptr second_half(new Lattice_element_slice(*(*it),
+                0.5 * length, length));
+        slices.push_back(first_half);
+        slices.push_back(second_half);
+    }
+    lattice_simulator.construct_sliced_chef_beamline(slices);
+}
+
