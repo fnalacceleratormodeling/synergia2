47158c2eeb6962c9d090ad3b0973772697526ec8
James Amundson <amundson@fnal.gov>
2010-04-16 16:57:54 -0500

    complete test_lattice_simulator

--- components/simulation/tests/test_lattice_simulator.cc
+++ components/simulation/tests/test_lattice_simulator.cc
@@ -22,13 +22,55 @@ BOOST_FIXTURE_TEST_CASE(construct_sliced_chef_beamline, Lattice_fixture)
             lattice_sptr->get_elements().begin(); it
             != lattice_sptr->get_elements().end(); ++it) {
         double length = (*it)->get_length();
-        Lattice_element_slice_sptr first_half(new Lattice_element_slice(*(*it), 0.0,
-                0.5 * length));
-        Lattice_element_slice_sptr second_half(new Lattice_element_slice(*(*it),
-                0.5 * length, length));
+        Lattice_element_slice_sptr first_half(new Lattice_element_slice(*(*it),
+                0.0, 0.5 * length));
+        Lattice_element_slice_sptr second_half(new Lattice_element_slice(
+                *(*it), 0.5 * length, length));
         slices.push_back(first_half);
         slices.push_back(second_half);
     }
     lattice_simulator.construct_sliced_chef_beamline(slices);
 }
 
+BOOST_FIXTURE_TEST_CASE(get_map_order, Lattice_fixture)
+{
+    Lattice_simulator lattice_simulator(lattice_sptr, map_order);
+
+    BOOST_CHECK_EQUAL(lattice_simulator.get_map_order(), map_order);
+}
+
+BOOST_FIXTURE_TEST_CASE(get_operation_extraction_map, Lattice_fixture)
+{
+    Lattice_simulator lattice_simulator(lattice_sptr, map_order);
+
+    std::list<std::string >
+            names(
+                    lattice_simulator.get_operation_extraction_map().get_extractor_names());
+    std::list<std::string > expected_names;
+    expected_names.push_back(default_operation_extractor_name);
+    expected_names.push_back(chef_mixed_operation_extractor_name);
+    expected_names.push_back(chef_map_operation_extractor_name);
+    expected_names.push_back(chef_propagate_operation_extractor_name);
+
+    BOOST_CHECK_EQUAL(names.size(), expected_names.size());
+    names.sort();
+    expected_names.sort();
+    for (std::list<std::string >::iterator it = names.begin(), expected_it =
+            expected_names.begin(); it != names.end(); ++it, ++expected_it) {
+        BOOST_CHECK_EQUAL((*it), (*expected_it));
+    }
+
+    BOOST_CHECK_EQUAL(lattice_simulator.get_map_order(), map_order);
+}
+
+BOOST_FIXTURE_TEST_CASE(get_lattice_sptr, Lattice_fixture)
+{
+    Lattice_simulator lattice_simulator(lattice_sptr, map_order);
+    lattice_simulator.get_lattice_sptr();
+}
+
+BOOST_FIXTURE_TEST_CASE(get_chef_lattice_sptr, Lattice_fixture)
+{
+    Lattice_simulator lattice_simulator(lattice_sptr, map_order);
+    lattice_simulator.get_chef_lattice_sptr();
+}
