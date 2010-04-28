8b90cb6ea65223c88a4fd76106856dc36bd2c939
James Amundson <amundson@fnal.gov>
2010-04-28 13:23:12 -0500

    First rumblings of an optics module

---
+++ components/optics/one_turn_map.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import numpy
+from pylattice import reference_particle_to_chef_jet_particle, \
+    chef_unit_conversion, get_chef_index
+
+import basic_toolkit
+import mxyzptlk
+from beamline import JetProton, JetParticle
+
+def _convert_linear_map(chef_map, reference_particle):
+    u = chef_unit_conversion(reference_particle)
+    map = numpy.zeros((6, 6), 'd')
+    for row in range(0, 6):
+        for column in range(0, 6):
+            chef_row = get_chef_index(row)
+            chef_column = get_chef_index(column)
+            map[row, column] = chef_map.get(chef_row, chef_column) * \
+                              u[row] / u[column]
+    return map
+
+def linear_one_turn_map(lattice_simulator):
+    reference_particle = lattice_simulator.get_lattice().get_reference_particle()
+    map_order = lattice_simulator.get_map_order()
+    JetParticle.createStandardEnvironments(map_order)
+    jet_particle = JetProton(reference_particle.get_four_momentum().get_total_energy())
+#    jet_particle = reference_particle_to_chef_jet_particle(reference_particle,
+#                                                           map_order)
+    lattice_simulator.get_chef_lattice().get_beamline().propagateJetParticle(jet_particle)
+    return _convert_linear_map(jet_particle.State().jacobian(), reference_particle)
+
+
---
+++ components/optics/tests/test_one_turn_map.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append("../../simulation")
+sys.path.append("../../lattice")
+sys.path.append("../../foundation")
+sys.path.append("..")
+sys.path.append("/home/amundson/work/synergia2-devel_1_0/install/lib")
+
+from one_turn_map import linear_one_turn_map
+from mad8_reader import Mad8_reader
+from pysimulation import Collective_operator, Lattice_simulator, \
+    Split_operator_stepper
+
+def test_linear_one_turn_map():
+    num_steps = 1
+    map_order = 2
+    lattice = Mad8_reader().get_lattice("fodo", "../../lattice/tests/fodo.lat")
+    space_charge = Collective_operator("dummy")
+    lattice_simulator = Lattice_simulator(lattice, map_order)
+    stepper = Split_operator_stepper(lattice_simulator, space_charge, num_steps)
+    map = linear_one_turn_map(lattice_simulator)
+
