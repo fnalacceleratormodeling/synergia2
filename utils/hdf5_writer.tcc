39861f87762cb67a39bc9d01291e00a110995ce7
James Amundson <amundson@fnal.gov>
2010-05-13 15:45:36 -0500

    Hdf5_writer now handles int, double, MArray1d, MArray2d and MArray3d

--- utils/hdf5_writer.tcc
+++ utils/hdf5_writer.tcc
@@ -67,6 +67,20 @@ template<>
     {
     }
 
+template<>
+    Hdf5_writer<MArray2d_ref >::Hdf5_writer(hid_t & file,
+            std::string const& name) :
+        file(file), name(name), data_rank(MArray2d_ref::dimensionality)
+    {
+    }
+
+template<>
+    Hdf5_writer<MArray3d_ref >::Hdf5_writer(hid_t & file,
+            std::string const& name) :
+        file(file), name(name), data_rank(MArray3d_ref::dimensionality)
+    {
+    }
+
 template<typename T>
     void
     Hdf5_writer<T >::append(T & data)
@@ -91,6 +105,52 @@ template<typename T>
 template<>
     void
     Hdf5_writer<MArray1d_ref >::append(MArray1d_ref & data)
+    {
+        if (!have_setup) {
+            std::vector<int > data_dims(data_rank);
+            for (int i = 0; i < data_rank; ++i) {
+                data_dims.at(i) = data.shape()[i];
+            }
+            setup(data_dims, h5_atomic_typename<double > ());
+        }
+        ++size[data_rank];
+        status = H5Dextend(dataset, &size[0]);
+
+        filespace = H5Dget_space(dataset);
+        have_filespace = true;
+        status = H5Sselect_hyperslab(filespace, H5S_SELECT_SET, &offset[0],
+                NULL, &dims[0], NULL);
+        status = H5Dwrite(dataset, h5_atomic_type, dataspace, filespace,
+                H5P_DEFAULT, data.origin());
+        ++offset[data_rank];
+    }
+
+template<>
+    void
+    Hdf5_writer<MArray2d_ref >::append(MArray2d_ref & data)
+    {
+        if (!have_setup) {
+            std::vector<int > data_dims(data_rank);
+            for (int i = 0; i < data_rank; ++i) {
+                data_dims.at(i) = data.shape()[i];
+            }
+            setup(data_dims, h5_atomic_typename<double > ());
+        }
+        ++size[data_rank];
+        status = H5Dextend(dataset, &size[0]);
+
+        filespace = H5Dget_space(dataset);
+        have_filespace = true;
+        status = H5Sselect_hyperslab(filespace, H5S_SELECT_SET, &offset[0],
+                NULL, &dims[0], NULL);
+        status = H5Dwrite(dataset, h5_atomic_type, dataspace, filespace,
+                H5P_DEFAULT, data.origin());
+        ++offset[data_rank];
+    }
+
+template<>
+    void
+    Hdf5_writer<MArray3d_ref >::append(MArray3d_ref & data)
     {
         if (!have_setup) {
             std::vector<int > data_dims(data_rank);
--- utils/tests/test_hdf5_writer.cc
+++ utils/tests/test_hdf5_writer.cc
@@ -3,11 +3,15 @@
 #include "utils/hdf5_writer.h"
 #include "utils/multi_array_typedefs.h"
 
+// jfa: these are bad tests because they require the user
+// to manually inspect the output files.
+
 BOOST_AUTO_TEST_CASE(integer)
 {
-    hid_t file = H5Fcreate("integer.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+    hid_t file = H5Fcreate("integer.h5", H5F_ACC_TRUNC, H5P_DEFAULT,
+            H5P_DEFAULT);
     Hdf5_writer<int > writer(file, "i");
-    for(int i=0; i<5; ++i) {
+    for (int i = 0; i < 5; ++i) {
         writer.append(i);
     }
     H5Fclose(file);
@@ -15,10 +19,11 @@ BOOST_AUTO_TEST_CASE(integer)
 
 BOOST_AUTO_TEST_CASE(double_float)
 {
-    hid_t file = H5Fcreate("double_float.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+    hid_t file = H5Fcreate("double_float.h5", H5F_ACC_TRUNC, H5P_DEFAULT,
+            H5P_DEFAULT);
     Hdf5_writer<double > writer(file, "double_float");
-    for(int i=0; i<5; ++i) {
-        double x = 1.1*i;
+    for (int i = 0; i < 5; ++i) {
+        double x = 1.1 * i;
         writer.append(x);
     }
     H5Fclose(file);
@@ -26,13 +31,55 @@ BOOST_AUTO_TEST_CASE(double_float)
 
 BOOST_AUTO_TEST_CASE(array1d)
 {
-    hid_t file = H5Fcreate("array1d.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+    hid_t file = H5Fcreate("array1d.h5", H5F_ACC_TRUNC, H5P_DEFAULT,
+            H5P_DEFAULT);
     const int dim = 6;
     MArray1d a(boost::extents[dim]);
     Hdf5_writer<MArray1d_ref > writer(file, "array1d");
-    for(int i=0; i<5; ++i) {
-        for(int j = 0; j< dim; ++j) {
-            a[j] = 1.1*j + 10*i;
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < dim; ++j) {
+            a[j] = 1.1 * j + 10 * i;
+        }
+        writer.append(a);
+    }
+    H5Fclose(file);
+}
+
+BOOST_AUTO_TEST_CASE(array2d)
+{
+    hid_t file = H5Fcreate("array2d.h5", H5F_ACC_TRUNC, H5P_DEFAULT,
+            H5P_DEFAULT);
+    const int dim1 = 2;
+    const int dim2 = 3;
+    MArray2d a(boost::extents[dim1][dim2]);
+    Hdf5_writer<MArray2d_ref > writer(file, "array2d");
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < dim1; ++j) {
+            for (int k = 0; k < dim2; ++k) {
+                a[j][k] = 1.1 * k + 10 * j + 100 * i;
+            }
+        }
+        writer.append(a);
+    }
+    H5Fclose(file);
+}
+
+BOOST_AUTO_TEST_CASE(array3d)
+{
+    hid_t file = H5Fcreate("array3d.h5", H5F_ACC_TRUNC, H5P_DEFAULT,
+            H5P_DEFAULT);
+    const int dim1 = 2;
+    const int dim2 = 3;
+    const int dim3 = 4;
+    MArray3d a(boost::extents[dim1][dim2][dim3]);
+    Hdf5_writer<MArray3d_ref > writer(file, "array3d");
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < dim1; ++j) {
+            for (int k = 0; k < dim2; ++k) {
+                for (int l = 0; l < dim2; ++l) {
+                    a[j][k][l] = 1.1 * l + 10 * k + 100 * j + 1000 * i;
+                }
+            }
         }
         writer.append(a);
     }
