6b2f48e0397d1faebd5847d660e7f5ae8600065c
James Amundson <amundson@fnal.gov>
2010-05-19 15:05:53 -0500

    inspect_h5_file.py: first of a set of analysis files; to be relocated later

---
+++ components/inspect_h5_file.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import sys
+import tables
+
+def usage(error=False):
+    print "usage:"
+    print "     " + sys.argv[0] + " <hdf5_file>"
+    print "     " + "    to list members"
+    print "or"
+    print "     " + sys.argv[0] + " <hdf5_file> <entry>"
+    print "     " + "      to display entry"
+    if error:
+        retval = 1
+    else:
+        retval = 0
+    sys.exit(retval)
+
+def print_entries(filename):
+    f = tables.openFile(filename, 'r')
+    for entry in  dir(f.root):
+        if entry[0] != '_':
+            print entry
+    f.close()
+
+def print_entry(filename, entry):
+    f = tables.openFile(filename, 'r')
+    print getattr(f.root, entry).read()
+    f.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        usage(error=True)
+    elif len(sys.argv) == 2:
+        if sys.argv[1] == '--help':
+            usage()
+        else:
+            print_entries(sys.argv[1])
+    elif len(sys.argv) == 3:
+        print_entry(sys.argv[1], sys.argv[2])
+    else:
+        usage(error=True)
