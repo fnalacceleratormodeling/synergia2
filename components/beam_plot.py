c7051b7a7ad01b68633091501c32957f3110c34d
James Amundson <amundson@fnal.gov>
2010-06-02 17:55:21 -0500

    basic plots work in beam_plot.py

--- components/beam_plot.py
+++ components/beam_plot.py
@@ -2,11 +2,37 @@
 
 import sys
 import tables
+import numpy
+import math
+import pylab
 from matplotlib import pyplot
+from mpl_toolkits.axes_grid import make_axes_locatable
 
-def plot_density(x, y, label):
+def plot_density(x, y, label, bins):
     fancylabel = label.replace('_', ' ')
-    pyplot.plot(x, y, '.', label=fancylabel)
+
+    fig = pyplot.figure(1)
+    axScatter = pyplot.subplot(111)
+    divider = make_axes_locatable(axScatter)
+    axHistx = divider.new_vertical(1.2, pad=0.1, sharex=axScatter)
+    axHisty = divider.new_horizontal(1.2, pad=0.1, sharey=axScatter)
+    fig.add_axes(axHistx)
+    fig.add_axes(axHisty)
+
+    pyplot.setp(axHistx.get_xticklabels() + axHisty.get_yticklabels(),
+         visible=False)
+
+    H, xedges, yedges = numpy.histogram2d(x, y, bins=bins)
+    for i in range(0, H.shape[0]):
+        for j in range(0, H.shape[1]):
+            if H[i, j] > 0:
+                H[i, j] = math.log(H[i, j])
+    X, Y = pylab.meshgrid(xedges, yedges)
+    axScatter.pcolor(X, Y, H.transpose())
+    axScatter.set_axis_bgcolor((0, 0, 0.5))
+    axHistx.hist(x, bins=bins)
+    axHisty.hist(y, bins=bins, orientation='horizontal')
+#    axScatter.plot(x, y, '.', label=fancylabel)
 
 coords = {}
 coords['x'] = 0
@@ -23,6 +49,7 @@ class Options:
         self.outputfile = None
         self.hcoord = None
         self.vcoord = None
+        self.bins = 10
 
 def do_error(message):
     sys.stderr.write(message + '\n')
@@ -32,6 +59,7 @@ def do_help():
     print "usage: beam_plot.py <filename> [option1] ... [optionn] <h coord> <v coord>"
     print "available options are:"
     print "    --nohist : do not show histograms (not on by default)"
+    print "    --bins=<num> : number of bins in each direction"
     print "    --output=<file> : save output to file (not on by default)"
     print "    --show : show plots on screen (on by default unless --output flag is present"
     print "available coords are:"
@@ -57,6 +85,8 @@ def handle_args(args):
                 do_help()
             elif arg == '--nohist':
                 options.hist = false
+            elif arg.find('--bins') == 0:
+                options.bins = int(arg.split('=')[1])
             elif arg == '--show':
                 options.show = True
             elif arg.find('--output') == 0:
@@ -70,7 +100,8 @@ def handle_args(args):
 def do_plots(options):
     f = tables.openFile(options.inputfile, 'r')
     particles = f.root.particles.read()
-    plot_density(particles[:, coords[options.hcoord]], particles[:, coords[options.vcoord]],'foobar')
+    plot_density(particles[:, coords[options.hcoord]],
+                 particles[:, coords[options.vcoord]], 'foobar', options.bins)
     pyplot.show()
 
 if __name__ == '__main__':
