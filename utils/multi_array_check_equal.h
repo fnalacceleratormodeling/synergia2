cfe0c5222254b62071c3ef79560911c84b6b0495
James Amundson <amundson@fnal.gov>
2010-06-28 15:39:10 -0500

    do not assume zero offsets when checking arrays

--- utils/multi_array_check_equal.h
+++ utils/multi_array_check_equal.h
@@ -5,8 +5,9 @@ inline void
 multi_array_check_equal(Const_MArray1d_ref const& a,
         Const_MArray1d_ref const& b, double tolerance)
 {
+    BOOST_CHECK_EQUAL(a.index_bases()[0], b.index_bases()[0]);
     BOOST_CHECK_EQUAL(a.shape()[0], b.shape()[0]);
-    for (unsigned int i = 0; i < a.shape()[0]; ++i) {
+    for (int i = a.index_bases()[0]; i < a.index_bases()[0] + a.shape()[0]; ++i) {
         if (a[i] == 0.0) {
             BOOST_CHECK_SMALL(b[i], tolerance);
         } else {
@@ -19,10 +20,12 @@ inline void
 multi_array_check_equal(Const_MArray2d_ref const& a,
         Const_MArray2d_ref const& b, double tolerance)
 {
+    BOOST_CHECK_EQUAL(a.index_bases()[0], b.index_bases()[0]);
+    BOOST_CHECK_EQUAL(a.index_bases()[1], b.index_bases()[1]);
     BOOST_CHECK_EQUAL(a.shape()[0], b.shape()[0]);
     BOOST_CHECK_EQUAL(a.shape()[1], b.shape()[1]);
-    for (unsigned int i = 0; i < a.shape()[0]; ++i) {
-        for (unsigned int j = 0; j < a.shape()[1]; ++j) {
+    for (int i = a.index_bases()[0]; i < a.index_bases()[0] + a.shape()[0]; ++i) {
+        for (int j = a.index_bases()[1]; j < a.index_bases()[1] + a.shape()[1]; ++j) {
             if (a[i][j] == 0.0) {
                 BOOST_CHECK_SMALL(b[i][j], tolerance);
             } else {
@@ -36,12 +39,16 @@ inline void
 multi_array_check_equal(Const_MArray3d_ref const& a,
         Const_MArray3d_ref const& b, double tolerance)
 {
+    BOOST_CHECK_EQUAL(a.index_bases()[0], b.index_bases()[0]);
+    BOOST_CHECK_EQUAL(a.index_bases()[1], b.index_bases()[1]);
+    BOOST_CHECK_EQUAL(a.index_bases()[2], b.index_bases()[2]);
     BOOST_CHECK_EQUAL(a.shape()[0], b.shape()[0]);
     BOOST_CHECK_EQUAL(a.shape()[1], b.shape()[1]);
     BOOST_CHECK_EQUAL(a.shape()[2], b.shape()[2]);
-    for (unsigned int i = 0; i < a.shape()[0]; ++i) {
-        for (unsigned int j = 0; j < a.shape()[1]; ++j) {
-            for (unsigned int k = 0; k < a.shape()[2]; ++k) {
+    for (int i = a.index_bases()[0]; i < a.index_bases()[0] + a.shape()[0]; ++i) {
+        for (int j = a.index_bases()[1]; j < a.index_bases()[1] + a.shape()[1]; ++j) {
+            for (int k = a.index_bases()[2]; k < a.index_bases()[2]
+                    + a.shape()[2]; ++k) {
                 if (a[i][j][k] == 0.0) {
                     BOOST_CHECK_SMALL(b[i][j][k], tolerance);
                 } else {
