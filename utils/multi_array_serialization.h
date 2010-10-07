b3ce54ed5e510494dbb072536577f8eca5889d4a
James Amundson <amundson@fnal.gov>
2010-10-07 13:06:02 -0500

    add serialization for 1- and 2-d multi_arrays

---
+++ utils/multi_array_serialization.h
@@ -0,0 +1,77 @@
+#ifndef MULTI_ARRAY_SERIALIZATION_H_
+#define MULTI_ARRAY_SERIALIZATION_H_
+
+#include "boost/multi_array.hpp"
+#include "boost/serialization/nvp.hpp"
+#include "boost/serialization/utility.hpp"
+#include "boost/serialization/version.hpp"
+
+// n.b. Serialization of 3d arrays not yet implemented.
+
+namespace boost
+{
+    namespace serialization
+    {
+        template<typename T, class Archive>
+            void
+            load(Archive & ar, boost::multi_array<T, 1 > & t,
+                    const unsigned int file_version)
+            {
+                typedef boost::multi_array<T, 1 > multi_array_;
+                typename multi_array_::size_type shape_0;
+                ar >> BOOST_SERIALIZATION_NVP(shape_0);
+                t.resize(boost::extents[shape_0]);
+                ar >> make_array(t.data(), t.num_elements());
+            }
+        template<typename T, class Archive>
+            void
+            load(Archive & ar, boost::multi_array<T, 2 > & t,
+                    const unsigned int file_version)
+            {
+                typedef boost::multi_array<T, 2 > multi_array_;
+                typename multi_array_::size_type shape_0, shape_1;
+                ar >> BOOST_SERIALIZATION_NVP(shape_0);
+                ar >> BOOST_SERIALIZATION_NVP(shape_1);
+                t.resize(boost::extents[shape_0][shape_1]);
+                ar >> make_array(t.data(), t.num_elements());
+            }
+        template<typename T, typename Archive>
+            void
+            save(Archive & ar, const boost::multi_array<T, 1 > & t,
+                    const unsigned int file_version)
+            {
+                typedef boost::multi_array<T, 1 > multi_array_;
+                typename multi_array_::size_type shape_0 = t.shape()[0];
+                ar << BOOST_SERIALIZATION_NVP(shape_0);
+                ar << make_array(t.data(), t.num_elements());
+            }
+        template<typename T, typename Archive>
+            void
+            save(Archive & ar, const boost::multi_array<T, 2 > & t,
+                    const unsigned int file_version)
+            {
+                typedef boost::multi_array<T, 2 > multi_array_;
+                typename multi_array_::size_type shape_0 = t.shape()[0];
+                typename multi_array_::size_type shape_1 = t.shape()[1];
+                ar << BOOST_SERIALIZATION_NVP(shape_0);
+                ar << BOOST_SERIALIZATION_NVP(shape_1);
+                ar << make_array(t.data(), t.num_elements());
+            }
+        template<typename T, class Archive>
+            void
+            serialize(Archive & ar, boost::multi_array<T, 1 > & t,
+                    const unsigned int file_version)
+            {
+                split_free(ar, t, file_version);
+            }
+        template<typename T, class Archive>
+            void
+            serialize(Archive & ar, boost::multi_array<T, 2 > & t,
+                    const unsigned int file_version)
+            {
+                split_free(ar, t, file_version);
+            }
+    }
+}
+
+#endif /* MULTI_ARRAY_SERIALIZATION_H_ */
---
+++ utils/tests/test_multi_array_serialization.cc
@@ -0,0 +1,36 @@
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+#include "utils/multi_array_typedefs.h"
+#include "utils/multi_array_serialization.h"
+#include "utils/xml_serialization.h"
+
+#include "utils/multi_array_check_equal.h"
+
+const double tolerance = 1.0e-14;
+
+BOOST_AUTO_TEST_CASE(test1d)
+{
+    MArray1d a(boost::extents[7]);
+    for (int i = 0; i < a.shape()[0]; ++i) {
+        a[i] = 1.1 * i;
+    }
+    xml_save<MArray1d > (a, "multi_array1d.xml");
+    MArray1d b;
+    xml_load<MArray1d > (b, "multi_array1d.xml");
+    multi_array_check_equal(a, b, tolerance);
+}
+
+BOOST_AUTO_TEST_CASE(test2d)
+{
+    MArray2d a(boost::extents[7][9]);
+    for (int i = 0; i < a.shape()[0]; ++i) {
+        for (int j = 0; j < a.shape()[1]; ++j) {
+            a[i][j] = 11.1 * i + 0.93 * j;
+        }
+    }
+    xml_save<MArray2d > (a, "multi_array2d.xml");
+    MArray2d b;
+    xml_load<MArray2d > (b, "multi_array2d.xml");
+    multi_array_check_equal(a, b, tolerance);
+}
+
