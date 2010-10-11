9af57035622f0777408efe5995223314e5652c19
James Amundson <amundson@fnal.gov>
2010-10-11 13:19:38 -0500

    add support for saving multi_arrays to xml serialization files. load support will require more thought.

--- components/convertors/convertors_wrap.cc
+++ components/convertors/convertors_wrap.cc
@@ -1,7 +1,11 @@
 #include <boost/python.hpp>
 #include "boost/multi_array.hpp"
+#include "utils/multi_array_typedefs.h"
 #include "utils/numpy_multi_ref_converter.h"
 #include "utils/comm_converter.h"
+#include "utils/multi_array_serialization.h"
+#include "utils/xml_serialization.h"
+
 
 using namespace boost::python;
 
@@ -16,4 +20,7 @@ BOOST_PYTHON_MODULE(pyconvertors)
     numpy_const_multi_array_ref_converter<double, 1 >::register_to_and_from_python();
     numpy_multi_array_ref_converter<double, 2 >::register_to_and_from_python();
     numpy_const_multi_array_ref_converter<double, 2 >::register_to_and_from_python();
+
+    def("xml_save_array1d", xml_save<MArray1d_ref > );
+    def("xml_save_array2d", xml_save<MArray2d_ref > );
 }
--- utils/multi_array_serialization.h
+++ utils/multi_array_serialization.h
@@ -23,6 +23,18 @@ namespace boost
                 t.resize(boost::extents[shape_0]);
                 ar >> make_array(t.data(), t.num_elements());
             }
+        // jfa: this code can't work as written...
+        //        template<typename T, class Archive>
+        //            void
+        //            load(Archive & ar, boost::multi_array_ref<T, 1 > & t,
+        //                    const unsigned int file_version)
+        //            {
+        //                typedef boost::multi_array<T, 1 > multi_array_;
+        //                typename multi_array_::size_type shape_0;
+        //                ar >> BOOST_SERIALIZATION_NVP(shape_0);
+        //                t.resize(boost::extents[shape_0]);
+        //                ar >> make_array(t.data(), t.num_elements());
+        //            }
         template<typename T, class Archive>
             void
             load(Archive & ar, boost::multi_array<T, 2 > & t,
@@ -47,7 +59,29 @@ namespace boost
             }
         template<typename T, typename Archive>
             void
+            save(Archive & ar, const boost::multi_array_ref<T, 1 > & t,
+                    const unsigned int file_version)
+            {
+                typedef boost::multi_array<T, 1 > multi_array_;
+                typename multi_array_::size_type shape_0 = t.shape()[0];
+                ar << BOOST_SERIALIZATION_NVP(shape_0);
+                ar << make_array(t.data(), t.num_elements());
+            }
+        template<typename T, typename Archive>
+            void
             save(Archive & ar, const boost::multi_array<T, 2 > & t,
+                    const unsigned int file_version)
+            {
+                typedef boost::multi_array<T, 2 > multi_array_;
+                typename multi_array_::size_type shape_0 = t.shape()[0];
+                typename multi_array_::size_type shape_1 = t.shape()[1];
+                ar << BOOST_SERIALIZATION_NVP(shape_0);
+                ar << BOOST_SERIALIZATION_NVP(shape_1);
+                ar << make_array(t.data(), t.num_elements());
+            }
+        template<typename T, typename Archive>
+            void
+            save(Archive & ar, const boost::multi_array_ref<T, 2 > & t,
                     const unsigned int file_version)
             {
                 typedef boost::multi_array<T, 2 > multi_array_;
@@ -66,7 +100,21 @@ namespace boost
             }
         template<typename T, class Archive>
             void
+            serialize(Archive & ar, boost::multi_array_ref<T, 1 > & t,
+                    const unsigned int file_version)
+            {
+                split_free(ar, t, file_version);
+            }
+        template<typename T, class Archive>
+            void
             serialize(Archive & ar, boost::multi_array<T, 2 > & t,
+                    const unsigned int file_version)
+            {
+                split_free(ar, t, file_version);
+            }
+        template<typename T, class Archive>
+            void
+            serialize(Archive & ar, boost::multi_array_ref<T, 2 > & t,
                     const unsigned int file_version)
             {
                 split_free(ar, t, file_version);
