80faf780e04e459ee0321eb1ac8b220024cd6ebe
James Amundson <amundson@fnal.gov>
2010-10-12 14:03:49 -0500

    some error handling for xml_serialization

--- utils/xml_serialization.h
+++ utils/xml_serialization.h
@@ -2,6 +2,8 @@
 #define XML_SERIALIZATION_H_
 
 #include <fstream>
+#include <stdexcept>
+#include <string>
 #include <boost/serialization/nvp.hpp>
 #include <boost/serialization/utility.hpp>
 #include <boost/serialization/version.hpp>
@@ -13,6 +15,11 @@ template<typename T>
     xml_save(T const& object, const char *filename)
     {
         std::ofstream output_stream(filename);
+        if (!output_stream.good()) {
+            std::string message("xml_save: unable to open ");
+            message += filename;
+            throw std::runtime_error(message);
+        }
         boost::archive::xml_oarchive output_archive(output_stream);
         output_archive << BOOST_SERIALIZATION_NVP(object);
         output_stream.close();
@@ -23,6 +30,11 @@ template<typename T>
     xml_load(T & object, const char *filename)
     {
         std::ifstream input_stream(filename);
+        if (!input_stream.good()) {
+            std::string message("xml_load: unable to open ");
+            message += filename;
+            throw std::runtime_error(message);
+        }
         boost::archive::xml_iarchive input_archive(input_stream);
         input_archive >> BOOST_SERIALIZATION_NVP(object);
         input_stream.close();
