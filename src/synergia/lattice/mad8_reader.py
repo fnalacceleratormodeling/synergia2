59219d9c954731d239bcb1e0d8ac0891543f92b1
James Amundson <amundson@fnal.gov>
2010-11-03 13:12:58 -0500

    Add caching for Mad8_reader

--- src/synergia/lattice/mad8_reader.py
+++ src/synergia/lattice/mad8_reader.py
@@ -1,8 +1,88 @@
 #!/usr/bin/env python
+import os, stat
 
 from synergia.lattice import Mad8_parser
 from synergia.lattice import Lattice_element, Element_adaptor_map, Lattice
+from synergia.lattice import xml_save_lattice, xml_load_lattice
 from synergia.foundation import pconstants, Four_momentum, Reference_particle
+
+class Cache_entry:
+    def __init__(self, time_stamp, index):
+        self.time_stamp = time_stamp
+        self.index = index
+
+class Lattice_cache:
+    def __init__(self, file_name, line_name):
+        self.file_name = file_name
+        self.line_name = line_name
+        self.cache_dir = os.path.join(os.path.dirname(file_name), "lattice_cache")
+        self.cache_file_name = os.path.join(self.cache_dir,
+                                            os.path.basename(file_name) + "_cache")
+        self.cache = None
+        self.max_index = 0
+
+    def _get_time_stamp(self):
+        stat_output = os.stat(self.file_name)
+        return stat_output[stat.ST_MTIME]
+
+    def _read_cache(self):
+        self.cache = {}
+        if os.path.exists(self.cache_file_name):
+            cache_file = open(self.cache_file_name, 'r')
+            line_name = cache_file.readline().rstrip()
+            while line_name:
+                time_stamp = int(cache_file.readline().rstrip())
+                index = int(cache_file.readline().rstrip())
+                if (index > self.max_index):
+                    self.max_index = index
+                self.cache[line_name] = Cache_entry(time_stamp, index)
+                line_name = cache_file.readline().rstrip()
+            cache_file.close()
+
+    def is_readable(self):
+        if not self.cache:
+            self._read_cache()
+        readable = False
+        exists = self.cache.has_key(self.line_name)
+        if exists:
+            readable = self.cache[self.line_name].time_stamp == self._get_time_stamp()
+        return readable
+
+    def _xml_file_name(self, index):
+        return self.cache_file_name + ("_xml%d" % index)
+
+    def read(self):
+        retval = None
+        if self.is_readable():
+            retval = Lattice()
+            index = self.cache[self.line_name].index
+            xml_load_lattice(retval, self._xml_file_name(index))
+        return retval
+
+    def write(self, lattice):
+        if not self.cache:
+            self._read_cache()
+        time_stamp = self._get_time_stamp()
+        index = None
+        need_to_write = True
+        if self.cache.has_key(self.line_name):
+            index = self.cache[self.line_name].index
+            if self.cache[self.line_name].time_stamp == time_stamp:
+                need_to_write = False
+        if index == None:
+            self.max_index += 1
+            index = self.max_index
+        if need_to_write:
+            self.cache[self.line_name] = Cache_entry(time_stamp, index)
+            if not os.path.exists(self.cache_dir):
+                os.mkdir(self.cache_dir)
+            cache_file = open(self.cache_file_name, 'w')
+            for line_name in self.cache.keys():
+                cache_file.write(line_name + '\n')
+                cache_file.write('%d\n' % self.cache[line_name].time_stamp)
+                cache_file.write('%d\n' % self.cache[line_name].index)
+            cache_file.close()
+            xml_save_lattice(lattice, self._xml_file_name(index))
 
 class Mad8_reader:
     def __init__(self, element_adaptor_map=None):
@@ -155,11 +235,18 @@ class Mad8_reader:
                 reference_particle = Reference_particle(charge, four_momentum)
                 lattice.set_reference_particle(reference_particle)
 
-    def get_lattice(self, line_name, filename=None):
-        self._parser_check(filename, "get_lattice")
-        lattice = Lattice(line_name)
-        self._extract_elements(self.parser.lines[line_name], [line_name], lattice)
-        self._extract_reference_particle(lattice)
+    def get_lattice(self, line_name, filename=None, enable_cache_write=True,
+                    enable_cache_read=True):
+        lattice_cache = Lattice_cache(filename, line_name)
+        if enable_cache_read and lattice_cache.is_readable():
+            lattice = lattice_cache.read()
+        else:
+            self._parser_check(filename, "get_lattice")
+            lattice = Lattice(line_name)
+            self._extract_elements(self.parser.lines[line_name], [line_name], lattice)
+            self._extract_reference_particle(lattice)
+            if enable_cache_write:
+                lattice_cache.write(lattice)
         return lattice
 
 if __name__ == "__main__":
