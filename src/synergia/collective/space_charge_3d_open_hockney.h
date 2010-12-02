52a65a2df054342de1b1671eb9e32069aff537a9
James Amundson <amundson@fnal.gov>
2010-12-02 14:30:55 -0600

    need to include Step definition in collective operators

--- src/synergia/collective/space_charge_3d_open_hockney.h
+++ src/synergia/collective/space_charge_3d_open_hockney.h
@@ -1,6 +1,7 @@
 #ifndef SPACE_CHARGE_3D_OPEN_HOCKNEY_H_
 #define SPACE_CHARGE_3D_OPEN_HOCKNEY_H_
 #include "synergia/simulation/operator.h"
+#include "synergia/simulation/step.h"
 #include "synergia/bunch/bunch.h"
 #include "synergia/collective/rectangular_grid_domain.h"
 #include "synergia/collective/rectangular_grid.h"
