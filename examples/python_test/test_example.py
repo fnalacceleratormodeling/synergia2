6ca15d10ddbb3213014718d36a10ecbf5b9d39cb
James Amundson <amundson@fnal.gov>
2010-12-03 14:54:43 -0600

    use real space charge

--- examples/python_test/test_example.py
+++ examples/python_test/test_example.py
@@ -13,13 +13,13 @@ stdz = 0.01
 dpop = 1e-4
 
 lattice = synergia.lattice.Mad8_reader().get_lattice("fodo", "fodo.lat")
-space_charge = synergia.simulation.Collective_operator("space charge")
 lattice_simulator = synergia.simulation.Lattice_simulator(lattice, map_order)
-stepper = synergia.simulation.Split_operator_stepper(lattice_simulator, space_charge,
-                                          num_steps)
 bunch = synergia.optics.generate_matched_bunch_transverse(lattice_simulator, emit, emit, stdz, dpop,
                                         num_real_particles, num_macro_particles,
                                         seed=seed)
+space_charge = synergia.collective.Space_charge_3d_open_hockney(grid, False, bunch.get_comm())
+stepper = synergia.simulation.Split_operator_stepper(lattice_simulator, space_charge,
+                                          num_steps)
 diagnostics_writer_step = synergia.bunch.Diagnostics_writer("example_full2.h5",
                                                             synergia.bunch.Diagnostics_full2())
 diagnostics_writer_turn = synergia.bunch.Diagnostics_writer("example_particles.h5",
