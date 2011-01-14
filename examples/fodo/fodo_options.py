6072d0fdd465f6b37690fe330b0fbd6524b97613
James Amundson <amundson@fnal.gov>
2011-01-14 15:49:03 -0600

    clean up fodo options

--- examples/fodo/fodo_options.py
+++ examples/fodo/fodo_options.py
@@ -14,21 +14,5 @@ opts.add("emit", 1e-6, "Horizontal and vertical emittance [m rad]", float)
 opts.add("stdz", 0.01, "RMS longitudinal length [m]", float)
 opts.add("dpop", 1e-4, "delta p / p", float)
 
-#opts.add(")
-#opts.add("gridnum", 24, "number of grid points to be used for all directions", int)
-#opts.add("solver", "3d", "solver", str)
-#opts.add("xoffset", 0.0, "x offset", float)
-#opts.add("impedance", 0, "whether to use resistive wall kicks", int)
-#opts.add("piperadius", 0.01, "pipe radius for impedance", float)
-#opts.add("pipeconduct", 1.4e6,
-#    "conductivity for pipe [/s], default is for stainless steel", float)
-#opts.add("spacecharge", 1, "whether to use space charge kicks", int)
-#opts.add("np", 2.0e11, "number of particles in real bunch", float)
-#opts.add("partpercell", 4, "particles per grid cell", float)
-#opts.add("xwidth", 0.004, "initial horizontal beam width in meters", float)
-#opts.add("kickspercell", 10, "space-charge kicks per cell", int)
-#opts.add("dpop", 1.0e-4, "delta p/p", float)
-
-job_mgr = synergia_workflow.Job_manager("fodo.py", opts, ["fodo.lat",
-                                                 "diagnostics_file.py"])
+job_mgr = synergia_workflow.Job_manager("fodo.py", opts, ["fodo.lat"])
 
