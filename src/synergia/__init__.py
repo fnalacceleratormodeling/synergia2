baa511ae29ea5d9dcf991c4f5f621d2945248fde
James Amundson <amundson@fnal.gov>
2011-02-28 17:29:02 -0600

    add mpi4py imports to synergia's __init__.py

--- src/synergia/__init__.py
+++ src/synergia/__init__.py
@@ -1,3 +1,8 @@
+# importing mpi4py here makes HopperII happy; there doesn't seem to be
+# anything wrong with doing so.
+import mpi4py
+from mpi4py import MPI
+
 from version import __version__, major_version, minor_version, subminor_version
 import convertors
 import bunch
