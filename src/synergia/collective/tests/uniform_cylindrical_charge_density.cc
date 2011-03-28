e09031a49f43adc459e1be31a1dda10bcaab7c6d
James Amundson <amundson@fnal.gov>
2011-03-28 14:23:33 -0500

    first test for uniform cylinder

---
+++ src/synergia/collective/tests/uniform_cylindrical_charge_density.cc
@@ -0,0 +1,50 @@
+#include "uniform_cylindrical_charge_density.h"
+#include "synergia/foundation/physical_constants.h"
+#include <cmath>
+
+double
+uniform_cylindrical_charge_density(double lambda, double r, double r0)
+{
+    double retval;
+    if (r < r0) {
+        retval = lambda / (mconstants::pi * r0 * r0);
+    } else {
+        retval = 0.0;
+    }
+    return retval;
+}
+
+// Electric potential due to cylindrical uniform charge density [V]
+double
+uniform_cylindrical_electric_potential(double lambda, double r, double r0)
+{
+    double retval;
+    if (r < r0) {
+        retval = -lambda / (4 * mconstants::pi * pconstants::epsilon0) * (r * r
+                / (r0 * r0) + 2 * std::log(r0) - 1);
+    } else {
+        retval = -lambda / (2 * mconstants::pi * pconstants::epsilon0)
+                * std::log(r);
+    }
+    return retval;
+}
+
+// Transverse electric field component due to cylindrical uniform
+// charge density [V]
+// Since both x- and y-components are the same, we don't pretend to distinguish
+// between them.
+double
+uniform_cylindrical_electric_field_component(double lambda, double r,
+        double r0, double x)
+{
+    double retval;
+    //    if (r < r0) {
+    //        retval = lambda / (mconstants::pi * pconstants::epsilon0) * (-1.0 / (12
+    //                * r0 * r0 * r0) * (18 * r * r0 - 12 * r * r)) * x / r;
+    //    } else {
+    //        retval = -lambda / (2 * mconstants::pi * pconstants::epsilon0)
+    //                * (1 / r) * x / r;
+    //    }
+    return retval;
+}
+
---
+++ src/synergia/collective/tests/uniform_cylindrical_charge_density.h
@@ -0,0 +1,20 @@
+#ifndef UNIFORM_CYLINDRICAL_CHARGE_DENSITY_H_
+#define UNIFORM_CYLINDRICAL_CHARGE_DENSITY_H_
+
+// Cylindrical uniform charge density [C/m^3]
+double
+uniform_cylindrical_charge_density(double lambda, double r, double r0);
+
+// Electric potential due to cylindrical uniform charge density [V]
+double
+uniform_cylindrical_electric_potential(double lambda, double r, double r0);
+
+// Transverse electric field component due to cylindrical uniform
+// charge density [V]
+// Since both x- and y-components are the same, we don't pretend to distinguish
+// between them.
+double
+uniform_cylindrical_electric_field_component(double lambda, double r, double r0,
+        double x);
+
+#endif /* UNIFORM_CYLINDRICAL_CHARGE_DENSITY_H_ */
