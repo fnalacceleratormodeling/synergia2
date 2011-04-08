37da8466c451ddbccda565c4cfceea12d67c85e3
James Amundson <amundson@fnal.gov>
2011-04-08 10:39:39 -0500

    much stronger tests for Independent_stepper_elements

--- src/synergia/simulation/tests/test_independent_stepper_elements.cc
+++ src/synergia/simulation/tests/test_independent_stepper_elements.cc
@@ -7,10 +7,11 @@
 BOOST_GLOBAL_FIXTURE(MPI_fixture)
 
 const double tolerance = 1.0e-12;
+const int map_order = 1;
 
 BOOST_FIXTURE_TEST_CASE(construct, Lattice_fixture2)
 {
-    Lattice_simulator lattice_simulator(lattice_sptr, 2);
+    Lattice_simulator lattice_simulator(lattice_sptr, map_order);
 
     const int steps_per_element = 1;
     Independent_stepper_elements stepper(lattice_simulator, steps_per_element);
@@ -20,7 +21,7 @@ BOOST_FIXTURE_TEST_CASE(construct, Lattice_fixture2)
 
 BOOST_FIXTURE_TEST_CASE(construct_bad, Lattice_fixture2)
 {
-    Lattice_simulator lattice_simulator(lattice_sptr, 2);
+    Lattice_simulator lattice_simulator(lattice_sptr, map_order);
 
     const int steps_per_element = 0;
     bool caught_error = false;
@@ -36,7 +37,7 @@ BOOST_FIXTURE_TEST_CASE(construct_bad, Lattice_fixture2)
 
 BOOST_FIXTURE_TEST_CASE(construct2, Lattice_fixture2)
 {
-    Lattice_simulator lattice_simulator(lattice_sptr, 2);
+    Lattice_simulator lattice_simulator(lattice_sptr, map_order);
 
     const int steps_per_element = 2;
     Independent_stepper_elements stepper(lattice_simulator, steps_per_element);
@@ -47,7 +48,7 @@ BOOST_FIXTURE_TEST_CASE(construct2, Lattice_fixture2)
 
 BOOST_FIXTURE_TEST_CASE(construct17, Lattice_fixture2)
 {
-    Lattice_simulator lattice_simulator(lattice_sptr, 2);
+    Lattice_simulator lattice_simulator(lattice_sptr, map_order);
 
     const int steps_per_element = 17;
     Independent_stepper_elements stepper(lattice_simulator, steps_per_element);
@@ -56,9 +57,92 @@ BOOST_FIXTURE_TEST_CASE(construct17, Lattice_fixture2)
             (lattice_sptr->get_elements().size()-1)*steps_per_element + 1);
 }
 
+BOOST_FIXTURE_TEST_CASE(get_steps, Lattice_fixture)
+{
+    Lattice_simulator lattice_simulator(lattice_sptr, map_order);
+
+    Independent_stepper stepper100(lattice_simulator, 100);
+
+    BOOST_CHECK_EQUAL(stepper100.get_steps().size(), 100);
+}
+
+void
+verify_steps(Independent_stepper_elements & stepper, int slices_per_element)
+{
+    const double end = -1;
+    double last_right = end;
+    for (Steps::iterator sit = stepper.get_steps().begin(); sit
+            != stepper.get_steps().end(); ++sit) {
+        Operators operators((*sit)->get_operators());
+        // Test 1: only one operator per step
+        BOOST_CHECK(operators.size() == 1);
+        for (Operators::iterator oit = operators.begin(); oit
+                != operators.end(); ++oit) {
+            // Test 2: the operator should be an Independent_operator
+            BOOST_CHECK((*oit)->get_type() == "independent");
+            Lattice_element_slices slices(boost::static_pointer_cast<
+                    Independent_operator >(*oit)->get_slices());
+            // Test 3: only one slice per operator
+            BOOST_CHECK(slices.size() == 1);
+            for (Lattice_element_slices::iterator slit = slices.begin(); slit
+                    != slices.end(); ++slit) {
+                double slice_length = (*slit)->get_right()
+                        - (*slit)->get_left();
+                // Test 4: slices should have the correct length
+                const double slice_length_tolerance = 1.0e-12;
+                BOOST_CHECK_CLOSE(slice_length,
+                        (*slit)->get_lattice_element().get_length() / slices_per_element,
+                        slice_length_tolerance);
+                // Test 5: slices should be continuous
+                double slice_left = (*slit)->get_left();
+                if (last_right == end) {
+                    BOOST_CHECK(slice_left == 0.0);
+                } else {
+                    const double edge_tolerance = 1.0e-12;
+                    BOOST_CHECK_CLOSE(last_right, slice_left, edge_tolerance);
+                }
+                if ((*slit)->has_right_edge()) {
+                    last_right = end;
+                } else {
+                    last_right = (*slit)->get_right();
+                }
+            }
+        }
+    }
+    // Test 6: we should end at the end of an element
+    BOOST_CHECK(last_right == end);
+}
+
+BOOST_FIXTURE_TEST_CASE(verify_steps1, Lattice_fixture)
+{
+    Lattice_simulator lattice_simulator(lattice_sptr, map_order);
+    const int num_steps = 1;
+    Independent_stepper_elements stepper(lattice_simulator, num_steps);
+
+    verify_steps(stepper, num_steps);
+}
+
+BOOST_FIXTURE_TEST_CASE(verify_steps2, Lattice_fixture)
+{
+    Lattice_simulator lattice_simulator(lattice_sptr, map_order);
+    const int num_steps = 2;
+    Independent_stepper_elements stepper(lattice_simulator, num_steps);
+
+    verify_steps(stepper, num_steps);
+}
+
+BOOST_FIXTURE_TEST_CASE(verify_steps17, Lattice_fixture)
+{
+    Lattice_simulator lattice_simulator(lattice_sptr, map_order);
+    const int num_steps = 17;
+    Independent_stepper_elements stepper(lattice_simulator, num_steps);
+
+    verify_steps(stepper, num_steps);
+}
+
 BOOST_FIXTURE_TEST_CASE(has_sliced_chef_beamline, Lattice_fixture2)
 {
-    Lattice_simulator lattice_simulator(lattice_sptr, 2);
+    Lattice_simulator lattice_simulator(lattice_sptr, map_order);
 
     const int steps_per_element = 1;
     Independent_stepper_elements stepper(lattice_simulator, steps_per_element);
