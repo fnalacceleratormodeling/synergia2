f9a0f80a30766da726aba75eb58b71facc23bed4
James Amundson <amundson@fnal.gov>
2011-04-12 15:01:09 -0500

    fix tests to account for zero-length steps in Split_operator_stepper_elements

--- src/synergia/simulation/tests/test_split_operator_stepper_elements.cc
+++ src/synergia/simulation/tests/test_split_operator_stepper_elements.cc
@@ -71,61 +71,72 @@ BOOST_FIXTURE_TEST_CASE(construct17, Lattice_fixture2)
 void
 verify_steps(Split_operator_stepper_elements & stepper, int steps_per_element)
 {
-    // jfa: remove print command after debugging
-    stepper.print();
     const double end = -1;
     double last_right = end;
     int slices_per_element = 2 * steps_per_element;
     for (Steps::iterator sit = stepper.get_steps().begin(); sit
             != stepper.get_steps().end(); ++sit) {
         Operators operators((*sit)->get_operators());
-        // Test 1: three operators per step
-        BOOST_CHECK(operators.size() == 3);
-        int operator_index = 0;
-        for (Operators::iterator oit = operators.begin(); oit
-                != operators.end(); ++oit) {
-            ++operator_index;
-            // Test 2: operator should be of the right type
-            if ((operator_index == 1) || (operator_index == 3)) {
-                BOOST_CHECK((*oit)->get_type() == "independent");
-            } else {
-                BOOST_CHECK((*oit)->get_type() == "collective");
+        if ((*sit)->get_length() == 0.0) {
+            // Zero-length split operator steps are special
+            for (Operators::iterator oit = operators.begin(); oit
+                    != operators.end(); ++oit) {
+                // Test 1a: zero-length steps should consist of one or more
+                //          independent operators
+                BOOST_CHECK ((*oit)->get_type() == "independent");
+                // Test 2a: operator should be named "step"
+                BOOST_CHECK ((*oit)->get_name() == "step");
             }
-            // Test 3: operators should have the correct names
-            if (operator_index == 1) {
-                BOOST_CHECK ((*oit)->get_name() == "first_half");
-            } else if (operator_index == 2) {
-                BOOST_CHECK ((*oit)->get_name() == "space_charge");
-            } else if (operator_index == 3) {
-                BOOST_CHECK ((*oit)->get_name() == "second_half");
-            }
-            if ((*oit)->get_type() == "independent") {
-                double substep_length = 0.0;
-                Lattice_element_slices slices(boost::static_pointer_cast<
-                        Independent_operator >(*oit)->get_slices());
-                // Test 4: only one slice per operator
-                BOOST_CHECK(slices.size() == 1);
-                for (Lattice_element_slices::iterator slit = slices.begin(); slit
-                        != slices.end(); ++slit) {
-                    double slice_length = (*slit)->get_right()
-                            - (*slit)->get_left();
-                    // Test 5: slices should have the correct length
-                    const double slice_length_tolerance = 1.0e-12;
-                    BOOST_CHECK_CLOSE(slice_length,
-                            (*slit)->get_lattice_element().get_length() / slices_per_element,
-                            slice_length_tolerance);
-                    // Test 6: slices should be continuous
-                    double slice_left = (*slit)->get_left();
-                    if (last_right == end) {
-                        BOOST_CHECK(slice_left == 0.0);
-                    } else {
-                        const double edge_tolerance = 1.0e-12;
-                        BOOST_CHECK_CLOSE(last_right, slice_left, edge_tolerance);
-                    }
-                    if ((*slit)->has_right_edge()) {
-                        last_right = end;
-                    } else {
-                        last_right = (*slit)->get_right();
+        } else {
+            // Nonzero-length split operator steps are the ordinary case
+            // Test 1b: three operators per step
+            BOOST_CHECK(operators.size() == 3);
+            int operator_index = 0;
+            for (Operators::iterator oit = operators.begin(); oit
+                    != operators.end(); ++oit) {
+                ++operator_index;
+                // Test 2b: operator should be of the right type
+                if ((operator_index == 1) || (operator_index == 3)) {
+                    BOOST_CHECK((*oit)->get_type() == "independent");
+                } else {
+                    BOOST_CHECK((*oit)->get_type() == "collective");
+                }
+                // Test 3b: operators should have the correct names
+                if (operator_index == 1) {
+                    BOOST_CHECK ((*oit)->get_name() == "first_half");
+                } else if (operator_index == 2) {
+                    BOOST_CHECK ((*oit)->get_name() == "space_charge");
+                } else if (operator_index == 3) {
+                    BOOST_CHECK ((*oit)->get_name() == "second_half");
+                }
+                if ((*oit)->get_type() == "independent") {
+                    double substep_length = 0.0;
+                    Lattice_element_slices slices(boost::static_pointer_cast<
+                            Independent_operator >(*oit)->get_slices());
+                    // Test 4b: only one slice per operator
+                    BOOST_CHECK(slices.size() == 1);
+                    for (Lattice_element_slices::iterator slit = slices.begin(); slit
+                            != slices.end(); ++slit) {
+                        double slice_length = (*slit)->get_right()
+                                - (*slit)->get_left();
+                        // Test 5b: slices should have the correct length
+                        const double slice_length_tolerance = 1.0e-12;
+                        BOOST_CHECK_CLOSE(slice_length,
+                                (*slit)->get_lattice_element().get_length() / slices_per_element,
+                                slice_length_tolerance);
+                        // Test 6b: slices should be continuous
+                        double slice_left = (*slit)->get_left();
+                        if (last_right == end) {
+                            BOOST_CHECK(slice_left == 0.0);
+                        } else {
+                            const double edge_tolerance = 1.0e-12;
+                            BOOST_CHECK_CLOSE(last_right, slice_left, edge_tolerance);
+                        }
+                        if ((*slit)->has_right_edge()) {
+                            last_right = end;
+                        } else {
+                            last_right = (*slit)->get_right();
+                        }
                     }
                 }
             }
