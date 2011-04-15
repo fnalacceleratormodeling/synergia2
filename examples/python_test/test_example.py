4ca582529aac776c28458d9dce2678b262d8fd31
James Amundson <amundson@fnal.gov>
2011-04-15 14:00:11 -0500

    update test_example.py for new diagnostics and space charge interfaces

--- examples/python_test/test_example.py
+++ examples/python_test/test_example.py
@@ -17,12 +17,10 @@ lattice_simulator = synergia.simulation.Lattice_simulator(lattice, map_order)
 bunch = synergia.optics.generate_matched_bunch_transverse(lattice_simulator, emit, emit, stdz, dpop,
                                         num_real_particles, num_macro_particles,
                                         seed=seed)
-space_charge = synergia.collective.Space_charge_3d_open_hockney(grid, False, bunch.get_comm())
+space_charge = synergia.collective.Space_charge_3d_open_hockney(bunch.get_comm(), grid)
 stepper = synergia.simulation.Split_operator_stepper(lattice_simulator, space_charge,
                                           num_steps)
-diagnostics_writer_step = synergia.bunch.Diagnostics_writer("example_full2.h5",
-                                                            synergia.bunch.Diagnostics_full2())
-diagnostics_writer_turn = synergia.bunch.Diagnostics_writer("example_particles.h5",
-                                                            synergia.bunch.Diagnostics_particles())
+diagnostics_step = synergia.bunch.Diagnostics_full2(bunch, "example_full2.h5")
+diagnostics_turn = synergia.bunch.Diagnostics_particles(bunch, "example_particles.h5")
 propagator = synergia.simulation.Propagator(stepper)
-propagator.propagate(bunch, num_turns, diagnostics_writer_step, diagnostics_writer_turn)
+propagator.propagate(bunch, num_turns, diagnostics_step, diagnostics_turn)
