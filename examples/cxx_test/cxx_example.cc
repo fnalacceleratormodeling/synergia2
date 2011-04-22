a179f480f1fa32f2da817e078e0e67aa77ad5070
James Amundson <amundson@fnal.gov>
2011-04-22 16:47:50 -0500

    use real space charge in cxx_example

--- examples/cxx_test/cxx_example.cc
+++ examples/cxx_test/cxx_example.cc
@@ -11,16 +11,22 @@
 #include "synergia/foundation/distribution.h"
 #include "synergia/bunch/populate.h"
 #include "synergia/bunch/diagnostics.h"
+#include "synergia/collective/space_charge_3d_open_hockney.h"
 
-int
-main(int argc, char **argv)
+// We put the actual code in a separate function so that shared_ptr's can
+// be cleanup up properly before we call MPI_Finalize.
+void
+run()
 {
-    MPI_Init(&argc, &argv);
-
-    const int num_macro_particles = 32000;
+    std::vector<int > grid_shape(3);
+    grid_shape[0] = 16;
+    grid_shape[1] = 16;
+    grid_shape[2] = 16;
+    const int part_per_cell = 10;
+    const int num_macro_particles = grid_shape[0] * grid_shape[1]
+            * grid_shape[2] * part_per_cell;
     const int seed = 4;
-    //    grid = [16, 16, 16]
-    const double num_real_particles = 1e12;
+    const double num_real_particles = 1e13;
     const int num_steps = 8;
     const int num_turns = 4;
     const int map_order = 2;
@@ -37,8 +43,8 @@ main(int argc, char **argv)
         std::cerr << "Run cxx_example.py to generate cxx_lattice.xml\n";
         exit(1);
     }
-    Dummy_collective_operator_sptr space_charge_sptr(
-            new Dummy_collective_operator("space_charge"));
+    Space_charge_3d_open_hockney_sptr space_charge_sptr(
+            new Space_charge_3d_open_hockney(Commxx(), grid_shape));
     Lattice_simulator lattice_simulator(lattice_sptr, map_order);
     Split_operator_stepper_sptr stepper_sptr(new Split_operator_stepper(
             lattice_simulator, space_charge_sptr, num_steps));
@@ -59,8 +65,14 @@ main(int argc, char **argv)
     Diagnostics_full2 per_turn_diagnostics(bunch_sptr,
             "cxx_example_per_turn.h5");
     propagator.propagate(*bunch_sptr, num_turns, per_step_diagnostics,
-            per_turn_diagnostics);
+            per_turn_diagnostics, true);
 
+}
+int
+main(int argc, char **argv)
+{
+    MPI_Init(&argc, &argv);
+    run();
     MPI_Finalize();
     return 0;
 }
