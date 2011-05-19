38875628a3257dec6b5b9cf90b847a817cd94207
James Amundson <amundson@fnal.gov>
2011-05-19 14:05:54 -0500

    inline Rectangular_grid_domain::get_leftmost_indices_offsets

--- src/synergia/collective/rectangular_grid_domain.h
+++ src/synergia/collective/rectangular_grid_domain.h
@@ -2,6 +2,7 @@
 #define RECTANGULAR_GRID_DOMAIN_H_
 
 #include <vector>
+#include <cmath>
 #include <boost/shared_ptr.hpp>
 
 class Rectangular_grid_domain
@@ -29,9 +30,25 @@ public:
     get_cell_size() const;
     bool
     is_periodic() const;
-    void
+    inline void
     get_leftmost_indices_offsets(double x, double y, double z, int &ix,
-            int &iy, int&iz, double &offx, double &offy, double &offz) const;
+            int &iy, int&iz, double &offx, double &offy, double &offz) const
+    {
+        double scaled_location;
+
+        scaled_location = (x - left[0]) / cell_size[0] - 0.5;
+        ix = static_cast<int > (floor(scaled_location));
+        offx = scaled_location - ix;
+
+        scaled_location = (y - left[1]) / cell_size[1] - 0.5;
+        iy = static_cast<int > (floor(scaled_location));
+        offy = scaled_location - iy;
+
+        scaled_location = (z - left[2]) / cell_size[2] - 0.5;
+        iz = static_cast<int > (floor(scaled_location));
+        offz = scaled_location - iz;
+    }
+
     void
     get_cell_coordinates(int ix, int iy, int iz, double & x, double & y,
             double & z) const;
