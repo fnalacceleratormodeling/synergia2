bb498e7eccdb522dc72a8d89d6e8ca94377cd52d
James Amundson <amundson@fnal.gov>
2011-07-27 10:08:28 -0500

    more extensive test

--- src/synergia/utils/hdf5_mem.cc
+++ src/synergia/utils/hdf5_mem.cc
@@ -3,17 +3,17 @@
 #include "hdf5_serial_writer.h"
 
 void
-doit0()
+doit0(const char *filename)
 {
     H5::H5File file("hdf5_mem0.h5", H5F_ACC_TRUNC);
-//    Hdf5_serial_writer<int > writer(file, "i");
-//    file.close();
+    Hdf5_serial_writer<int > writer(file, "i");
+    file.close();
 }
 
 void
-doit1e5()
+doit1e5(const char *filename)
 {
-    H5::H5File file("hdf5_mem1e5.h5", H5F_ACC_TRUNC);
+    H5::H5File file(filename, H5F_ACC_TRUNC);
     Hdf5_serial_writer<int > writer(file, "i");
     for (int i = 0; i < 100000; ++i) {
         writer.append(i);
@@ -27,20 +27,26 @@ main()
     Simple_memory_tracker memtracker;
     memtracker.print("start (absolute usage)");
 
-    doit0();
+    doit0("10.h5");
     memtracker.print("after doit0 (relative)");
 
-    doit0();
+    doit0("11.h5");
     memtracker.print("after doit0 again (relative)");
 
-    doit0();
+    doit0("12.h5");
     memtracker.print("after doit0 yet again (relative)");
 
-    doit1e5();
+    doit1e5("20.h5");
     memtracker.print("after doit1e5 (relative)");
 
-    doit1e5();
+    doit1e5("21.h5");
     memtracker.print("after doit1e5 again (relative)");
 
-    memtracker.print("end (relative)");
+    doit1e5("22.h5");
+    memtracker.print("after doit1e5 again (relative)");
+
+    doit1e5("23.h5");
+    memtracker.print("after doit1e5 again (relative)");
+
+    memtracker.print("end (absolute)", false);
 }
