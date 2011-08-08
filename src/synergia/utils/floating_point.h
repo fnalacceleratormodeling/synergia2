f6001f0e84e7486e355ff590f5e69e61402d9b14
Chong Shik Park <cspark@fnal.gov>
2011-08-08 18:48:35 -0500

    utils/floating_point

--- src/synergia/utils/floating_point.h
+++ src/synergia/utils/floating_point.h
@@ -2,6 +2,7 @@
 #define FLOATING_POINT_H_
 
 #include <cmath>
+#include <complex>
 
 /// check a = b within the given tolerance
 inline bool
@@ -19,6 +20,18 @@ inline bool
 floating_point_leq(double a, double b, double tolerance)
 {
     return ((b - a) > -tolerance);
+}
+
+/// check a = b within the given tolerance for complex values
+inline bool
+complex_floating_point_equal(std::complex<double > a, std::complex<double > b,
+        double tolerance)
+{
+    if (std::abs(a) < tolerance) {
+        return (std::abs(a - b) < tolerance);
+    } else {
+        return (std::abs((a - b) / a) < tolerance);
+    }
 }
 
 #endif /* FLOATING_POINT_H_ */
--- src/synergia/utils/tests/test_floating_point.cc
+++ src/synergia/utils/tests/test_floating_point.cc
@@ -7,6 +7,14 @@ const double small = 1.0e-14;
 const double medium = 1.0;
 const double large = 1.0e20;
 
+const std::complex<double > tolerance_complex = std::complex<double >(1.0e-12, 
+        1.0e-12);
+const std::complex<double > small_complex = std::complex<double >(1.0e-14, 
+        1.0e-14); 
+const std::complex<double > medium_complex = std::complex<double >(1.0, 1.0);
+const std::complex<double > large_complex = std::complex<double >(1.0e20, 
+        1.0e20);
+
 BOOST_AUTO_TEST_CASE(equal_medium)
 {
     BOOST_CHECK(floating_point_equal(medium, medium+0.5*tolerance, tolerance));
@@ -42,3 +50,52 @@ BOOST_AUTO_TEST_CASE(equal_medium_negative)
     BOOST_CHECK(floating_point_equal(-medium, -medium+0.5*tolerance, tolerance));
 }
 
+BOOST_AUTO_TEST_CASE(equal_medium_complex1)
+{
+    BOOST_CHECK(complex_floating_point_equal(medium_complex, medium_complex+std::complex<double >(0.5, 0.0)*tolerance_complex, tolerance));
+}
+
+BOOST_AUTO_TEST_CASE(equal_medium_complex2)
+{
+    BOOST_CHECK(complex_floating_point_equal(medium_complex, medium_complex+std::complex<double >(0.0, 0.5)*tolerance_complex, tolerance));
+}
+
+BOOST_AUTO_TEST_CASE(equal_large_complex1)
+{
+    BOOST_CHECK(complex_floating_point_equal(large_complex, large_complex+std::complex<double >(0.5, 0.0)*large_complex*tolerance_complex, tolerance));
+}
+
+BOOST_AUTO_TEST_CASE(equal_large_complex2)
+{
+    BOOST_CHECK(complex_floating_point_equal(large_complex, large_complex+std::complex<double >(0.0, 0.5)*large_complex*tolerance_complex, tolerance));
+}
+
+BOOST_AUTO_TEST_CASE(equal_small_complex1)
+{
+    BOOST_CHECK(complex_floating_point_equal(small_complex, small_complex*std::complex<double >(0.5, 0.0), tolerance));
+}
+
+BOOST_AUTO_TEST_CASE(equal_small_complex2)
+{
+    BOOST_CHECK(complex_floating_point_equal(small_complex, small_complex*std::complex<double >(0.0, 0.5), tolerance));
+}
+
+BOOST_AUTO_TEST_CASE(not_equal_medium_complex)
+{
+    BOOST_CHECK(!complex_floating_point_equal(medium_complex, medium_complex+std::complex<double >(2.0, 2.0)*tolerance_complex, tolerance));
+}
+
+BOOST_AUTO_TEST_CASE(not_equal_large_complex)
+{
+    BOOST_CHECK(!complex_floating_point_equal(large_complex, large_complex+std::complex<double >(2.0, 2.0)*large_complex*tolerance_complex, tolerance));
+}
+
+BOOST_AUTO_TEST_CASE(not_equal_small_complex)
+{
+    BOOST_CHECK(!complex_floating_point_equal(small_complex, small_complex+std::complex<double >(2.0, 2.0)*tolerance_complex, tolerance));
+}
+
+BOOST_AUTO_TEST_CASE(equal_medium_complex_negative)
+{
+    BOOST_CHECK(complex_floating_point_equal(-medium_complex, -medium_complex+std::complex<double >(0.5, 0.5)*tolerance_complex, tolerance));
+}
