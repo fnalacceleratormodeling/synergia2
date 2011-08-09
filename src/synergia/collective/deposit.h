62b242e3f8362e14c91267931761c8914bd1f6b6
Chong Shik Park <cspark@fnal.gov>
2011-08-08 19:04:27 -0500

    collective/deposit

--- src/synergia/collective/deposit.h
+++ src/synergia/collective/deposit.h
@@ -7,4 +7,8 @@ void
 deposit_charge_rectangular_zyx(Rectangular_grid & rho_grid, Bunch const& bunch,
         bool zero_first = true);
 
+void
+deposit_charge_rectangular_2d(Rectangular_grid & rho_grid, Bunch const& bunch,
+        bool zero_first = true);
+
 #endif /* DEPOSIT_H_ */
