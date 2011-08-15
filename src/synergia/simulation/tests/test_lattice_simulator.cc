bdd7963c2d20a72bbb930a91042122f8f889b904
James Amundson <amundson@fnal.gov>
2011-08-15 15:03:22 -0500

    Start of tests for Lattice_simulator::calculate_lattice_functions

--- src/synergia/simulation/tests/test_lattice_simulator.cc
+++ src/synergia/simulation/tests/test_lattice_simulator.cc
@@ -73,3 +73,22 @@ BOOST_FIXTURE_TEST_CASE(get_chef_lattice_sptr, Lattice_fixture)
     Lattice_simulator lattice_simulator(lattice_sptr, map_order);
     lattice_simulator.get_chef_lattice_sptr();
 }
+
+BOOST_FIXTURE_TEST_CASE(calculate_lattice_functions, Fobodobo_sbend_fixture)
+{
+    Lattice_simulator lattice_simulator(lattice_sptr, map_order);
+    Lattice_element_slices slices;
+    for (Lattice_elements::const_iterator it =
+            lattice_sptr->get_elements().begin(); it
+            != lattice_sptr->get_elements().end(); ++it) {
+        double length = (*it)->get_length();
+        Lattice_element_slice_sptr first_half(new Lattice_element_slice(*(*it),
+                0.0, 0.5 * length));
+        Lattice_element_slice_sptr second_half(new Lattice_element_slice(
+                *(*it), 0.5 * length, length));
+        slices.push_back(first_half);
+        slices.push_back(second_half);
+    }
+    lattice_simulator.construct_sliced_chef_beamline(slices);
+    lattice_simulator.calculate_lattice_functions();
+}
