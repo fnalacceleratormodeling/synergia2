6ac2fc651c9a1a3f41c82c3404542e4a288ea36d
Alexandru Macridin <macridin@gmail.com>
2011-10-18 16:31:33 -0500

    try and catch errors in propgate

--- src/synergia/simulation/propagator.cc
+++ src/synergia/simulation/propagator.cc
@@ -46,68 +46,74 @@ Propagator::propagate(Bunch_with_diagnostics & bunch_with_diagnostics,
 {
 
 
-
-    double t;
-    double t_turn, t_turn1;
-    int rank = Commxx().get_rank();
-    Bunch_sptr bunch_sptr=bunch_with_diagnostics.get_bunch_sptr();
-   
+try{
+        double t;
+        double t_turn, t_turn1;
+        int rank = Commxx().get_rank();
+        Bunch_sptr bunch_sptr=bunch_with_diagnostics.get_bunch_sptr();
     
-    
-    std::ofstream logfile;
-    if (rank == 0) logfile.open("log");
-    t = simple_timer_current(); 
-    bunch_with_diagnostics.get_diagnostics_actions_sptr()->first_action(*stepper_sptr, *bunch_sptr);
-    t = simple_timer_show(t, "diagnostics_first");
-    general_actions.first_action(*stepper_sptr, *bunch_sptr);
-    t = simple_timer_show(t, "propagate-general_actions");
-    for (int turn = 0; turn < num_turns; ++turn) {
-        t_turn=MPI_Wtime();
-        if (verbose) {
-            if (rank == 0) {
-                std::cout << "Propagator: turn " << turn + 1 << "/"
-                        << num_turns << std::endl;
-            }
-        }
-        bunch_sptr->get_reference_particle().start_repetition();
-        int step_count = 0;
-        int num_steps = stepper_sptr->get_steps().size();             
-        for (Steps::const_iterator it = stepper_sptr->get_steps().begin(); it
-               != stepper_sptr->get_steps().end(); ++it) {
-            ++step_count;
-            if (verbose>0) {
-                if (rank == 0) {
-                   std::cout << "Propagator:   step " << step_count << "/"
-                           << num_steps <<" s= "<<bunch_sptr->get_reference_particle().get_s()<<" trajectory length="<<bunch_sptr->get_reference_particle().get_trajectory_length()<< std::endl;
-                }
-            }      
-             (*it)->apply(*bunch_sptr); 
-             /// apply with diagnostics only for testing purposes
-             //(*it)->apply(*bunch_sptr, bunch_with_diagnostics.get_per_step_diagnostics()); 
-            t = simple_timer_current(); 
-            bunch_with_diagnostics.get_diagnostics_actions_sptr()->step_end_action(*stepper_sptr, *(*it), *bunch_sptr,
-                   turn, step_count); 
-            t = simple_timer_show(t, "diagnostics-step"); 
-            general_actions.step_end_action(*stepper_sptr, *(*it),*bunch_sptr,
-                    turn, step_count);
-            t = simple_timer_show(t, "propagate-general_actions-step");      
-        }
         
-       t_turn1= MPI_Wtime();
-       if (rank == 0) {
-            logfile<<" turn "<<turn + 1<<" : "<< t_turn1-t_turn<< " \n";
-            std::cout<<"  turn "<<turn + 1<<" : "<< t_turn1-t_turn<<std::endl;
-            logfile.flush();
-        }
-        t = simple_timer_current();
- 
-        bunch_with_diagnostics.get_diagnostics_actions_sptr()->turn_end_action(*stepper_sptr, *bunch_sptr, turn);
-        t = simple_timer_show(t, "diagnostics-turn");
-        general_actions.turn_end_action(*stepper_sptr, *bunch_sptr, turn);
-        t = simple_timer_show(t, "propagate-general_actions-turn");
-         
-    }     
-    if (rank == 0) logfile.close();    
+        
+        std::ofstream logfile;
+        if (rank == 0) logfile.open("log");
+        t = simple_timer_current(); 
+        bunch_with_diagnostics.get_diagnostics_actions_sptr()->first_action(*stepper_sptr, *bunch_sptr);
+        t = simple_timer_show(t, "diagnostics_first");
+        general_actions.first_action(*stepper_sptr, *bunch_sptr);
+        t = simple_timer_show(t, "propagate-general_actions");
+        for (int turn = 0; turn < num_turns; ++turn) {
+            t_turn=MPI_Wtime();
+            if (verbose) {
+                if (rank == 0) {
+                    std::cout << "Propagator: turn " << turn + 1 << "/"
+                            << num_turns << std::endl;
+                }
+            }
+            bunch_sptr->get_reference_particle().start_repetition();
+            int step_count = 0;
+            int num_steps = stepper_sptr->get_steps().size();             
+            for (Steps::const_iterator it = stepper_sptr->get_steps().begin(); it
+                != stepper_sptr->get_steps().end(); ++it) {
+                ++step_count;
+                if (verbose>0) {
+                    if (rank == 0) {
+                    std::cout << "Propagator:   step " << step_count << "/"
+                            << num_steps <<" s= "<<bunch_sptr->get_reference_particle().get_s()<<" trajectory length="<<bunch_sptr->get_reference_particle().get_trajectory_length()<< std::endl;
+                    }
+                }      
+                (*it)->apply(*bunch_sptr); 
+                /// apply with diagnostics only for testing purposes
+                //(*it)->apply(*bunch_sptr, bunch_with_diagnostics.get_per_step_diagnostics()); 
+                t = simple_timer_current(); 
+                bunch_with_diagnostics.get_diagnostics_actions_sptr()->step_end_action(*stepper_sptr, *(*it), *bunch_sptr,
+                    turn, step_count); 
+                t = simple_timer_show(t, "diagnostics-step"); 
+                general_actions.step_end_action(*stepper_sptr, *(*it),*bunch_sptr,
+                        turn, step_count);
+                t = simple_timer_show(t, "propagate-general_actions-step");      
+            }
+            
+        t_turn1= MPI_Wtime();
+        if (rank == 0) {
+                logfile<<" turn "<<turn + 1<<" : "<< t_turn1-t_turn<< " \n";
+                std::cout<<"  turn "<<turn + 1<<" : "<< t_turn1-t_turn<<std::endl;
+                logfile.flush();
+            }
+            t = simple_timer_current();
+    
+            bunch_with_diagnostics.get_diagnostics_actions_sptr()->turn_end_action(*stepper_sptr, *bunch_sptr, turn);
+            t = simple_timer_show(t, "diagnostics-turn");
+            general_actions.turn_end_action(*stepper_sptr, *bunch_sptr, turn);
+            t = simple_timer_show(t, "propagate-general_actions-turn");
+            
+        }     
+        if (rank == 0) logfile.close(); 
+    }
+    catch (std::exception const& e)
+    {
+        std::cout<<e.what()<<std::endl;   
+        MPI_Abort(MPI_COMM_WORLD, 888);
+    }   
 } 
 
 
@@ -172,73 +178,79 @@ Propagator::propagate(Bunch_with_diagnostics_train & bunch_diag_train,
              int num_turns,  Propagate_actions & general_actions, bool verbose)
 {
 
-
-    int rank = Commxx().get_rank();
-    double t_turn, t_turn1;
-    
-    std::ofstream logfile;
-    if (rank == 0) logfile.open("log");
-    for (int index = 0; index < bunch_diag_train.get_num_bunches(); ++index) {
-                if (bunch_diag_train.is_on_this_rank(index)) {
-                    Bunch_sptr bunch_sptr=bunch_diag_train.get_bunch_diag_sptr(index)->get_bunch_sptr();
-                    bunch_diag_train.get_bunch_diag_sptr(index)->get_diagnostics_actions_sptr()->first_action(*stepper_sptr, *bunch_sptr);
-                    general_actions.first_action(*stepper_sptr, *bunch_sptr);
-                }
-     }      
-    
-    
-    for (int turn = 0; turn < num_turns; ++turn) {
-            t_turn=MPI_Wtime();
-            if (verbose) {
-                if (rank == 0) {
-                    std::cout << "Propagator: turn " << turn + 1 << "/"
-                            << num_turns << std::endl;
-                }
-            } 
-            
-            for (int index = 0; index < bunch_diag_train.get_num_bunches(); ++index) {
-                if (bunch_diag_train.is_on_this_rank(index)) {
-                  bunch_diag_train.get_bunch_diag_sptr(index)->get_bunch_sptr()->get_reference_particle().start_repetition();
-               }
-            }
-
-
-            int step_count = 0;
-            int num_steps = stepper_sptr->get_steps().size();  
-            for (Steps::const_iterator it = stepper_sptr->get_steps().begin(); it
-                != stepper_sptr->get_steps().end(); ++it) {
-                ++step_count;
+try{
+        int rank = Commxx().get_rank();
+        double t_turn, t_turn1;
+        
+        std::ofstream logfile;
+        if (rank == 0) logfile.open("log");
+        for (int index = 0; index < bunch_diag_train.get_num_bunches(); ++index) {
+                    if (bunch_diag_train.is_on_this_rank(index)) {
+                        Bunch_sptr bunch_sptr=bunch_diag_train.get_bunch_diag_sptr(index)->get_bunch_sptr();
+                        bunch_diag_train.get_bunch_diag_sptr(index)->get_diagnostics_actions_sptr()->first_action(*stepper_sptr, *bunch_sptr);
+                        general_actions.first_action(*stepper_sptr, *bunch_sptr);
+                    }
+        }      
+        
+        
+        for (int turn = 0; turn < num_turns; ++turn) {
+                t_turn=MPI_Wtime();
                 if (verbose) {
                     if (rank == 0) {
-                        std::cout << "Propagator:   step " << step_count << "/" << num_steps <<std::endl;
+                        std::cout << "Propagator: turn " << turn + 1 << "/"
+                                << num_turns << std::endl;
                     }
                 } 
-                (*it)->apply(bunch_diag_train); 
-                 for (int index = 0; index < bunch_diag_train.get_num_bunches(); ++index) {  
-                   if (bunch_diag_train.is_on_this_rank(index)) {
-                        Bunch_sptr bunch_sptr=bunch_diag_train.get_bunch_diag_sptr(index)->get_bunch_sptr();
-                        bunch_diag_train.get_bunch_diag_sptr(index)->get_diagnostics_actions_sptr()
-                                ->step_end_action(*stepper_sptr, *(*it), *bunch_sptr, turn, step_count);
-                        general_actions.step_end_action(*stepper_sptr,*(*it), *bunch_sptr, turn, step_count);
-                   } 
-                }               
-            }    
-            t_turn1= MPI_Wtime();                
-            if (rank == 0) {
-                logfile<<" turn "<<turn + 1<<" : "<< t_turn1-t_turn<< " \n";
-                std::cout<<"  turn "<<turn + 1<<" : "<< t_turn1-t_turn<<std::endl;
-                logfile.flush();
-            } 
-            for (int index = 0; index < bunch_diag_train.get_num_bunches(); ++index) {
-                if (bunch_diag_train.is_on_this_rank(index)) {
-                        Bunch_sptr bunch_sptr=bunch_diag_train.get_bunch_diag_sptr(index)->get_bunch_sptr();
-                        bunch_diag_train.get_bunch_diag_sptr(index)->get_diagnostics_actions_sptr()
-                                ->turn_end_action(*stepper_sptr, *bunch_sptr, turn);
-                        general_actions.turn_end_action(*stepper_sptr,*bunch_sptr, turn);
+                
+                for (int index = 0; index < bunch_diag_train.get_num_bunches(); ++index) {
+                    if (bunch_diag_train.is_on_this_rank(index)) {
+                    bunch_diag_train.get_bunch_diag_sptr(index)->get_bunch_sptr()->get_reference_particle().start_repetition();
                 }
-            }                  
+                }
+    
+    
+                int step_count = 0;
+                int num_steps = stepper_sptr->get_steps().size();  
+                for (Steps::const_iterator it = stepper_sptr->get_steps().begin(); it
+                    != stepper_sptr->get_steps().end(); ++it) {
+                    ++step_count;
+                    if (verbose) {
+                        if (rank == 0) {
+                            std::cout << "Propagator:   step " << step_count << "/" << num_steps <<std::endl;
+                        }
+                    } 
+                    (*it)->apply(bunch_diag_train); 
+                    for (int index = 0; index < bunch_diag_train.get_num_bunches(); ++index) {  
+                    if (bunch_diag_train.is_on_this_rank(index)) {
+                            Bunch_sptr bunch_sptr=bunch_diag_train.get_bunch_diag_sptr(index)->get_bunch_sptr();
+                            bunch_diag_train.get_bunch_diag_sptr(index)->get_diagnostics_actions_sptr()
+                                    ->step_end_action(*stepper_sptr, *(*it), *bunch_sptr, turn, step_count);
+                            general_actions.step_end_action(*stepper_sptr,*(*it), *bunch_sptr, turn, step_count);
+                    } 
+                    }               
+                }    
+                t_turn1= MPI_Wtime();                
+                if (rank == 0) {
+                    logfile<<" turn "<<turn + 1<<" : "<< t_turn1-t_turn<< " \n";
+                    std::cout<<"  turn "<<turn + 1<<" : "<< t_turn1-t_turn<<std::endl;
+                    logfile.flush();
+                } 
+                for (int index = 0; index < bunch_diag_train.get_num_bunches(); ++index) {
+                    if (bunch_diag_train.is_on_this_rank(index)) {
+                            Bunch_sptr bunch_sptr=bunch_diag_train.get_bunch_diag_sptr(index)->get_bunch_sptr();
+                            bunch_diag_train.get_bunch_diag_sptr(index)->get_diagnostics_actions_sptr()
+                                    ->turn_end_action(*stepper_sptr, *bunch_sptr, turn);
+                            general_actions.turn_end_action(*stepper_sptr,*bunch_sptr, turn);
+                    }
+                }                  
+        }
+        if (rank == 0) logfile.close();   
     }
-    if (rank == 0) logfile.close();     
+    catch (std::exception const& e)
+    {
+        std::cout<<e.what()<<std::endl;   
+        MPI_Abort(MPI_COMM_WORLD, 888);
+    }
 }
 
 void
