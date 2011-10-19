262b0e9bf6ad01641a8737448784d62cf7cf019f
Alexandru Macridin <macridin@gmail.com>
2011-10-19 10:49:54 -0500

    comments deleted

--- src/synergia/collective/impedance.h
+++ src/synergia/collective/impedance.h
@@ -21,7 +21,8 @@ private:
 
 public:
 
-    Impedance(std::string const & wake_file, double const & orbit_length, double const & bunchsp, int const  & zgrid, std::string const & pipe_symmetry, int const nstored_turns);
+    Impedance(std::string const & wake_file, double const & orbit_length, double const & bunchsp, 
+                int const  & zgrid, std::string const & pipe_symmetry, int const nstored_turns);
 
 
     int get_z_grid() const;
@@ -34,41 +35,8 @@ public:
     std::vector<double> get_x_wake() const;
     std::vector<double> get_y_wake() const;
     std::vector<double> get_z_wake() const;
-    virtual int get_nstored_turns() const;
-
-   /*
-    get_n_sigma() const;
-    void
-    set_fixed_domain(Rectangular_grid_domain_sptr domain_sptr);
-    void
-    update_domain(Bunch const& bunch);
-    Rectangular_grid_domain_sptr
-    get_domain_sptr() const;
-    Rectangular_grid_domain_sptr
-    get_doubled_domain_sptr() const;
-    /// Returns local charge density on original grid in [C/m^3]
-    Rectangular_grid_sptr
-    get_local_charge_density(Bunch const& bunch);
-    /// Returns global charge density on doubled grid in [C/m^3]
-    Distributed_rectangular_grid_sptr
-    get_global_charge_density2(Rectangular_grid const& local_charge_density);
-    /// Returns Green function on the doubled grid in [1/m^3]
-    Distributed_rectangular_grid_sptr
-    get_green_fn2_pointlike();
-    Distributed_rectangular_grid_sptr
-    get_green_fn2_linear();
-    Distributed_rectangular_grid_sptr
-    get_scalar_field2(Distributed_rectangular_grid & charge_density22,
-            Distributed_rectangular_grid & green_fn2);
-    Distributed_rectangular_grid_sptr
-    extract_scalar_field(Distributed_rectangular_grid const& scalar_field2);
-    Distributed_rectangular_grid_sptr
-    get_electric_field_component(
-            Distributed_rectangular_grid const& scalar_field, int component);
-    Rectangular_grid_sptr
-    get_global_electric_field_component(
-            Distributed_rectangular_grid const& dist_field); */
-   // void impedance_kick(Bunch & bunch, double delta_tau,);
+    virtual int 
+    get_nstored_turns() const;
     virtual void
     apply(Bunch & bunch, double time_step, Step & step);
     virtual
