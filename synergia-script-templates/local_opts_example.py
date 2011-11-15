d734b2f6dfedd6cbdd6eac45efd53ade8ea91683
James Amundson <amundson@fnal.gov>
2011-11-15 12:51:58 -0600

    update examples for new job manager

---
+++ synergia-script-templates/local_opts_example.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# The local_options file must be named local_opts.py and placed
+# in the Synergia2 job manager search path.
+
+from synergia_workflow import options
+
+# Any instance of the Options class will be added as a suboption.
+opts = options.Options('local')
+opts.add("ompnumthreads",2,"number of openmp threads")
+
+# Any instance of the Override class will be used to override
+#   the defaults of the existing options.
+override = options.Override()
+override.account = "notonmy"
+override.numprocs = 4
+
