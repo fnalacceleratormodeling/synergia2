37354b6e37f887b4638a778b4fb49bc362ab4065
James Amundson <amundson@fnal.gov>
2011-12-05 16:43:00 -0600

    Extend Hdf5_file to read multi arrays

--- src/synergia/utils/hdf5_file.cc
+++ src/synergia/utils/hdf5_file.cc
@@ -1,4 +1,6 @@
 #include "hdf5_file.h"
+#include "synergia/utils/multi_array_typedefs.h"
+#include "synergia/utils/hdf5_misc.h"
 
 Hdf5_file::Hdf5_file(std::string const& file_name, bool read_only) :
     file(file_name.c_str(), read_only ? H5F_ACC_RDONLY : H5F_ACC_TRUNC)
@@ -9,3 +11,75 @@ Hdf5_file::~Hdf5_file()
 {
     file.close();
 }
+
+template<>
+    MArray1d
+    Hdf5_file::read<MArray1d >(std::string const& name)
+    {
+        DataSet dataset = file.openDataSet(name.c_str());
+        H5::DataType atomic_type = hdf5_atomic_data_type<double > ();
+
+        const int rank = 1;
+        std::vector<hsize_t > dims(rank);
+        DataSpace dataspace = dataset.getSpace();
+        int file_rank = dataspace.getSimpleExtentNdims();
+        if (file_rank != rank) {
+            throw std::runtime_error(
+                    "Hdf5_file::read<MArray1d>: data to read has wrong rank");
+        }
+        dataspace.getSimpleExtentDims(&dims[0], NULL);
+        MArray1d retval(boost::extents[dims[0]]);
+
+        DataSpace memspace(rank, &dims[0]);
+        double * data_out = retval.origin();
+        dataset.read(data_out, atomic_type, memspace, dataspace);
+        return retval;
+    }
+
+template<>
+    MArray2d
+    Hdf5_file::read<MArray2d >(std::string const& name)
+    {
+        DataSet dataset = file.openDataSet(name.c_str());
+        H5::DataType atomic_type = hdf5_atomic_data_type<double > ();
+
+        const int rank = 2;
+        std::vector<hsize_t > dims(rank);
+        DataSpace dataspace = dataset.getSpace();
+        int file_rank = dataspace.getSimpleExtentNdims();
+        if (file_rank != rank) {
+            throw std::runtime_error(
+                    "Hdf5_file::read<MArray2d>: data to read has wrong rank");
+        }
+        dataspace.getSimpleExtentDims(&dims[0], NULL);
+        MArray2d retval(boost::extents[dims[0]][dims[1]]);
+
+        DataSpace memspace(rank, &dims[0]);
+        double * data_out = retval.origin();
+        dataset.read(data_out, atomic_type, memspace, dataspace);
+        return retval;
+    }
+
+template<>
+    MArray3d
+    Hdf5_file::read<MArray3d >(std::string const& name)
+    {
+        DataSet dataset = file.openDataSet(name.c_str());
+        H5::DataType atomic_type = hdf5_atomic_data_type<double > ();
+
+        const int rank = 3;
+        std::vector<hsize_t > dims(rank);
+        DataSpace dataspace = dataset.getSpace();
+        int file_rank = dataspace.getSimpleExtentNdims();
+        if (file_rank != rank) {
+            throw std::runtime_error(
+                    "Hdf5_file::read<MArray3d>: data to read has wrong rank");
+        }
+        dataspace.getSimpleExtentDims(&dims[0], NULL);
+        MArray3d retval(boost::extents[dims[0]][dims[1]][dims[2]]);
+
+        DataSpace memspace(rank, &dims[0]);
+        double * data_out = retval.origin();
+        dataset.read(data_out, atomic_type, memspace, dataspace);
+        return retval;
+    }
--- src/synergia/utils/tests/test_hdf5_file.cc
+++ src/synergia/utils/tests/test_hdf5_file.cc
@@ -2,6 +2,7 @@
 #include <boost/test/unit_test.hpp>
 #include "synergia/utils/hdf5_file.h"
 #include "synergia/utils/multi_array_typedefs.h"
+#include "synergia/utils/multi_array_check_equal.h"
 
 BOOST_AUTO_TEST_CASE(construct)
 {
@@ -41,12 +42,34 @@ BOOST_AUTO_TEST_CASE(read_write_data)
     const char * filename = "hdf5_file_read_write.h5";
     const char * int_label = "int_data";
     const char * double_label = "double_data";
+    const char * array1d_label = "array1d_data";
+    const char * array2d_label = "array2d_data";
+    const char * array3d_label = "array3d_data";
     int int_data = 7;
     double double_data = 2.71828;
+    const int dim1 = 2;
+    const int dim2 = 3;
+    const int dim3 = 4;
+    MArray1d a1d(boost::extents[dim1]);
+    MArray2d a2d(boost::extents[dim1][dim2]);
+    MArray3d a3d(boost::extents[dim1][dim2][dim3]);
+    for (int j = 0; j < dim1; ++j) {
+        a1d[j] = 100 * j;
+        for (int k = 0; k < dim2; ++k) {
+            a2d[j][k] = 10 * k + 100 * j;
+            for (int l = 0; l < dim2; ++l) {
+                a3d[j][k][l] = 1.1 * l + 10 * k + 100 * j;
+            }
+        }
+    }
+
     {
         Hdf5_file write_file(filename);
         write_file.write(int_data, int_label);
         write_file.write(double_data, double_label);
+        write_file.write(a1d, array1d_label);
+        write_file.write(a2d, array2d_label);
+        write_file.write(a3d, array3d_label);
     }
 
     {
@@ -55,5 +78,12 @@ BOOST_AUTO_TEST_CASE(read_write_data)
         BOOST_CHECK_EQUAL(int_read, int_data);
         double double_read = read_file.read<double > (double_label);
         BOOST_CHECK_CLOSE(double_read, double_data, tolerance);
+        MArray1d a1d_read(read_file.read<MArray1d > (array1d_label));
+        multi_array_check_equal(a1d_read, a1d, tolerance);
+        MArray2d a2d_read(read_file.read<MArray2d > (array2d_label));
+        multi_array_check_equal(a2d_read, a2d, tolerance);
+        MArray3d a3d_read(read_file.read<MArray3d > (array3d_label));
+        multi_array_check_equal(a3d_read, a3d, tolerance);
     }
 }
+
