f137f92842a72ca947272fee900699ce51f07f9e
James Amundson <amundson@fnal.gov>
2012-02-17 11:24:50 -0600

    Revert "add USE_SIMPLE_TIMER_MEM AND USE_SIMPLE_TIMER_NO_BARRIER options"
    This reverts commit 6a21fb1cdd92aadd4bad047c02257a7f5f561055.

--- src/synergia/utils/simple_timer.h
+++ src/synergia/utils/simple_timer.h
@@ -1,17 +1,10 @@
 #include "mpi.h"
 #include <iostream>
-#ifdef USE_SIMPLE_TIMER_MEM
-#include <fstream>
-#include <string>
-#endif // USE_SIMPLE_TIMER_MEM
 
 inline double
 simple_timer_current()
 {
 #ifdef USE_SIMPLE_TIMER
-#ifndef USE_SIMPLE_TIMER_NO_BARRIER
-    MPI_Barrier(MPI_COMM_WORLD);
-#endif // USE_SIMPLE_TIMER_NO_BARRIER
     return MPI_Wtime();
 #else
     return 0.0;
@@ -24,32 +17,13 @@ simple_timer_show(double t0, const char * label)
 #ifdef USE_SIMPLE_TIMER
     double t1 = MPI_Wtime();
     int rank;
-#ifndef USE_SIMPLE_TIMER_NO_BARRIER
-    MPI_Barrier(MPI_COMM_WORLD);
-#endif // USE_SIMPLE_TIMER_NO_BARRIER
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     if (rank == 0) {
         std::cout << "simple_timer:" << label << ":" << (t1-t0) << std::endl;
     }
-#ifdef USE_SIMPLE_TIMER_MEM
-    // the order of fields in this file is vmsize, vmrss, "shared", "code",
-    // "library", "data-stack", "dirty", units are 4k pages.
-    // True for at least slf5,6 and ubuntu 10.04-11.10 
-    std::ifstream statm("/proc/self/statm", std::ifstream::in);
-    std::string vmsize, vmrss, vmstk, throwaway1, throwaway2,throwaway3;
-    statm >> vmsize;
-    statm >> vmrss;
-    statm >> throwaway1; // shared
-    statm >> throwaway2; // code;
-    statm >> throwaway3; // library;
-    statm >> vmstk; // data-stack
-    statm.close();
-    if (rank == 0) {
-	std::cout << "simple_timer_mem:" << label << ":" << vmsize << ":" << vmrss << ":" << vmstk << std::endl;
-    }
-#endif // USE_SIMPLE_TIMER_MEM
     return MPI_Wtime();
 #else
     return 0.0;
 #endif // USE_SIMPLE_TIMER
 }
+
