e9875638e4f2be894cece18e88f0a4bd1005ac0b
James Amundson <amundson@fnal.gov>
2012-03-03 10:50:31 -0600

    start synergia-resume

---
+++ src/frontend/synergia-resume.cc
@@ -0,0 +1,80 @@
+#include <iostream>
+#include "synergia/utils/command_line_arg.h"
+#include "synergia/simulation/resume.h"
+
+void
+usage(int retval)
+{
+    std::cout << "usage: synergia-resume [options] [checkpoint directory]\n";
+    std::cout << "  options:\n";
+    std::cout << "    --help: this message\n";
+    std::cout
+            << "    --new-dir=<dir>: directory name to use for subsequent checkpointing\n";
+    std::cout << "    --period=<period>: period for subsequent checkpointing\n";
+    std::cout << "    --max=<num>: maximum number of turns for this run\n";
+    std::cout << "\n";
+    exit(retval);
+}
+
+struct Resume_options
+{
+    std::string directory;
+    std::string new_checkpoint_directory;
+    int checkpoint_period;
+    int max_turns;
+
+    static const int unspecified_int = -1;
+    static const char unspecified_str[];
+
+    Resume_options(int argc, char **argv) :
+        directory(unspecified_str), new_checkpoint_directory(unspecified_str),
+                checkpoint_period(unspecified_int), max_turns(unspecified_int)
+    {
+        for (int i = 1; i < argc; ++i) {
+            if (std::string(argv[i]) == "--help") {
+                usage(0);
+            } else {
+                Command_line_arg arg(argv[i]);
+                if (arg.is_equal_pair()) {
+                    if (arg.get_lhs() == "--new-dir") {
+                        new_checkpoint_directory = arg.extract_value<
+                                std::string > ();
+                    } else if (arg.get_lhs() == "--period") {
+                        checkpoint_period = arg.extract_value<int > ();
+                    } else if (arg.get_lhs() == "--max") {
+                        max_turns = arg.extract_value<int > ();
+                    } else {
+                        std::cout << "Unknown argument " << arg.get_lhs()
+                                << std::endl;
+                        usage(1);
+                    }
+                } else {
+                    if (argv[i][0] == '-') {
+                        std::cout << "Unknown argument " << argv[i]
+                                << std::endl;
+                        usage(1);
+                    }
+                    directory = argv[i];
+                }
+            }
+        }
+    }
+};
+
+const char Resume_options::unspecified_str[] = "";
+
+void
+run(Resume_options &opts)
+{
+
+}
+
+int
+main(int argc, char **argv)
+{
+    MPI_Init(&argc, &argv);
+    Resume_options opts(argc, argv);
+    run(opts);
+    MPI_Finalize();
+    return 0;
+}
