520ea09a4cd30ad6d2539e3f923e76adba6201bc
Eric G. Stern <egstern@fnal.gov>
2012-03-19 16:34:10 -0500

    incorrect sense of test for new_max_turns

--- src/frontend/synergia-resume.cc
+++ src/frontend/synergia-resume.cc
@@ -79,7 +79,7 @@ run(Resume_options &opts)
     if (opts.new_checkpoint_directory != Resume_options::unspecified_str) {
         resume.set_new_checkpoint_dir(opts.new_checkpoint_directory);
     }
-    bool new_max_turns = (opts.max_turns == Resume_options::unspecified_int);
+    bool new_max_turns = (opts.max_turns != Resume_options::unspecified_int);
     bool new_verbosity = (opts.verbosity != Resume_options::unspecified_int);
     resume.propagate(new_max_turns, opts.max_turns, new_verbosity,
             opts.verbosity);
