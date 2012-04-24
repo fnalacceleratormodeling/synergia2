1da5ebbbc686df4bf54a934b69ba62fc3b07d228
Eric G. Stern <egstern@fnal.gov>
2012-04-24 12:47:49 -0500

    start trying to add new diagnostic for bulk track writing

---
+++ src/synergia/bunch/diagnostics_bulk_track.cc
@@ -0,0 +1,177 @@
+#include "diagnostics_bulk_track.h"
+
+const char Diagnostics_bulk_track::name[] = "diagnostics_bulk_track";
+
+Diagnostics_bulk_track::Diagnostics_bulk_track(
+        std::string const& filename, int num_tracks) :
+  Diagnostics(Diagnostics_bulk_track::name, filename), num_tracks(num_tracks),
+  have_writers(false), first_search(true), diag_track_status(),
+  tracks_coords(boost::extents[num_tracks][7])
+  
+{
+}
+
+Diagnostics_bulk_track::Diagnostics_bulk_track()
+{
+}
+
+bool
+Diagnostics_bulk_track::is_serial() const
+{
+    return true;
+}
+
+Diagnostics_write_helper *
+Diagnostics_bulk_track::new_write_helper_ptr()
+{
+    delete_write_helper_ptr();
+    std::stringstream sstream;
+    sstream << get_filename();
+    sstream << "_";
+    sstream << std::setw(4);
+    sstream << std::setfill('0');
+    sstream << get_bunch().get_comm().get_rank();
+    return new Diagnostics_write_helper(sstream.str(), true,
+            get_bunch().get_comm(), get_bunch().get_comm().get_rank());
+}
+
+void
+Diagnostics_bulk_track::update()
+{
+  if (diag_track_status.empty()) {
+    for (int idxtrk=0; idxtrk<num_tracks; ++idxtrk) {
+      Diag_track_status new_diag_status(true, //found,
+					idxtrk, // last_index
+					static_cast<int > (get_bunch().get_local_particles()[idxtrk][Bunch::id]));
+      diag_track_status.push_back(new_diag_status);
+    }
+
+    get_bunch().convert_to_state(get_bunch().fixed_z_lab);
+    repetition = get_bunch().get_reference_particle().get_repetition();
+    trajectory_length
+            = get_bunch().get_reference_particle().get_trajectory_length();
+    for (int idxtrk=0; idxtrk<num_tracks; ++idxtrk) {
+
+      Diag_track_status *dtsptr = &diag_track_status[idxtrk];
+      if (dtsptr->found || first_search) {
+        int index;
+        dtsptr->found = false;
+        if ((dtsptr->last_index > -1) &&
+	    (dtsptr->last_index < get_bunch().get_local_num())) {
+            if (dtsptr->particle_id
+                    == static_cast<int > (get_bunch().get_local_particles()[Bunch::id][last_index])) {
+                index = dtsptr->last_index;
+                dtsptr->found = true;
+            }
+        }
+        if (!(dtsptr->found)) {
+	    index = 0;
+            while ((index < get_bunch().get_local_num())
+                    && (dtsptr->particle_id
+                            != static_cast<int > (get_bunch().get_local_particles()[index][Bunch::id]))) {
+                index += 1;
+            }
+            if (index < get_bunch().get_local_num()) {
+                dtsptr->found = true;
+            } else {
+                dtsptr->found = false;
+            }
+        }
+        if (dtsptr->found) {
+            if (first_search) {
+                get_write_helper();
+            }
+            track_coords[idxtrk][0] = get_bunch().get_local_particles()[index][0];
+            track_coords[idxtrk][1] = get_bunch().get_local_particles()[index][1];
+            track_coords[idxtrk][2] = get_bunch().get_local_particles()[index][2];
+            track_coords[idxtrk][3] = get_bunch().get_local_particles()[index][3];
+            track_coords[idxtrk][4] = get_bunch().get_local_particles()[index][4];
+            track_coords[idxtrk][5] = get_bunch().get_local_particles()[index][5];
+	    track_coords[idxtrk][6] = get_bunch().get_local_particles()[index][6];
+            s = get_bunch().get_reference_particle().get_s();
+            repetition = get_bunch().get_reference_particle().get_repetition();
+            trajectory_length
+                    = get_bunch().get_reference_particle().get_trajectory_length();
+        }
+        first_search = false;
+    }
+}
+
+void
+Diagnostics_bulk_track::init_writers(Hdf5_file_sptr file_sptr)
+{
+    if (!have_writers) {
+        writer_coords = new Hdf5_serial_writer<MArray2d_ref > (file_sptr,
+                "track_coords");
+        writer_s = new Hdf5_serial_writer<double > (file_sptr, "s");
+        writer_repetition = new Hdf5_serial_writer<int > (file_sptr,
+                "repetition");
+        writer_trajectory_length = new Hdf5_serial_writer<double > (file_sptr,
+                "trajectory_length");
+        have_writers = true;
+    }
+}
+
+void
+Diagnostics_bulk_track::write()
+{
+    get_bunch().convert_to_state(get_bunch().fixed_z_lab);
+    if (found) {
+        init_writers(get_write_helper().get_hdf5_file_sptr());
+        writer_coords->append(track_coords);
+        writer_s->append(s);
+        writer_repetition->append(repetition);
+        writer_trajectory_length->append(trajectory_length);
+        get_write_helper().finish_write();
+    }
+}
+
+template<class Archive>
+    void
+    Diagnostics_bulk_track::serialize(Archive & ar, const unsigned int version)
+    {
+        ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Diagnostics)
+                & BOOST_SERIALIZATION_NVP(have_writers)
+                & BOOST_SERIALIZATION_NVP(first_search)
+	  & BOOST_SERIALIZATION_NVP(diag_track_status)
+                & BOOST_SERIALIZATION_NVP(s)
+                & BOOST_SERIALIZATION_NVP(writer_s)
+                & BOOST_SERIALIZATION_NVP(repetition)
+                & BOOST_SERIALIZATION_NVP(writer_repetition)
+                & BOOST_SERIALIZATION_NVP(trajectory_length)
+                & BOOST_SERIALIZATION_NVP(writer_trajectory_length)
+                & BOOST_SERIALIZATION_NVP(track_coords)
+                & BOOST_SERIALIZATION_NVP(writer_coords);
+    }
+
+template
+void
+Diagnostics_bulk_track::serialize<boost::archive::binary_oarchive >(
+        boost::archive::binary_oarchive & ar, const unsigned int version);
+
+template
+void
+Diagnostics_bulk_track::serialize<boost::archive::xml_oarchive >(
+        boost::archive::xml_oarchive & ar, const unsigned int version);
+
+template
+void
+Diagnostics_bulk_track::serialize<boost::archive::binary_iarchive >(
+        boost::archive::binary_iarchive & ar, const unsigned int version);
+
+template
+void
+Diagnostics__bulk_track::serialize<boost::archive::xml_iarchive >(
+        boost::archive::xml_iarchive & ar, const unsigned int version);
+
+Diagnostics_bulk_track::~Diagnostics_bulk_track()
+{
+    if (have_writers) {
+        delete writer_trajectory_length;
+        delete writer_repetition;
+        delete writer_s;
+        delete writer_coords;
+    }
+}
+BOOST_CLASS_EXPORT_IMPLEMENT(Diagnostics_bulk_track)
+
---
+++ src/synergia/bunch/diagnostics_bulk_track.h
@@ -0,0 +1,74 @@
+#ifndef DIAGNOSTICS_BULK_TRACK_H_
+#define DIAGNOSTICS_BULK_TRACK_H_
+
+#include "synergia/bunch/diagnostics.h"
+
+struct Diag_track_status {
+  bool found;
+  int last_index;
+  int particle_id;
+}
+
+/// Diagnostics_bulk_track records the phase space coordinates of a multiple
+/// particles.
+/// Particles will only be tracked if they stay on the same processor.
+/// Lost particles that are somehow restored or particles not available when
+/// the first update is called will also not be tracked.
+class Diagnostics_bulk_track : public Diagnostics
+{
+public:
+    static const char name[];
+private:
+    int num_tracks;
+    bool have_writers;
+    bool first_search;
+    std::vector<Diag_track_status > diag_track_status;
+    double s;
+    Hdf5_serial_writer<double > * writer_s;
+    int repetition;
+    Hdf5_serial_writer<int > * writer_repetition;
+    double trajectory_length;
+    Hdf5_serial_writer<double > * writer_trajectory_length;
+    MArray2d track_coords;
+    Hdf5_serial_writer<MArray2d_ref > * writer_coords;
+    virtual void
+    init_writers(Hdf5_file_sptr file_sptr);
+
+public:
+    /// Create an empty Diagnostics_bulk_track object
+    /// @param bunch_sptr the Bunch
+    /// @param filename the base name for file to write to (base names will have
+    ///        a numerical index inserted
+    /// @param num_tracks the number of local particles to track
+    Diagnostics_bulk_track(std::string const& filename,
+            int num_tracks);
+
+    // Default constructor for serialization use only
+    Diagnostics_bulk_track();
+
+    virtual Diagnostics_write_helper *
+    new_write_helper_ptr();
+
+    /// Multiple serial diagnostics can be written to a single file.
+    /// The Diagnostics_bulk_track class is serial.
+    virtual bool
+    is_serial() const;
+
+    /// Update the diagnostics
+    virtual void
+    update();
+
+    virtual void
+    write();
+
+    template<class Archive>
+        void
+        serialize(Archive & ar, const unsigned int version);
+
+    virtual
+    ~Diagnostics_bulk_track();
+};
+BOOST_CLASS_EXPORT_KEY(Diagnostics_bulk_track)
+typedef boost::shared_ptr<Diagnostics_bulk_track > Diagnostics_bulk_track_sptr;
+
+#endif /* DIAGNOSTICS_BULK_TRACK_H_ */
