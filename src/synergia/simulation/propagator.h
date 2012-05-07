1974d971d792d16142ae8656ff0fae3d684cb461
Eric G. Stern <egstern@fnal.gov>
2012-05-07 14:56:29 -0500

    default checkpoint throttling to 1 writer

--- src/synergia/simulation/propagator.h
+++ src/synergia/simulation/propagator.h
@@ -22,7 +22,7 @@ public:
     static const std::string log_file_name;
     static const std::string stop_file_name;
     static const std::string alt_stop_file_name;
-    static const int default_concurrent_io = 8;
+    static const int default_concurrent_io = 1;
 
     struct State
     {
