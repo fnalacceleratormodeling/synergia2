6e67e92fbd43c5f6a9fcefb10657c4c3e2f7f67d
Chong Shik Park <cspark@fnal.gov>
2012-06-07 14:34:34 -0500

    update mu2e resumer script

--- examples/mu2e_resume/resumer.py
+++ examples/mu2e_resume/resumer.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
+import sys
+import os
+import mpi4py.MPI as MPI
 import synergia
 from resumer_options import opts
 
+myrank = synergia.utils.Commxx().get_rank()
+
+t0 = MPI.Wtime()
+
 resume_dir = "%s/checkpoint" % opts.resume_dir
-r = synergia.simulation.Resume(resume_dir)
+resume = synergia.simulation.Resume(resume_dir)
+content = resume.get_content()
 
 if opts.max_turns == -1:
     new_max_turns = False
@@ -16,10 +24,16 @@ else:
     new_verbosity = True
 
 # these arguments will tell Resume to use the original options
-#r.propagate(False, -1, False, -1)
-r.propagate(new_max_turns, opts.max_turns, new_verbosity, opts.verbosity)
+#resume.propagate(False, -1, False, -1)
+resume.propagate(new_max_turns, opts.max_turns, new_verbosity, opts.verbosity)
 
-#lattice_diagnostics = synergia.lattice.Lattice_diagnostics(synergia_lattice,
-#                "lattice_deposited_charge.h5", "deposited_charge")
-#lattice_diagnostics.update_and_write()
+t1 = MPI.Wtime()
+
+if myrank == 0:
+    print
+    print "Propagate time =", t1 - t0
+
+lattice_diagnostics = synergia.lattice.Lattice_diagnostics(content.lattice,
+                "lattice_deposited_charge.h5", "deposited_charge")
+lattice_diagnostics.update_and_write()
 
