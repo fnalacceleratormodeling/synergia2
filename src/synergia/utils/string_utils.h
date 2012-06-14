379ea46204ae080177a34db840d80c1eae2e4b01
James Amundson <amundson@fnal.gov>
2012-06-14 13:58:29 -0500

    string_false bug fix

--- src/synergia/utils/string_utils.h
+++ src/synergia/utils/string_utils.h
@@ -5,7 +5,7 @@
 inline bool
 false_string(std::string const& val)
 {
-    return (val == "false") || (val != "False") || (val == "FALSE");
+    return (val == "false") || (val == "False") || (val == "FALSE");
 }
 
 #endif /* STRING_UTILS_H_ */
