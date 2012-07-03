5bb10edcf5803955f4647a62f9d768208ee5211e
James Amundson <amundson@fnal.gov>
2012-07-03 15:23:57 -0500

    avoid unused variable warnings

--- src/synergia/bunch/tests/test_bunch_train_mpi.cc
+++ src/synergia/bunch/tests/test_bunch_train_mpi.cc
@@ -58,7 +58,7 @@ BOOST_AUTO_TEST_CASE(is_on_this_rank)
     Bunch_train bunch_train(num_bunches, bunch_separation,
             commxx_sptr);
     for (int bunch = 0; bunch < num_bunches; ++bunch) {
-        bool on_this_rank = bunch_train.is_on_this_rank(bunch);
+        bunch_train.is_on_this_rank(bunch);
         // could use a better test here...
     }
 }
@@ -66,7 +66,6 @@ BOOST_AUTO_TEST_CASE(is_on_this_rank)
 BOOST_AUTO_TEST_CASE(diag_get_comm)
 {
     const int num_bunches = 2;
-    const double bunch_separation = 1.7;
     Commxx_sptr mcommxx_sptr(new Commxx);
 //     std::cout<<" mcommxx_sptr rank="<<mcommxx_sptr.get_rank()<<std::endl;
 //     std::cout<<" mcommxx_sptr size="<<mcommxx_sptr.get_size()<<std::endl;
@@ -74,9 +73,8 @@ BOOST_AUTO_TEST_CASE(diag_get_comm)
     Train_comms train(num_bunches, mcommxx_sptr);
     for (int bunch = 0; bunch < num_bunches; ++bunch) {
         if (train.is_on_this_rank(bunch)) {
-           int  rk, sz;
-            rk = train.get_comm(bunch).get_rank();
-            sz= train.get_comm(bunch).get_size();
+            train.get_comm(bunch).get_rank();
+            train.get_comm(bunch).get_size();
            // std::cout<<" bunch="<<bunch<<" comm rank="<<rk<<" size="<<sz<<" master com rank="<<mcommxx_sptr.get_rank()<<std::endl;
        //  std::cout<<" comm size="<<comm.get_size()<<std::endl;
         }
