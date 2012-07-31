84846a57f8a580451aeec3f266a43e31b3d80b58
James Amundson <amundson@fnal.gov>
2012-07-31 15:38:07 -0500

    add simplify_all, et al., to lattice module

---
+++ src/synergia/lattice/simplify.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import synergia
+
+no_simplify_attribute = 'no_simplify'
+
+def has_no_simplify(element):
+    retval = False
+    if element.has_string_attribute(no_simplify_attribute):
+        if not element.get_string_attribute(no_simplify_attribute).lower == 'false':
+            retval = True
+    return retval
+
+def _copy_attributes_ancestors(element, new_element):
+    for attr in element.get_string_attributes():
+        new_element.set_string_attribute(attr, 
+            element.get_string_attribute(attr))
+    for attr in element.get_double_attributes():
+        new_element.set_double_attribute(attr, 
+            element.get_double_attribute(attr))
+    for ancestor in element.get_ancestors():
+        new_element.add_ancestor(ancestor)
+
+def eliminate_markers(lattice):
+    new_lattice = synergia.lattice.Lattice(lattice.get_name())
+    new_lattice.set_reference_particle(lattice.get_reference_particle())
+    for element in lattice.get_elements():
+            if (element.get_type() == 'marker') and \
+                (not has_no_simplify(element)):
+                pass
+            else:
+                new_lattice.append(element)
+    return new_lattice
+
+def convert_monitors(lattice):
+    new_lattice = synergia.lattice.Lattice(lattice.get_name())
+    new_lattice.set_reference_particle(lattice.get_reference_particle())
+    for element in lattice.get_elements():
+            if ((element.get_type() == 'monitor') or \
+                (element.get_type() == 'hmonitor') or \
+                (element.get_type() == 'vmonitor')) and \
+                (not has_no_simplify(element)):
+                new_element = synergia.lattice.Lattice_element('drift', element.get_name())
+                _copy_attributes_ancestors(element, new_element)
+                new_lattice.append(new_element)
+            else:
+                new_lattice.append(element)
+    return new_lattice
+
+def _is_magnet(type):
+    suffix = 'pole'
+    return type.find(suffix) == (len(type) - len(suffix))
+
+def convert_magnets(lattice):
+    new_lattice = synergia.lattice.Lattice(lattice.get_name())
+    new_lattice.set_reference_particle(lattice.get_reference_particle())
+    for element in lattice.get_elements():
+            if _is_magnet(element.get_type()) and \
+                (not has_no_simplify(element)):
+                has_nonzero_strength = False
+                for attr in element.get_double_attributes():
+                    if (len(attr) == 1) and (attr == 'k'):
+                        if element.get_double_attribute(attr) != 0.0:
+                            has_nonzero_strength = True
+                    else:
+                        if (attr[0] == 'k') and attr[1].isdigit():
+                            if element.get_double_attribute(attr) != 0.0:
+                                has_nonzero_strength = True
+                if has_nonzero_strength:
+                    new_lattice.append(element)
+                else:                    
+                    new_element = synergia.lattice.Lattice_element('drift', element.get_name())
+                    _copy_attributes_ancestors(element, new_element)
+                    new_lattice.append(new_element)
+            else:
+                new_lattice.append(element)
+    return new_lattice
+
+def convert_kickers(lattice):
+    new_lattice = synergia.lattice.Lattice(lattice.get_name())
+    new_lattice.set_reference_particle(lattice.get_reference_particle())
+    for element in lattice.get_elements():
+            if ((element.get_type() == 'hicker') or \
+                (element.get_type() == 'vkicker')) and \
+                (not has_no_simplify(element)):
+                if element.get_double_attribute('kick') == 0.0:
+                    new_element = synergia.lattice.Lattice_element('drift', element.get_name())
+                    _copy_attributes_ancestors(element, new_element)
+                    new_lattice.append(new_element)
+                else:
+                    new_lattice.append(element)
+            elif (element.get_type() == 'kicker') and \
+                (not has_no_simplify(element)):
+                if (element.get_double_attribute('hkick') == 0.0) and \
+                    (element.get_double_attribute('vkick') == 0.0):
+                    new_element = synergia.lattice.Lattice_element('drift', element.get_name())
+                    _copy_attributes_ancestors(element, new_element)
+                    new_lattice.append(new_element)                    
+                else:
+                    new_lattice.append(element)
+            else:
+                new_lattice.append(element)
+    return new_lattice
+
+def combine_drifts(lattice):
+    new_lattice = synergia.lattice.Lattice(lattice.get_name())
+    new_lattice.set_reference_particle(lattice.get_reference_particle())
+    last_element = None
+    for element in lattice.get_elements():
+        if last_element:
+            last_l = last_element.get_length()
+            ancestors = last_element.get_ancestors()
+            if (element.get_type() == 'drift') and \
+                (not has_no_simplify(element)):
+                concat_name = last_element.get_name()
+                concat_name += '+'
+                concat_name += element.get_name()
+                this_l = element.get_length()
+                last_element = synergia.lattice.Lattice_element('drift', concat_name)
+                last_element.set_double_attribute('l', last_l + this_l)
+                for ancestor in ancestors:
+                    last_element.add_ancestor(ancestor)
+            else:
+                new_lattice.append(last_element)
+                new_lattice.append(element)
+                last_element = None
+        else:
+            if element.get_type() == 'drift':
+                last_element = element
+            else:
+                new_lattice.append(element)
+                last_element = None
+    if last_element:
+        new_lattice.append(last_element)
+    return new_lattice
+
+def simplify_all(lattice):
+    return \
+        combine_drifts(\
+            convert_kickers(\
+                convert_magnets(\
+                    convert_monitors(\
+                        eliminate_markers(lattice)))))
+
---
+++ src/synergia/lattice/tests/test_simplify.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('../../..')
+import local_paths
+
+from nose.tools import *
+from synergia.lattice import eliminate_markers, convert_monitors, convert_magnets, \
+    combine_drifts, simplify_all
+from synergia.lattice import Lattice, xml_load_lattice
+
+def test_booster():
+    orig = Lattice()
+    xml_load_lattice(orig, "lattices/fnal_booster.xml")
+    simplified = simplify_all(orig)
+    assert_almost_equal(orig.get_length(), simplified.get_length())
+    print "booster went from", len(orig.get_elements()), "to",
+    print len(simplified.get_elements()), "elements"
+
+def test_debuncher():
+    orig = Lattice()
+    xml_load_lattice(orig, "lattices/fnal_debuncher.xml")
+    simplified = simplify_all(orig)
+    assert_almost_equal(orig.get_length(), simplified.get_length())
+    print "debuncher went from", len(orig.get_elements()), "to",
+    print len(simplified.get_elements()), "elements"
+
+def test_main_injector():
+    orig = Lattice()
+    xml_load_lattice(orig, "lattices/fnal_main_injector.xml")
+    simplified = simplify_all(orig)
+    assert_almost_equal(orig.get_length(), simplified.get_length())
+    print "main injector went from", len(orig.get_elements()), "to",
+    print len(simplified.get_elements()), "elements"
+
+if __name__ == '__main__':
+    test_booster()
+    test_debuncher()
+    test_main_injector()
+
