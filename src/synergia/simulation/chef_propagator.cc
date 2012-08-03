4aa5140b4ef79025f8f373b5d61f0a3161ca4913
James Amundson <amundson@fnal.gov>
2012-08-03 16:00:13 -0500

    optimize chef/synergia particle conversion

--- src/synergia/simulation/chef_propagator.cc
+++ src/synergia/simulation/chef_propagator.cc
@@ -41,34 +41,29 @@ Chef_propagator::apply(Bunch & bunch, int verbosity, Logger & logger)
     // std::cout<<"chef operate apply with length= "<<length<<std::endl;
     bunch.get_reference_particle().increment_trajectory(length);
 
-    std::vector<double > u = chef_unit_conversion(
-            bunch.get_reference_particle());
     int local_num = bunch.get_local_num();
     MArray2d_ref particles = bunch.get_local_particles();
 
-    Vector chef_state(6);
     for (int part = 0; part < local_num; ++part) {
-        for (int synergia_index = 0; synergia_index < 6; ++synergia_index) {
-            int chef_idx = get_chef_index(synergia_index);
-            chef_state[chef_idx] = particles[part][synergia_index]
-                    / u[synergia_index];
-        }
-
-        particle.State() = chef_state;
+        particle.set_x(particles[part][Bunch::x]);
+        particle.set_npx(particles[part][Bunch::xp]);
+        particle.set_y(particles[part][Bunch::y]);
+        particle.set_npy(particles[part][Bunch::yp]);
+        particle.set_cdt(particles[part][Bunch::cdt]);
+        particle.set_ndp(particles[part][Bunch::dpop]);
 
         for (Chef_lattice_section::iterator it =
                 chef_lattice_section_sptr->begin(); it
                 != chef_lattice_section_sptr->end(); ++it) {
             (*it)->propagate(particle);
         }
-        chef_state = particle.State();
 
-        for (int synergia_index = 0; synergia_index < 6; ++synergia_index) {
-            int chef_idx = get_chef_index(synergia_index);
-            particles[part][synergia_index] = chef_state[chef_idx]
-                    * u[synergia_index];
-        }
-
+        particles[part][Bunch::x] = particle.get_x();
+        particles[part][Bunch::xp] = particle.get_npx();
+        particles[part][Bunch::y] = particle.get_y();
+        particles[part][Bunch::yp] = particle.get_npy();
+        particles[part][Bunch::cdt] = particle.get_cdt();
+        particles[part][Bunch::dpop] = particle.get_ndp();
     }
 }
 
