850e82ea1b3677add9b01bde34c91299955d0d0e
James Amundson <amundson@fnal.gov>
2012-08-15 15:24:04 -0500

    Add Raw_multi_array and friends

--- src/synergia/utils/multi_array_typedefs.h
+++ src/synergia/utils/multi_array_typedefs.h
@@ -4,10 +4,38 @@
 #include <complex>
 #include "boost/multi_array.hpp"
 
+typedef boost::multi_array_types::index_range range;
+typedef boost::multi_array_types::extent_range extent_range;
+
+template<typename T, size_t N_dims>
+    struct Raw_multi_array
+    {
+        boost::const_multi_array_ref<T, N_dims > * dummy;
+        typedef typename boost::const_multi_array_ref<T, N_dims >::storage_order_type storage_order_type;
+        T * p;
+        boost::multi_array_ref<T, N_dims > m;
+        template<class ExtentList>
+            explicit
+            Raw_multi_array(const ExtentList& extents,
+                    const storage_order_type& store = boost::c_storage_order()) :
+                    dummy(
+                            new boost::const_multi_array_ref<T, N_dims >(
+                                    static_cast<T* >(0L), extents, store)), p(
+                            new T[dummy->num_elements()]), m(p, extents)
+            {
+                delete dummy;
+            }
+        ~Raw_multi_array()
+        {
+            delete[] p;
+        }
+    };
+
 typedef boost::multi_array<double, 1 > MArray1d;
 typedef boost::multi_array_ref<double, 1 > MArray1d_ref;
 typedef boost::const_multi_array_ref<double, 1 > Const_MArray1d_ref;
 typedef boost::detail::multi_array::multi_array_view<double, 1 > MArray1d_view;
+typedef Raw_multi_array<double, 1> Raw_MArray1d;
 
 
 typedef boost::multi_array<double, 2 > MArray2d;
@@ -16,13 +44,15 @@ typedef boost::const_multi_array_ref<double, 2 > Const_MArray2d_ref;
 typedef boost::detail::multi_array::multi_array_view<double, 2 > MArray2d_view;
 typedef boost::detail::multi_array::const_multi_array_view<double, 2 >
         Const_MArray2d_view;
-typedef boost::general_storage_order<2> storage2d;        
+typedef boost::general_storage_order<2> storage2d;
+typedef Raw_multi_array<double, 2> Raw_MArray2d;
 
 typedef boost::multi_array<double, 3 > MArray3d;
 typedef boost::multi_array_ref<double, 3 > MArray3d_ref;
 typedef boost::const_multi_array_ref<double, 3 > Const_MArray3d_ref;
 typedef boost::detail::multi_array::multi_array_view<double, 3 > MArray3d_view;
 typedef boost::general_storage_order<3> storage3d;
+typedef Raw_multi_array<double, 3> Raw_MArray3d;
 
 typedef boost::multi_array<std::complex<double >, 1 > MArray1dc;
 typedef boost::multi_array_ref<std::complex<double >, 1 > MArray1dc_ref;
@@ -37,6 +67,7 @@ typedef boost::const_multi_array_ref<std::complex<double >, 2 >
         Const_MArray2dc_ref;
 typedef boost::detail::multi_array::multi_array_view<std::complex<double >, 2 >
         MArray2dc_view;
+typedef Raw_multi_array<std::complex<double >, 2> Raw_MArray2dc;
 
 typedef boost::multi_array<std::complex<double >, 3 > MArray3dc;
 typedef boost::multi_array_ref<std::complex<double >, 3 > MArray3dc_ref;
@@ -49,8 +80,5 @@ typedef boost::multi_array<int, 1 > MArray1int;
 typedef boost::multi_array_ref<int, 1 > MArray1int_ref;
 typedef boost::const_multi_array_ref<int, 1 > Const_MArray1int_ref;
 typedef boost::detail::multi_array::multi_array_view<int, 1 > MArray1int_view;
-
-typedef boost::multi_array_types::index_range range;
-typedef boost::multi_array_types::extent_range extent_range;
 
 #endif /* MULTI_ARRAY_TYPEDEFS_H_ */
