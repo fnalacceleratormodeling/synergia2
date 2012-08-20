7a38ca1f58a1df47bc4c47ef5d54486e8fdc23dc
Chong Shik Park <cspark@fnal.gov>
2012-08-20 13:14:48 -0500

    update pre-calculated cell coords

--- src/synergia/collective/interpolate_rectangular_zyx.h
+++ src/synergia/collective/interpolate_rectangular_zyx.h
@@ -29,36 +29,8 @@ interpolate_rectangular_zyx(double x, double y, double z,
     return val;
 }
 
-inline std::vector<double >
-interpolate_rectangular_2d(double x, double y, double z,
-        Rectangular_grid_domain const& domain, MArray2dc_ref const& a, 
-        MArray1d_ref const& b)
-{
-    // bi-linear interpolation
-    int ix, iy, iz;
-    double offx, offy, offz;
-    bool in_domain = domain.get_leftmost_indices_offsets(x, y, z, ix, iy, iz,
-            offx, offy, offz);
-    std::vector<double > val(2);
-    if (in_domain) {
-        double line_density = ((1.0 - offz) * b[iz] + offz * b[iz + 1]);
-        val[0] = line_density * ((1.0 - offx) * (1.0 - offy) * a[ix][iy].real() 
-                + offx * (1.0 - offy) * a[ix + 1][iy].real() 
-                + (1.0 - offx) * offy * a[ix][iy + 1].real() 
-                + offx * offy * (1.0 - offz) * a[ix + 1][iy + 1].real());
-        val[1] = line_density * ((1.0 - offx) * (1.0 - offy) * a[ix][iy].imag() 
-                + offx * (1.0 - offy) * a[ix + 1][iy].imag() 
-                + (1.0 - offx) * offy * a[ix][iy + 1].imag() 
-                + offx * offy * (1.0 - offz) * a[ix + 1][iy + 1].imag());
-    } else {
-        val[0] = 0.0;
-        val[1] = 0.0;
-    }
-    return val;
-}
-
 inline std::complex<double >
-interpolate_rectangular_2_5d(double x, double y, double z,
+interpolate_rectangular_2d(double x, double y, double z,
         Rectangular_grid_domain const& domain, MArray2dc_ref const& a,
         MArray1d_ref const& b)
 {
@@ -79,7 +51,7 @@ interpolate_rectangular_2_5d(double x, double y, double z,
 }
 
 inline std::complex<double >
-interpolate_rectangular_2_5d(MArray1d_ref const& bin,
+interpolate_rectangular_2d(MArray1d_ref const& bin,
         std::vector<int > const& grid_shape, bool periodic_z,
         MArray2dc_ref const& a, MArray1d_ref const& b)
 {
@@ -93,10 +65,6 @@ interpolate_rectangular_2_5d(MArray1d_ref const& bin,
     offx = bin[1];
     offy = bin[3];
     offz = bin[5];
-    //aoffx = 1.0 - offx;
-    //aoffy = 1.0 - offy;
-    //aoffz = 1.0 - offz;
-    //std::vector<double > val(2);
     std::complex<double > val;
     if (((ix>=0) && (ix<grid_shape[0] - 1) && (iy>=0) && (iy<grid_shape[1] - 1))
         && (periodic_z || ((iz>=0) && (iz<grid_shape[2] - 1)))) {
