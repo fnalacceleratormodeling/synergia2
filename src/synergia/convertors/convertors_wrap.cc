8feedcc59c339cb0658d8ea12648e96cd4a371d6
James Amundson <amundson@fnal.gov>
2012-10-17 14:51:57 -0500

    first pass at numpy_multi_array_converter

--- src/synergia/convertors/convertors_wrap.cc
+++ src/synergia/convertors/convertors_wrap.cc
@@ -1,12 +1,24 @@
 #include <boost/python.hpp>
 #include "boost/multi_array.hpp"
 #include "synergia/utils/multi_array_typedefs.h"
+#include "synergia/utils/numpy_multi_array_converter.h"
 #include "synergia/utils/numpy_multi_ref_converter.h"
 #include "synergia/utils/comm_converter.h"
 #include "synergia/utils/multi_array_serialization.h"
 #include "synergia/utils/serialization.h"
 #include "synergia/utils/serialization_files.h"
 #include "synergia/utils/container_conversions.h"
+
+MArray1d
+get1d()
+{
+    MArray1d a(boost::extents[3]);
+    a[0] = 7.0;
+    a[1] = 8.0;
+    a[2] = 9.0;
+
+    return a;
+}
 
 using namespace boost::python;
 
@@ -15,6 +27,7 @@ BOOST_PYTHON_FUNCTION_OVERLOADS(xml_save_array1d_overloads23,
 BOOST_PYTHON_FUNCTION_OVERLOADS(xml_save_array2d_overloads23,
         xml_save<MArray2d >, 2, 3)
 
+
 BOOST_PYTHON_MODULE(convertors)
 {
     import_array();
@@ -22,6 +35,10 @@ BOOST_PYTHON_MODULE(convertors)
         return;
     }
     comm_converter::register_to_and_from_python();
+
+    numpy_multi_array_converter<double, 1 >::register_to_and_from_python();
+    numpy_multi_array_converter<double, 2 >::register_to_and_from_python();
+
     numpy_multi_array_ref_converter<double, 1 >::register_to_and_from_python();
     numpy_const_multi_array_ref_converter<double, 1 >::register_to_and_from_python();
     numpy_multi_array_ref_converter<double, 2 >::register_to_and_from_python();
@@ -48,4 +65,6 @@ BOOST_PYTHON_MODULE(convertors)
             container_conversions::variable_capacity_policy >();
     container_conversions::from_python_sequence<std::list<int >,
             container_conversions::variable_capacity_policy >();
+
+    def("get1d", &get1d);
 }
