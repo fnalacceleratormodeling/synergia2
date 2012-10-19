feb78b0b28968cb5390107fd8606300c7efc9bb7
Paul Lebrun <lebrun@fnal.gov>
2012-10-19 14:51:54 -0500

    fix interpolation

--- src/synergia/collective/rectangular_grid.h
+++ src/synergia/collective/rectangular_grid.h
@@ -76,14 +76,14 @@ public:
               < 0) || (iz >= get_domain_sptr()->get_grid_shape()[2] - 1)) {
           val = 0.0;
        } else {
-          val = ((1.0 - offz) * (1.0 - offy) * (1.0 - offx) * a[iz][iy][ix]
-                + offz * (1.0 - offy) * (1.0 - offx) * a[iz + 1][iy][ix] + (1.0
-                - offz) * offy * (1.0 - offx) * a[iz][iy + 1][ix]
-                + (1.0 - offz) * (1.0 - offy) * offx * a[iz][iy][ix + 1] + offz
-                * offy * (1.0 - offx) * a[iz + 1][iy + 1][ix] + offz * (1.0
-                - offy) * offx * a[iz + 1][iy][ix + 1] + (1.0 - offz) * offy
-                * offx * a[iz][iy + 1][ix + 1] + offz * offy * offx
-                * a[iz + 1][iy + 1][ix + 1]);
+          val = ( (1.0 - offx) * (1.0 - offy) * (1.0 - offz) * a[ix][iy][iz]
+                + (1.0 - offx) * (1.0 - offy) * offz * a[ix][iy][iz + 1] 
+		+ (1.0 - offx) * offy * (1.0 - offz) * a[ix][iy + 1][iz]
+		+ (1.0 - offx) * offy * offz * a[ix][iy + 1][iz + 1] 
+                + offx * (1.0 - offy) * (1.0 - offz) * a[ix + 1][iy][iz] 
+		+ offx * (1.0 - offy) * offz * a[ix + 1][iy][iz + 1] 
+		+ offx * offy * (1.0 - offz) * a[ix + 1][iy + 1][iz] 
+		+ offx * offy * offz * a[ix + 1][iy + 1][iz + 1]);
          }
        } else if (get_domain_sptr()->get_grid_shape()[0] == 1) {
 	    // 2D,  Y-Z plane
