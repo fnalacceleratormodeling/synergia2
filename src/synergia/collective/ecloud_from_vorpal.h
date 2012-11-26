e92f013d1e43a181058155c08f6bf1fc09afecc2
James Amundson <amundson@fnal.gov>
2012-11-26 15:46:34 -0600

    add typedef doc comments

--- src/synergia/collective/ecloud_from_vorpal.h
+++ src/synergia/collective/ecloud_from_vorpal.h
@@ -38,4 +38,4 @@ public:
 #endif // ECLOUD_FROM_VORPAL_H
 BOOST_CLASS_EXPORT_KEY(Ecloud_from_vorpal)
 
-typedef boost::shared_ptr<Ecloud_from_vorpal > Ecloud_from_vorpal_sptr;
+typedef boost::shared_ptr<Ecloud_from_vorpal > Ecloud_from_vorpal_sptr; // syndoc:include
