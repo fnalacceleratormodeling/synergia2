ec7febb70838ae24266e7395634460bb7179c73c
James Amundson <amundson@fnal.gov>
2012-11-26 15:41:39 -0600

    add typedef doc comments

--- src/synergia/simulation/propagate_actions.h
+++ src/synergia/simulation/propagate_actions.h
@@ -27,6 +27,6 @@ public:
     ~Propagate_actions();
 };
 
-typedef boost::shared_ptr<Propagate_actions > Propagate_actions_sptr;
+typedef boost::shared_ptr<Propagate_actions > Propagate_actions_sptr; // syndoc:include
 
 #endif /* PROPAGATE_ACTIONS_H_ */
