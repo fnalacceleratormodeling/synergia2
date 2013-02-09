1ebe659d7a2bdb7548df423bd2b7c9f627a4f748
James Amundson <amundson@fnal.gov>
2013-02-09 09:42:29 -0600

    test_propagator: remove obsolete code

--- src/synergia/simulation/tests/test_propagator.cc
+++ src/synergia/simulation/tests/test_propagator.cc
@@ -16,14 +16,6 @@ BOOST_GLOBAL_FIXTURE(MPI_fixture)
 
 const double tolerance = 1.0e-12;
 
-struct Object_to_sptr_hack
-{
-    void
-    operator()(void const *) const
-    {
-    }
-};
-
 BOOST_FIXTURE_TEST_CASE(construct, Lattice_fixture)
 {
     Dummy_collective_operator_sptr space_charge(
@@ -122,61 +114,6 @@ BOOST_FIXTURE_TEST_CASE(propagate_max_turns, Lattice_fixture)
     propagator.resume(second_checkpoint, true, 2, false, 0);
 }
 
-//BOOST_FIXTURE_TEST_CASE(propagate1, Lattice_fixture)
-//{
-//
-//    Dummy_collective_operator_sptr space_charge(new Dummy_collective_operator(
-//            "space_charge"));
-//    Lattice_simulator lattice_simulator(lattice_sptr, 2);
-//
-//    Split_operator_stepper_sptr stepper_sptr(new Split_operator_stepper(
-//            lattice_simulator, space_charge, 4));
-//    Propagator propagator(stepper_sptr);
-//
-//    Bunch_sptr bunch_sptr(&b.bunch,Object_to_sptr_hack());
-//
-//    Diagnostics_full2_sptr per_step_diagnostics1(new Diagnostics_full2(bunch_sptr,
-//            "test_propagate_per_step1.h5"));
-//    Diagnostics_full2_sptr per_step_diagnostics2(new Diagnostics_full2(bunch_sptr,
-//            "test_propagate_per_step2.h5"));
-//    Multi_diagnostics multi_diagnostics_step;
-//    multi_diagnostics_step.append(per_step_diagnostics1);
-//    multi_diagnostics_step.append(per_step_diagnostics2);
-//
-//    Diagnostics_particles_sptr per_turn_diagnostics1(new Diagnostics_particles(bunch_sptr,
-//            "test_propagate_per_turn1.h5"));
-//    Diagnostics_particles_sptr per_turn_diagnostics2(new Diagnostics_particles(bunch_sptr,
-//            "test_propagate_per_turn2.h5"));
-//    Multi_diagnostics multi_diagnostics_turn;
-//    multi_diagnostics_turn.append(per_turn_diagnostics1);
-//    multi_diagnostics_turn.append(per_turn_diagnostics2);
-//
-//    int num_turns = 4;
-//    propagator.propagate(b.bunch, num_turns, multi_diagnostics_step,
-//            multi_diagnostics_turn);
-//
-//}
-
-//BOOST_FIXTURE_TEST_CASE(propagate2, Lattice_fixture)
-//{
-//    Dummy_collective_operator_sptr space_charge(new Dummy_collective_operator(
-//            "space_charge"));
-//    Lattice_simulator lattice_simulator(lattice_sptr, 2);
-//
-//    Split_operator_stepper_sptr stepper_sptr(new Split_operator_stepper(
-//            lattice_simulator, space_charge, 4));
-//    Propagator propagator(stepper_sptr);
-//
-//    Bunch_sptr bunch_sptr(new Bunch(b.bunch));
-//
-//    Diagnostics_particles per_turn_diagnostics(bunch_sptr,
-//            "test_propagate_per_turn.h5");
-//    Diagnostics_basic per_step_diagnostics(bunch_sptr,
-//            "test_propagate_per_step.h5");
-//    int num_turns = 4;
-//    propagator.propagate(*bunch_sptr, num_turns, per_step_diagnostics,
-//            per_turn_diagnostics);
-//}
 BOOST_FIXTURE_TEST_CASE(serialize_xml, Lattice_fixture)
 {
     Dummy_collective_operator_sptr space_charge(
