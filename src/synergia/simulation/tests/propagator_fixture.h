7c509d1a2b2741bc6f5c4edfd04c3fe4619cbb53
James Amundson <amundson@fnal.gov>
2013-02-11 16:45:02 -0600

    add bunches to Propagator_fixture

--- src/synergia/simulation/tests/propagator_fixture.h
+++ src/synergia/simulation/tests/propagator_fixture.h
@@ -2,11 +2,15 @@
 #define PROPAGATOR_FIXTURE_H_
 
 #include "lattice_fixture.h"
+namespace kludge
+{
+#include "synergia/bunch/tests/bunches_fixture.h"
+}
 
 struct Propagator_fixture
 {
     Propagator_fixture() :
-            l(), space_charge(new Dummy_collective_operator("space_charge")), lattice_simulator(
+            l(), bs(), space_charge(new Dummy_collective_operator("space_charge")), lattice_simulator(
                     l.lattice_sptr, 2), stepper_sptr(
                     new Split_operator_stepper(lattice_simulator, space_charge,
                             7)), propagator(stepper_sptr)
@@ -21,6 +25,7 @@ struct Propagator_fixture
     ;
 
     Lattice_fixture l;
+    kludge::Bunches_fixture bs;
     Dummy_collective_operator_sptr space_charge;
     Lattice_simulator lattice_simulator;
     Split_operator_stepper_sptr stepper_sptr;
