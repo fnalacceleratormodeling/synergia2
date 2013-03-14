9c69a4b619714ee9bcc48e7377128456ff5f721c
Eric G. Stern <egstern@fnal.gov>
2013-03-14 13:31:09 -0500

    scripts for running jobs on intrepid/bgp

---
+++ synergia-script-templates/local_opts_intrepid.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# The local_options file must be named local_opts.py and placed
+# in the Synergia2 job manager search path.
+
+from synergia_workflow import options
+
+# Any instance of the Options class will be added as a suboption.
+opts = options.Options('local')
+
+# Any instance of the Override class will be used to override
+#   the defaults of the existing options.
+override = options.Override()
+# change the next line with your ALCF account
+override.account = "YourAccountHere"
+override.numproc = 4096
+override.procspernode=4
+override.template="job_intrepid"
+override.queue="prod"
+override.walltime="24:00:00"
