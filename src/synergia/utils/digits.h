985981106c78a4d4bc1f39df62771454348badef
James Amundson <amundson@fnal.gov>
2013-04-04 14:53:38 -0500

    add decimal_digits function

--- src/synergia/utils/digits.h
+++ src/synergia/utils/digits.h
@@ -1,6 +1,7 @@
 #ifndef DIGITS_H_
 #define DIGITS_H_
 
+inline
 long int
 digits(long int val)
 {
@@ -9,6 +10,21 @@ digits(long int val)
     for (int i = 0; i < 9; ++i) {
         base *= 10;
         if (val > base) {
+            retval += 1;
+        }
+    }
+    return retval;
+}
+
+inline
+int
+decimal_digits(double val)
+{
+    int retval = 1;
+    double base = 1;
+    for (int i = 0; i < 14; ++i) {
+        base *= 0.1;
+        if (std::abs(val) < base) {
             retval += 1;
         }
     }
