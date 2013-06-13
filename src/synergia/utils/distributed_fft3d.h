f2db19c1bd7d61470e5d66ec287c7ca71c121abf
Qiming Lu <qlu@fnal.gov>
2013-06-13 18:12:35 +0000

    1. distributed_fft3d now uses mpi+openmp enabled fftw 2. added methods to transform into array of fftw native data types (fftw_complex)

--- src/synergia/utils/distributed_fft3d.h
+++ src/synergia/utils/distributed_fft3d.h
@@ -24,12 +24,34 @@ private:
     fftw_plan plan, inv_plan;
     double *data;
     fftw_complex *workspace;
+    fftw_complex *workspace1;
+    fftw_complex *workspace2;
 #endif //USE_FFTW2
     int lower, upper;
     std::vector<int> uppers, lengths;
     int local_size_real, local_size_complex;
     std::vector<int > shape;
     bool have_local_data;
+
+    int nt;   // number of threads
+    int np;   // number of mpi procs
+    int rank; // mpi rank
+
+    int n0, n1, n2, n2r, n2c;
+    fftw_plan fp1, fp2, fp3;
+    fftw_plan bp1, bp2, bp3;
+
+    // complex type (contiguous of two doubles)
+    MPI_Datatype mpi_cplx;
+
+    // data types for the all to all BEFORE 3rd dim fft
+    MPI_Datatype mpi_fs11, mpi_fs12;
+    MPI_Datatype mpi_fr11, mpi_fr12, mpi_fr13, mpi_fr14;
+
+    // data types for the all to all AFTER 3rd dim fft
+    MPI_Datatype mpi_fs21, mpi_fs22, mpi_fs23;
+    MPI_Datatype mpi_fr21, mpi_fr22;
+
     Commxx_sptr comm_sptr;
     void
     calculate_uppers_lengths();
@@ -41,6 +63,12 @@ public:
     get_comm_sptr();
     Commxx &
     get_comm();
+    int
+    get_nt() const { return nt; }
+    int
+    get_np() const { return np; }
+    int
+    get_rank() const { return rank; }
     int
     get_lower() const;
     int
@@ -58,7 +86,11 @@ public:
     void
     transform(MArray3d_ref & in, MArray3dc_ref & out);
     void
+    transform(MArray3d_ref & in, fftw_complex * out);
+    void
     inv_transform(MArray3dc_ref & in, MArray3d_ref & out);
+    void
+    inv_transform(fftw_complex * in, MArray3d_ref & out);
     double
     get_roundtrip_normalization() const;
     ~Distributed_fft3d();
