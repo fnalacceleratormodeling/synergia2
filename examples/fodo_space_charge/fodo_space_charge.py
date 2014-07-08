b5f7f0b8b806b831ad2cb290fec01693b7192e29
James Amundson <amundson@fnal.gov>
2014-07-08 15:46:31 -0500

    add space charge

--- examples/fodo_space_charge/fodo_space_charge.py
+++ examples/fodo_space_charge/fodo_space_charge.py
@@ -2,10 +2,15 @@
 import synergia
 
 from synergia.foundation import Four_momentum, Reference_particle, pconstants
+from synergia.utils import Commxx
 from synergia.lattice import MadX_reader, Lattice
-from synergia.bunch import Bunch, Diagnostics_basic, Diagnostics_track
-from synergia.simulation import Independent_stepper_elements, Bunch_simulator, \
-    Propagator
+from synergia.bunch import Bunch, Diagnostics_full2
+from synergia.simulation import Split_operator_stepper, \
+    Split_operator_stepper_elements, Dummy_collective_operator, \
+    Bunch_simulator, Propagator
+from synergia.collective import Space_charge_2d_bassetti_erskine, \
+    Space_charge_2d_open_hockney, \
+    Space_charge_3d_open_hockney
 
 # Get options from separate options file
 from fodo_space_charge_options import opts
@@ -14,11 +19,29 @@ from fodo_space_charge_options import opts
 #     Read the lattice named "fodo" from the MadX file "fodo.madx"
 lattice = synergia.lattice.MadX_reader().get_lattice("fodo", "fodo.madx")
 
+# Define a space charge operator
+if opts.space_charge == "none":
+    space_charge = Dummy_collective_operator("null space charge")
+elif opts.space_charge == "bassetti":
+    space_charge = Space_charge_2d_bassetti_erskine()
+else:
+    # Set up parallel space charge solvers
+    # MPI communicator, True to use simple communication avoidance
+    commxx = Commxx(True)
+    grid = [opts.gridx, opts.gridy, opts.gridz]
+    if opts.space_charge == "2d":
+        space_charge = Space_charge_2d_open_hockney(commxx, grid)
+    elif opts.space_charge == "3d":
+        space_charge = Space_charge_3d_open_hockney(commxx, grid)
 
 # Define a set of simulation steps
-stepper = Independent_stepper_elements(lattice, opts.map_order, 
-                                       opts.steps_per_element)
-
+if opts.stepper == "element":
+    stepper = Split_operator_stepper_elements(lattice, opts.map_order,
+                                              space_charge,
+                                              opts.steps_per_element)
+else:
+    stepper = Split_operator_stepper(lattice, opts.map_order,
+                                     space_charge, opts.num_steps)
 # Define a bunch
 bunch = synergia.optics.generate_matched_bunch_transverse(
               stepper.get_lattice_simulator(),
--- examples/fodo_space_charge/fodo_space_charge_options.py
+++ examples/fodo_space_charge/fodo_space_charge_options.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 
-from synergia_space_charge import Options, Job_manager
+from synergia_workflow import Options, Job_manager
 
 opts = Options("fodo_space_charge")
 opts.add("map_order", 1, "map order")
-opts.add("steps_per_element", 2, "steps per element")
+opts.add("stepper", "element", 
+  "split operator stepper, either fixed steps per element or fixed length steps",
+  valid_values=["element", "fixed"])
+opts.add("steps_per_element", 2, "steps per element (only for element stepper)")
+opts.add("num_steps", 8, "total number of steps (only for fixed length stepper)")
+opts.add("space_charge", "2d", "space charge model", 
+  valid_values=["none", "bassetti", "2d", "3d"])
+opts.add("gridx", 16, "grid cells in x-direction (2d and 3d space charge)")
+opts.add("gridy", 16, "grid cells in y-direction (2d and 3d space charge)")
+opts.add("gridz", 16, "grid cells in z-direction (2d and 3d space charge)")
 opts.add("x_emit", 1.0e-6, "x RMS emittance [m-rad]")
 opts.add("y_emit", 1.0e-6, "y RMS emittance [m-rad]")
 opts.add("z_std", 0.01, "z RMS length [m]")
