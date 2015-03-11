d8f6ac2def203dbb59732197f90fb98aabb17dd2
James Amundson <amundson@fnal.gov>
2015-03-10 19:59:54 -0500

    add FF_drift

---
+++ src/synergia/libFF/ff_drift.cc
@@ -0,0 +1,121 @@
+#include "ff_drift.h"
+#include "synergia/lattice/chef_utils.h"
+
+struct Chef
+{
+    static const int x = 0;
+    static const int xp = 3;
+    static const int y = 1;
+    static const int yp = 4;
+    static const int cdt = 2;
+    static const int dpop = 5;
+};
+
+FF_drift::FF_drift()
+{
+}
+
+double FF_drift::get_reference_cdt(double length,
+                                   Reference_particle & reference_particle)
+{
+    double x(reference_particle.get_state()[Bunch::x]);
+    double xp(reference_particle.get_state()[Bunch::xp]);
+    double y(reference_particle.get_state()[Bunch::y]);
+    double yp(reference_particle.get_state()[Bunch::yp]);
+    double cdt(reference_particle.get_state()[Bunch::cdt]);
+    double dpop(reference_particle.get_state()[Bunch::dpop]);
+    double reference_momentum = reference_particle.get_momentum();
+    double m = reference_particle.get_mass();
+
+    double cdt_orig = cdt;
+    drift_unit(x, y, cdt, xp, yp, dpop, length, reference_momentum, m, 0.0);
+
+    return cdt - cdt_orig;
+}
+
+void FF_drift::apply(Lattice_element_slice const& slice, JetParticle& jet_particle)
+{
+    double length = slice.get_right() - slice.get_left();
+
+    typedef PropagatorTraits<JetParticle>::State_t State_t;
+    typedef PropagatorTraits<JetParticle>::Component_t Component_t;
+
+    State_t& state = jet_particle.State();
+
+    Component_t x(state[Chef::x]);
+    Component_t xp(state[Chef::xp]);
+    Component_t y(state[Chef::y]);
+    Component_t yp(state[Chef::yp]);
+    Component_t cdt(state[Chef::cdt]);
+    Component_t dpop(state[Chef::dpop]);
+
+    double reference_momentum = jet_particle.ReferenceMomentum();
+    double m = jet_particle.Mass();
+
+    Particle chef_particle(jet_particle);
+    Reference_particle reference_particle(
+                chef_particle_to_reference_particle(chef_particle));
+    double reference_cdt = get_reference_cdt(length, reference_particle);
+
+    drift_unit(x, y, cdt, xp, yp, dpop, length, reference_momentum, m,
+               reference_cdt);
+}
+
+void FF_drift::apply(Lattice_element_slice const& slice, Bunch& bunch)
+{
+     double length = slice.get_right() - slice.get_left();
+     int local_num = bunch.get_local_num();
+     MArray2d_ref particles = bunch.get_local_particles();
+     double reference_momentum = bunch.get_reference_particle().get_momentum();
+     double m = bunch.get_mass();
+     double reference_cdt = get_reference_cdt(length,
+                                              bunch.get_reference_particle());
+
+     for (int part = 0; part < local_num; ++part) {
+         double x(particles[part][Bunch::x]);
+         double xp(particles[part][Bunch::xp]);
+         double y(particles[part][Bunch::y]);
+         double yp(particles[part][Bunch::yp]);
+         double cdt(particles[part][Bunch::cdt]);
+         double dpop(particles[part][Bunch::dpop]);
+
+         drift_unit(x, y, cdt, xp, yp, dpop, length, reference_momentum, m,
+                    reference_cdt);
+
+         particles[part][Bunch::x] = x;
+         particles[part][Bunch::y] = y;
+         particles[part][Bunch::cdt] = cdt;
+    }
+}
+
+template<class Archive>
+    void
+    FF_drift::serialize(Archive & ar, const unsigned int version)
+    {
+        ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(FF_element);
+    }
+
+template
+void
+FF_drift::serialize<boost::archive::binary_oarchive >(
+        boost::archive::binary_oarchive & ar, const unsigned int version);
+
+template
+void
+FF_drift::serialize<boost::archive::xml_oarchive >(
+        boost::archive::xml_oarchive & ar, const unsigned int version);
+
+template
+void
+FF_drift::serialize<boost::archive::binary_iarchive >(
+        boost::archive::binary_iarchive & ar, const unsigned int version);
+
+template
+void
+FF_drift::serialize<boost::archive::xml_iarchive >(
+        boost::archive::xml_iarchive & ar, const unsigned int version);
+
+FF_drift::~FF_drift()
+{
+
+}
---
+++ src/synergia/libFF/ff_drift.h
@@ -0,0 +1,42 @@
+#ifndef FF_DRIFT_H
+#define FF_DRIFT_H
+
+#include "synergia/libFF/ff_element.h"
+
+class FF_drift : public FF_element
+{
+private:
+    double get_reference_cdt(double length, Reference_particle & reference_particle);
+public:
+    FF_drift();
+    template <typename T>
+    inline static void drift_unit(T & x, T & y, T & cdt,
+                                  T const& xp, T const& yp, T const& dpop,
+                                  double length, double reference_momentum,
+                                  double m, double reference_cdt);
+    virtual void apply(Lattice_element_slice const& slice, JetParticle & jet_particle);
+    virtual void apply(Lattice_element_slice const& slice, Bunch & bunch);
+    template<class Archive>
+        void serialize(Archive & ar, const unsigned int version);
+    virtual ~FF_drift();
+};
+
+#include "synergia/utils/invsqrt.h"
+
+template <typename T>
+void FF_drift::drift_unit(T & x, T & y, T & cdt,
+                          T const& xp, T const& yp, T const& dpop,
+                          double length, double reference_momentum,
+                          double m, double reference_cdt) {
+    T inv_npz = invsqrt((dpop + 1.0) * (dpop + 1.0) - xp * xp - yp * yp);
+    T lxpr = xp * length * inv_npz;
+    T lypr = yp * length * inv_npz;
+    T D2 = lxpr * lxpr + length * length + lypr * lypr;
+    T p = dpop * reference_momentum + reference_momentum;
+    T E2 = p * p + m * m;
+    T beta2 = p*p / E2;
+    x += lxpr;
+    y += lypr;
+    cdt += sqrt(D2 / beta2) - reference_cdt;
+}
+#endif // FF_DRIFT_H
