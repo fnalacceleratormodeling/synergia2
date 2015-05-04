e82e4ecafbe3f65018246093776d9185a111f531
James Amundson <amundson@mac-119764.dhcp.fnal.gov>
2015-05-04 11:38:49 -0500

    refactor hdf5_file test to use fixture

--- src/synergia/utils/tests/test_hdf5_file.cc
+++ src/synergia/utils/tests/test_hdf5_file.cc
@@ -38,34 +38,35 @@ BOOST_AUTO_TEST_CASE(write_data)
     file.write(a3d, "a3d");
 }
 
-const double tolerance = 1.0e-13;
-BOOST_AUTO_TEST_CASE(read_write_data)
+namespace
 {
-    const char * filename = "hdf5_file_read_write.h5";
+    const char * filename = "hdf5_file.h5";
     const char * int_label = "int_data";
     const char * double_label = "double_data";
     const char * array1d_label = "array1d_data";
     const char * array2d_label = "array2d_data";
     const char * array3d_label = "array3d_data";
-    int int_data = 7;
-    double double_data = 2.71828;
-    const int dim1 = 2;
-    const int dim2 = 3;
-    const int dim3 = 4;
-    MArray1d a1d(boost::extents[dim1]);
-    MArray2d a2d(boost::extents[dim1][dim2]);
-    MArray3d a3d(boost::extents[dim1][dim2][dim3]);
-    for (int j = 0; j < dim1; ++j) {
-        a1d[j] = 100 * j;
-        for (int k = 0; k < dim2; ++k) {
-            a2d[j][k] = 10 * k + 100 * j;
-            for (int l = 0; l < dim2; ++l) {
-                a3d[j][k][l] = 1.1 * l + 10 * k + 100 * j;
+    const int num_members = 5;
+}
+
+struct Hdf5_file_fixture
+{
+    Hdf5_file_fixture() :
+        int_data(7),
+        double_data(2.71828),
+        a1d(boost::extents[dim1]),
+        a2d(boost::extents[dim1][dim2]),
+        a3d(boost::extents[dim1][dim2][dim3])
+    {
+        for (int j = 0; j < dim1; ++j) {
+            a1d[j] = 100 * j;
+            for (int k = 0; k < dim2; ++k) {
+                a2d[j][k] = 10 * k + 100 * j;
+                for (int l = 0; l < dim2; ++l) {
+                    a3d[j][k][l] = 1.1 * l + 10 * k + 100 * j;
+                }
             }
         }
-    }
-
-    {
         Hdf5_file write_file(filename, Hdf5_file::truncate);
         write_file.write(int_data, int_label);
         write_file.write(double_data, double_label);
@@ -73,20 +74,35 @@ BOOST_AUTO_TEST_CASE(read_write_data)
         write_file.write(a2d, array2d_label);
         write_file.write(a3d, array3d_label);
     }
-
+    ~Hdf5_file_fixture()
     {
-        Hdf5_file read_file(filename, Hdf5_file::read_only);
-        int int_read = read_file.read<int > (int_label);
-        BOOST_CHECK_EQUAL(int_read, int_data);
-        double double_read = read_file.read<double > (double_label);
-        BOOST_CHECK_CLOSE(double_read, double_data, tolerance);
-        MArray1d a1d_read(read_file.read<MArray1d > (array1d_label));
-        multi_array_check_equal(a1d_read, a1d, tolerance);
-        MArray2d a2d_read(read_file.read<MArray2d > (array2d_label));
-        multi_array_check_equal(a2d_read, a2d, tolerance);
-        MArray3d a3d_read(read_file.read<MArray3d > (array3d_label));
-        multi_array_check_equal(a3d_read, a3d, tolerance);
     }
+
+    int int_data;
+    double double_data;
+    static const int dim1 = 2;
+    static const int dim2 = 3;
+    static const int dim3 = 4;
+    MArray1d a1d;
+    MArray2d a2d;
+    MArray3d a3d;
+};
+
+
+const double tolerance = 1.0e-13;
+BOOST_FIXTURE_TEST_CASE(read_data, Hdf5_file_fixture)
+{
+    Hdf5_file read_file(filename, Hdf5_file::read_only);
+    int int_read = read_file.read<int > (int_label);
+    BOOST_CHECK_EQUAL(int_read, int_data);
+    double double_read = read_file.read<double > (double_label);
+    BOOST_CHECK_CLOSE(double_read, double_data, tolerance);
+    MArray1d a1d_read(read_file.read<MArray1d > (array1d_label));
+    multi_array_check_equal(a1d_read, a1d, tolerance);
+    MArray2d a2d_read(read_file.read<MArray2d > (array2d_label));
+    multi_array_check_equal(a2d_read, a2d, tolerance);
+    MArray3d a3d_read(read_file.read<MArray3d > (array3d_label));
+    multi_array_check_equal(a3d_read, a3d, tolerance);
 }
 
 
