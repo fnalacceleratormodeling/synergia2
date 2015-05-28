cf1944706af43ba9c9d45c100964d3dd7085aed3
Qiming Lu <qlu@fnal.gov>
2015-05-28 16:14:26 -0500

    test of sbend element for libff

--- src/synergia/simulation/tests/test_beamline_element_dynamics_libff.cc
+++ src/synergia/simulation/tests/test_beamline_element_dynamics_libff.cc
@@ -144,6 +144,223 @@ BOOST_AUTO_TEST_CASE(drift_propagation)
     BOOST_CHECK(floating_point_equal(local_particles[3][Bunch::dpop], dpp_offset, tolerance));
 }
 
+BOOST_AUTO_TEST_CASE(sbend_propagation_libff)
+{
+    const double pc = 2.784435311;
+    const double sbend_length = 1.09812;
+    const double sbend_angle = 0.01567741327;
+    const double dpp_offset = 0.04;
+    const double x_offset = .001;
+    const double sbend_tolerance = 1.0e-7;
+
+    const double real_particles = 1.0e9;
+    const int map_order = 1;    // not really used for step for test
+
+    // create chef lattice
+    Lattice_sptr lattice_sptr(new Lattice("my_lattice"));
+
+    Lattice_element my_sbend("sbend", "my_sbend");
+    my_sbend.set_double_attribute("l", sbend_length);
+    my_sbend.set_double_attribute("angle", sbend_angle);
+    my_sbend.set_string_attribute("extractor_type", "libff");
+    //my_sbend.set_string_attribute("extractor_type", "chef_propagate");
+    lattice_sptr->append(my_sbend);
+#if 0
+    lattice_sptr->print();
+#endif
+
+    Four_momentum four_momentum(pconstants::proton_mass);
+    four_momentum.set_momentum(pc);
+
+    Reference_particle reference_particle(1, four_momentum);
+    lattice_sptr->set_reference_particle(reference_particle);
+
+    double p0 = pc;
+    double beta = reference_particle.get_beta();
+
+    Commxx_sptr commxx(new Commxx());
+
+    Bunch_sptr bunch_sptr(new Bunch(reference_particle, 4, real_particles, commxx));
+    Bunch_simulator bunch_simulator(bunch_sptr);
+
+    MArray2d_ref local_particles(bunch_sptr->get_local_particles());
+
+    Independent_stepper_sptr stepper_sptr(new Independent_stepper(lattice_sptr, map_order, 1));
+    Propagator propagator(stepper_sptr);
+
+    // reference particle at 0
+    MArray1d ref_state(reference_particle.get_state());
+    for (int i=0; i<6; ++i) {
+        local_particles[0][i] = ref_state[i];
+    }
+
+    // test straight drift central particle
+    local_particles[1][Bunch::x] = 0.0;
+    local_particles[1][Bunch::xp] = 0.0;
+    local_particles[1][Bunch::y] = 0.0;
+    local_particles[1][Bunch::yp] = 0.0;
+    local_particles[1][Bunch::cdt] = 0.0;
+    local_particles[1][Bunch::dpop] = 0.0;
+
+    // at offset dpp
+    local_particles[2][Bunch::x] = 0.0;
+    local_particles[2][Bunch::xp] = 0.0;
+    local_particles[2][Bunch::y] = 0.0;
+    local_particles[2][Bunch::yp] = 0.0;
+    local_particles[2][Bunch::cdt] = 0.0;
+    local_particles[2][Bunch::dpop] = dpp_offset;
+
+    // at offset x
+    local_particles[3][Bunch::x] = x_offset;
+    local_particles[3][Bunch::xp] = 0.0;
+    local_particles[3][Bunch::y] = 0.0;
+    local_particles[3][Bunch::yp] = 0.0;
+    local_particles[3][Bunch::cdt] = 0.0;
+    local_particles[3][Bunch::dpop] = 0.0;
+
+    propagator.propagate(bunch_simulator, 1);
+
+    // subtract reference particle cdt
+    for (int j=1; j<4; ++j)
+        local_particles[j][Bunch::cdt] -= local_particles[0][Bunch::cdt];
+
+    BOOST_CHECK(floating_point_equal(local_particles[1][Bunch::x], 0.0, tolerance));
+    BOOST_CHECK(floating_point_equal(local_particles[1][Bunch::xp], 0.0, tolerance));
+    BOOST_CHECK(floating_point_equal(local_particles[1][Bunch::y], 0.0, tolerance));
+    BOOST_CHECK(floating_point_equal(local_particles[1][Bunch::yp], 0.0, tolerance));
+    BOOST_CHECK(floating_point_equal(local_particles[1][Bunch::cdt], 0.0, tolerance));
+    BOOST_CHECK(floating_point_equal(local_particles[1][Bunch::dpop], 0.0, tolerance));
+
+
+    // calculate where it should be
+
+    double brho = p0 * 1.0e9/pconstants::c;
+    double magnetic_field = sbend_angle*brho/sbend_length;
+
+    double radius = brho/magnetic_field; // radius of curvature for on-momentum
+
+    /*
+     * on-momentum particle moves around the circle of radius
+     * Make the center of the circle (0, -radius)
+     * the nominal particle travels length l.  The sbend edge will be
+     * the line that includes the (0, -radius) and the end of travel.
+     */
+    double sbend_exit_x = radius * std::sin(sbend_angle);
+    double sbend_exit_y = -radius * (1.0 - std::cos(sbend_angle));
+
+    double m = pconstants::proton_mass;
+    double p1 = p0 * (1.0 + dpp_offset);
+    double E1 = sqrt(p1*p1 + m*m);
+    double igamma1 = m / E1;
+    double beta1  = sqrt(1.0 - igamma1 * igamma1); 
+    double radius1 = (p1/magnetic_field) * (1.0e9/pconstants::c);
+    double delta_radius = radius1-radius;
+    double cot_theta = std::cos(sbend_angle)/std::sin(sbend_angle);
+
+    /*
+     * particle 1 exit point at the intersection of the circle of radius
+     * radius1 centered at
+     * (0, -radius1) with line from (0,-radius), (sbend_exit_x, sbend_exit_y)
+     * which works out to be
+     * y = -radius + cos(angle)/sin(angle)*x
+     *
+     *  intersection with circle
+     * 
+     * x**2 + (y + radius1)**2 = radius1**2
+     *
+     * substitute
+     * (1+ct**2)*x**2 + 2*ct*dr*x + dr**2-radius1**2 = 0 and
+     * solve quadratic equation
+     *
+     * px/pref calculation L. Michelotti:
+     * px/pref = (dp/pref) * sin(theta_0)
+     *
+     */
+    double a = 1.0 + cot_theta*cot_theta;
+    double b = 2.0*cot_theta*delta_radius;
+    double c = delta_radius*delta_radius - radius1*radius1;
+
+    // quadratic solution (for 1/x, inverted)
+    double sbend_exit_x1 = 2*c/(-b - std::sqrt(b*b - 4*a*c));
+    double sbend_exit_y1 = -radius + cot_theta*sbend_exit_x1;
+    double d_offset = std::sqrt(std::pow(sbend_exit_x-sbend_exit_x1,2) +
+                                std::pow(sbend_exit_y-sbend_exit_y1,2));
+
+    // offset distance is the x offset in accelerator coordinates
+    BOOST_CHECK(floating_point_equal(d_offset, local_particles[2][Bunch::x], sbend_tolerance));
+
+    // check px/pref
+    double px_over_pref = dpp_offset * std::sin(sbend_angle);
+    BOOST_CHECK(floating_point_equal(px_over_pref, local_particles[2][Bunch::xp], sbend_tolerance));
+
+    // check path length difference
+    // path length for particle 0 is just sbend_length
+    // calculate path length for particle 1 on its circular trajectory
+    // origin of circle is (0, -radius1)
+    // vector of particle start position is (0, radius1)
+    // vector of particle end position is (sbend_exit_x1, sbend_exit_y1-(-radius1))
+    // calculate dot product to get angle
+    double v0dotv1 = sbend_exit_y1+radius1; // *radius1 which will cancel
+    double v1_norm = std::sqrt(pow(sbend_exit_x1, 2) +
+                               pow(sbend_exit_y1+radius1, 2));
+    double theta1 = std::acos(v0dotv1/v1_norm);  // * 1/radius1
+    double sbend_length1 = theta1 * radius1;
+    double delta_cdt = sbend_length1/beta1 - sbend_length/beta;
+    BOOST_CHECK(floating_point_equal(delta_cdt, local_particles[2][Bunch::cdt], sbend_tolerance));
+
+
+    // In this case, the trajectory follows a circle of original radius
+    // but centered on (0, -R+x_offset).  The exit point is the intersection of that
+    // circle with the line of close cot(theta) passing through (0, -R) which is the solution
+    // of the quadratic equation
+    // (1+ct**2)*x**2 - 2*ct*x_offset*x + x_offset**2-radius**2 = 0
+    a = 1.0 + cot_theta*cot_theta;
+    b = -2.0*cot_theta*x_offset;
+    c = x_offset*x_offset - radius*radius;
+    // quadratic solution (for 1/x, inverted)
+    double sbend_exit_x2 = 2*c/(-b - std::sqrt(b*b - 4*a*c));
+    double sbend_exit_y2 = -radius + cot_theta*sbend_exit_x2;
+    d_offset = std::sqrt(std::pow(sbend_exit_x-sbend_exit_x2,2) +
+                                std::pow(sbend_exit_y-sbend_exit_y2,2));
+    // offset distance is the x offset in accelerator coordinates
+#if 0
+    std::cout << "central  exit position: " << sbend_exit_x  << ", " << sbend_exit_y  << std::endl;
+    std::cout << "particle exit position: " << sbend_exit_x2 << ", " << sbend_exit_y2 << std::endl;
+    std::cout << "proton.get_x(): " << proton.get_x() << ", d_offset: " << d_offset << std::endl;
+#endif
+    BOOST_CHECK(floating_point_equal(local_particles[3][Bunch::x], d_offset, sbend_tolerance));
+
+    // get pathlength
+    // start position vector is (0, -radius)
+    // final position vector is (sbend_exit_x2, sbend_exit_y2-(-radius+x_offset)
+    double v0dotv2 = sbend_exit_y2+radius-x_offset; // times radius
+    double v2norm = std::sqrt(pow(sbend_exit_x2, 2) +
+    		                  pow(sbend_exit_y2-radius+x_offset, 2));
+    double theta2 = std::acos(v0dotv2/radius);
+    double sbend_length2 = theta2 * radius;
+    delta_cdt = sbend_length2/beta - sbend_length/beta;
+#if 0
+    std::cout << "proton.get_cdt(): " << proton.get_cdt() << ", delta_cdt: " << delta_cdt << std::endl;
+#endif
+    BOOST_CHECK(floating_point_equal(local_particles[3][Bunch::cdt], delta_cdt, sbend_tolerance));
+
+    // check x'.  Transverse momentum is the sin of the angle between the
+    // vector normal to the sbend exit face and the particle trajectory vector.
+    // calculate with the cross product.
+    double exit_normal_x = (sbend_exit_y2-(-radius+x_offset))/radius;
+    double exit_normal_y = -sbend_exit_x2/radius;
+    double face_normal_x = std::cos(sbend_angle);
+    double face_normal_y = -std::sin(sbend_angle);
+    // cross product
+    double exit_px = face_normal_x*exit_normal_y - face_normal_y*exit_normal_x;
+#if 0
+    std::cout << "exit_px: " << exit_px << ", proton.get_npx(): " << proton.get_npx() << std::endl;
+#endif
+    BOOST_CHECK(floating_point_equal(local_particles[3][Bunch::xp], exit_px, sbend_tolerance));
+
+}
+
+
 BOOST_AUTO_TEST_CASE(sbend_propagation)
 {
     const double pc = 2.784435311;
@@ -249,9 +466,11 @@ BOOST_AUTO_TEST_CASE(sbend_propagation)
                                 std::pow(sbend_exit_y-sbend_exit_y1,2));
     // offset distance is the x offset in accelerator coordinates
     BOOST_CHECK(floating_point_equal(d_offset, proton.get_x(), sbend_tolerance));
+    std::cout << "d_offset = " << d_offset << "\n";
     // check px/pref
     double px_over_pref = dpp_offset * std::sin(sbend_angle);
     BOOST_CHECK(floating_point_equal(proton.get_npx(), px_over_pref, sbend_tolerance));
+    std::cout << "px = " << px_over_pref << "\n";
     // check path length difference
     // path length for particle 0 is just sbend_length
     // calculate path length for particle 1 on its circular trajectory
@@ -267,6 +486,7 @@ BOOST_AUTO_TEST_CASE(sbend_propagation)
     double delta_cdt = sbend_length1/beta1 - sbend_length/beta;
     BOOST_CHECK(floating_point_equal(proton.get_cdt(), delta_cdt, sbend_tolerance));
 
+    std::cout << "cdt = " << delta_cdt << "\n";
     // check propagation of x-offset particle on-momentum.
     proton.setStateToZero();
     double x_offset = .001;
@@ -294,6 +514,7 @@ BOOST_AUTO_TEST_CASE(sbend_propagation)
 #endif
     BOOST_CHECK(floating_point_equal(proton.get_x(), d_offset, sbend_tolerance));
 
+    std::cout << "d_offset = " << d_offset << "\n";
     // get pathlength
     // start position vector is (0, -radius)
     // final position vector is (sbend_exit_x2, sbend_exit_y2-(-radius+x_offset)
@@ -307,6 +528,7 @@ BOOST_AUTO_TEST_CASE(sbend_propagation)
     std::cout << "proton.get_cdt(): " << proton.get_cdt() << ", delta_cdt: " << delta_cdt << std::endl;
 #endif
     BOOST_CHECK(floating_point_equal(proton.get_cdt(), delta_cdt, sbend_tolerance));
+    std::cout << "cdt = " << delta_cdt << "\n";
 
     // check x'.  Transverse momentum is the sin of the angle between the
     // vector normal to the sbend exit face and the particle trajectory vector.
@@ -321,4 +543,5 @@ BOOST_AUTO_TEST_CASE(sbend_propagation)
     std::cout << "exit_px: " << exit_px << ", proton.get_npx(): " << proton.get_npx() << std::endl;
 #endif
     BOOST_CHECK(floating_point_equal(proton.get_npx(), exit_px, sbend_tolerance));
+    std::cout << "px = " << exit_px << "\n";
 }
