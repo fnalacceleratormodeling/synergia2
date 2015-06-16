6164f1be75a8b4e742467d09ef2e5fc67b4aaba1
Eric G. Stern <egstern@fnal.gov>
2015-06-16 10:22:42 -0500

    remove non-working step_end action from ramp_module

--- archived-applications/sis18_accel/ramp_module.py
+++ archived-applications/sis18_accel/ramp_module.py
@@ -24,13 +24,3 @@ class Ramp_actions(synergia.simulation.Propagate_actions, Pickle_helper):
         stepper.get_lattice_simulator().get_lattice().get_reference_particle().set_total_energy(refpart_energy)
         self.last_energy = refpart_energy
         stepper.get_lattice_simulator().update()
-
-    # def step_end_action(self, stepper, step, bunch, turn_num, step_num):
-    #     bunch_energy = bunch.get_reference_particle().get_total_energy()
-    #     if bunch_energy != self.last_energy:
-    #         print "step ", step_num, " energy changed from ", self.last_energy, " to ", bunch_energy
-    #         stepper.get_lattice_simulator().get_lattice().get_reference_particle().set_total_energy(bunch_energy)
-    #         self.last_energy = bunch_energy
-    #         stepper.get_lattice_simulator().update()
-    #         sliced_beamline = stepper.get_lattice_simulator().get_chef_lattice().get_sliced_beamline()
-    #         print "sliced_beamline energy: ", sliced_beamline.Energy()
