5d88996b9d3cbc8a05a729673c0cd1b9cd19fb62
James Amundson <amundson@fnal.gov>
2015-07-02 13:34:56 -0500

    first working version

--- src/ctest_python.py
+++ src/ctest_python.py
@@ -58,58 +58,134 @@ class Test:
             end += "   Passed   "
         end += "%0.2f sec" % (t1 - t0)
         dots = '...................................................................'
-        num_dots = 78 - len(beginning) - len(end)
+        output_width = 78
+        num_dots = output_width - len(beginning) - len(end)
         print "%s %s %s" % (beginning, dots[0:num_dots], end)
         return status, output
 
-def extract_tests(ctesttestfile):
-    f = open(ctesttestfile, 'r')
+def extract_tests_subdirs(dir_name):
     tests = []
-    names = []
-    commands = []
-    for line in f.readlines():
-        match = re.match('ADD_TEST\((.*)\)', line)
-        if match:
-            split = match.group(1).split(' ')
-            name = split[0]
-            concat_command = string.join(split[1:])
-            command = extract_command_args(concat_command)
-            tests.append(Test(name, concat_command, os.path.dirname(ctesttestfile)))
+    subdirs = []
+#    commands = []
+    ctesttestfile = os.path.join(dir_name, 'CTestTestfile.cmake')
+    try:
+        f = open(ctesttestfile, 'r')
+        for line in f.readlines():
+            match = re.match('add_test\((.*)\)', line, re.IGNORECASE)
+            if match:
+                split = match.group(1).split(' ')
+                name = split[0]
+                concat_command = string.join(split[1:])
+    #            command = extract_command_args(concat_command)
+                tests.append(Test(name, concat_command, os.path.dirname(ctesttestfile)))
+            match = re.match('subdirs\((.*)\)', line, re.IGNORECASE)
+            if match:
+                split = match.group(1).split(' ')
+                subdir_name = split[0]
+                subdirs.append(os.path.join(dir_name, subdir_name))
+    except IOError:
+        print "ignored missing", ctesttestfile
+    return tests, subdirs
+
+def extract_all_tests(dir_name):
+    tests = []
+    subdirs = [dir_name]
+    while subdirs != []:
+        old_subdirs = subdirs
+        subdirs = []
+        for subdir in old_subdirs:
+            new_tests, new_subdirs = extract_tests_subdirs(subdir)
+            tests.extend(new_tests)
+            subdirs.extend(new_subdirs)
     return tests
 
-if __name__ == '__main__':
-    cttfiles = find_ctesttestfiles('.')
-    all_tests = []
-    for cttfile in cttfiles:
-        tests = extract_tests(cttfile)
-        all_tests.extend(tests)
-    logdir = os.path.abspath('./ctest_python_logs')
-    if os.path.exists(logdir):
-        shutil.rmtree(logdir)
-    os.mkdir(logdir)
-    count = 0
-    errors = []
-    error_names = []
-    t0 = time.time()
-    for test in all_tests:
-        count += 1
-        status, output = test.run(count, len(all_tests))
-        if status:
-            errors.append(count)
-            error_names.append(test.name)
-        open(os.path.join(logdir, '%d.log' % count), 'w').write(output + '\n')
-    t1 = time.time()
+def create_unique_directory(directory):
+    max_version = 99
+    version = 0
+    created_directory = "%s.%02d" % (directory, version)
+    while os.path.isdir(created_directory):
+        version += 1
+        if version > max_version:
+            sys.stderr.write('ctest_python.py: output directory index too high (max=%d)\n' % \
+                max_version)
+            sys.exit(1)
+        created_directory = "%s.%02d" % (directory, version)
+    os.mkdir(created_directory)
+    return created_directory
 
+def run_tests(tests):
+    num_tests = len(tests)
+    err_indices = []
+    err_tests = []
+    err_output_base = "ctest_python_logs"
+    err_output_dir = None
+    t0 = time.time()
+    for index, test in zip(range(1,num_tests+1), tests):
+        status, output = test.run(index, num_tests)
+        if status != 0:
+            err_indices.append(index)
+            err_tests.append(test)
+            if not err_output_dir:
+                err_output_dir = create_unique_directory(err_output_base)            
+            output_fname = '%03d_%s.out' % (index, test.name)
+            output_path = os.path.join(err_output_dir, output_fname)
+            open(output_path, 'w').write(output)
+    t1 = time.time()
+    num_errs = len(err_indices)
     print
-    print "%d%%" % (100 * (1.0 - len(errors) / (1.0 * len(all_tests)))),
-    print "tests passed,",
-    print len(errors), "tests failed out of", len(all_tests)
+    pass_percentage = 100.0*(num_tests - num_errs)/(1.0*num_tests)
+    print '%0.1f%% tests passed, %d tests failed out of %d' % \
+        (pass_percentage, num_errs, num_tests)
     print
-    print "Total Test time (real) = %0.2f" % (t1 - t0) , "sec"
+    print 'Total Test time (real) = %0.2f sec' % (t1 - t0)
     print
-    if len(errors) > 0:
-        print "The following tests FAILED:"
-        for error, error_name in zip(errors, error_names):
-            print "%8d" % error, '-', error_name, '(Failed)'
-        print 'Errors while running ctest_python.py'
-        sys.exit(1)
+    if num_errs > 0:
+        print 'The following tests FAILED:'
+        for index, test in zip(err_indices, err_tests):
+            print '    % 3d' % index, '-', test.name
+        print 'log files written in directory', err_output_dir
+        
+            
+           
+if __name__ == '__main__':
+    tests = extract_all_tests('.')
+#    print 'found', len(tests), 'tests'
+#    for index, test in zip(range(1,len(tests)+1), tests):
+#        print index, test.name, test.dir
+    run_tests(tests)
+
+#    cttfiles = find_ctesttestfiles('.')
+#    all_tests = []
+#    for cttfile in cttfiles:
+#        tests = extract_tests(cttfile)
+#        all_tests.extend(tests)
+#    logdir = os.path.abspath('./ctest_python_logs')
+#    if os.path.exists(logdir):
+#        shutil.rmtree(logdir)
+#    os.mkdir(logdir)
+#    count = 0
+#    errors = []
+#    error_names = []
+#    t0 = time.time()
+#    for test in all_tests:
+#        count += 1
+#        status, output = test.run(count, len(all_tests))
+#        if status:
+#            errors.append(count)
+#            error_names.append(test.name)
+#        open(os.path.join(logdir, '%d.log' % count), 'w').write(output + '\n')
+#    t1 = time.time()
+#
+#    print
+#    print "%d%%" % (100 * (1.0 - len(errors) / (1.0 * len(all_tests)))),
+#    print "tests passed,",
+#    print len(errors), "tests failed out of", len(all_tests)
+#    print
+#    print "Total Test time (real) = %0.2f" % (t1 - t0) , "sec"
+#    print
+#    if len(errors) > 0:
+#        print "The following tests FAILED:"
+#        for error, error_name in zip(errors, error_names):
+#            print "%8d" % error, '-', error_name, '(Failed)'
+#        print 'Errors while running ctest_python.py'
+#        sys.exit(1)
