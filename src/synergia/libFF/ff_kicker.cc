b515a5b215268ba6cc4c3ce40a03961b2b97cd66
Qiming Lu <qlu@fnal.gov>
2015-08-05 16:15:44 -0500

    a general ff_kicker (can have both hkick and vkick)

---
+++ src/synergia/libFF/ff_kicker.cc
@@ -0,0 +1,203 @@
+#include "ff_kicker.h"
+#include "ff_algorithm.h"
+#include "synergia/lattice/chef_utils.h"
+#include "synergia/utils/gsvector.h"
+
+double FF_kicker::get_reference_cdt(double length, double hk, double vk, Reference_particle &reference_particle) 
+{
+    if (length == 0) return 0.0;
+
+    double reference_cdt;
+
+    double pref = reference_particle.get_momentum();
+    double m = reference_particle.get_mass();
+    double step_length = length * 0.5;
+
+    double x(reference_particle.get_state()[Bunch::x]);
+    double xp(reference_particle.get_state()[Bunch::xp]);
+    double y(reference_particle.get_state()[Bunch::y]);
+    double yp(reference_particle.get_state()[Bunch::yp]);
+    double cdt(reference_particle.get_state()[Bunch::cdt]);
+    double dpop(reference_particle.get_state()[Bunch::dpop]);
+
+    double cdt_orig = cdt;
+
+    FF_algorithm::drift_unit(x, xp, y, yp, cdt, dpop, step_length, pref, m, 0.0);
+    FF_algorithm::thin_kicker_unit(xp, yp, hk, vk);
+    FF_algorithm::drift_unit(x, xp, y, yp, cdt, dpop, step_length, pref, m, 0.0);
+
+    reference_cdt = cdt - cdt_orig;
+
+    return reference_cdt;
+}
+
+void FF_kicker::apply(Lattice_element_slice const& slice, JetParticle& jet_particle)
+{
+    double length = slice.get_right() - slice.get_left();
+
+    double hk = slice.get_lattice_element().get_double_attribute("hkick");
+    double vk = slice.get_lattice_element().get_double_attribute("vkick");
+
+    typedef PropagatorTraits<JetParticle>::State_t State_t;
+    typedef PropagatorTraits<JetParticle>::Component_t Component_t;
+
+    State_t& state = jet_particle.State();
+
+    Component_t & x(state[Chef::x]);
+    Component_t & xp(state[Chef::xp]);
+    Component_t & y(state[Chef::y]);
+    Component_t & yp(state[Chef::yp]);
+    Component_t & cdt(state[Chef::cdt]);
+    Component_t & dpop(state[Chef::dpop]);
+
+    double pref = jet_particle.ReferenceMomentum();
+    double m = jet_particle.Mass();
+
+    Particle chef_particle(jet_particle);
+    Reference_particle reference_particle(
+                chef_particle_to_reference_particle(chef_particle));
+
+    double reference_cdt = get_reference_cdt(length, hk, vk, reference_particle);
+    double step_reference_cdt = reference_cdt * 0.5;
+    double step_length = length * 0.5;
+
+    if (length == 0.0) 
+    {
+        FF_algorithm::thin_kicker_unit(xp, yp, hk, vk);
+    } 
+    else 
+    {
+        FF_algorithm::drift_unit(x, xp, y, yp, cdt, dpop, step_length, pref, m, 0.0);
+        FF_algorithm::thin_kicker_unit(xp, yp, hk, vk);
+        FF_algorithm::drift_unit(x, xp, y, yp, cdt, dpop, step_length, pref, m, 0.0);
+    }
+}
+
+void FF_kicker::apply(Lattice_element_slice const& slice, Bunch& bunch)
+{
+    double length = slice.get_right() - slice.get_left();
+    double hk = slice.get_lattice_element().get_double_attribute("hkick");
+    double vk = slice.get_lattice_element().get_double_attribute("vkick");
+
+    int local_num = bunch.get_local_num();
+    MArray2d_ref particles = bunch.get_local_particles();
+
+    double * RESTRICT xa;
+    double * RESTRICT xpa;
+    double * RESTRICT ya;
+    double * RESTRICT ypa;
+    double * RESTRICT cdta;
+    double * RESTRICT dpopa;
+
+    bunch.set_arrays(xa, xpa, ya, ypa, cdta, dpopa);
+
+    const int num_blocks = local_num / GSVector::size;
+    const int block_last = num_blocks * GSVector::size;
+
+    if (length == 0.0) 
+    {
+        for (int part = 0; part < block_last; part += GSVector::size) 
+        {
+            GSVector xp(&xpa[part]);
+            GSVector yp(&ypa[part]);
+            FF_algorithm::thin_kicker_unit(xp, yp, hk, vk);
+            xp.store(&xpa[part]);
+            yp.store(&ypa[part]);
+        }
+
+        for (int part = block_last; part < local_num; ++part) 
+        {
+            double xp(xpa[part]);
+            double yp(ypa[part]);
+            FF_algorithm::thin_kicker_unit(xp, yp, hk, vk);
+            xpa[part] = xp;
+            ypa[part] = yp;
+        }
+    } 
+    else 
+    {
+        double pref = bunch.get_reference_particle().get_momentum();
+        double m = bunch.get_mass();
+        double reference_cdt = get_reference_cdt(length, hk, vk, bunch.get_reference_particle());
+
+        double step_reference_cdt = reference_cdt * 0.5;
+        double step_length = length * 0.5;
+
+        for (int part = 0; part < block_last; part += GSVector::size) 
+        {
+            GSVector x(&xa[part]);
+            GSVector xp(&xpa[part]);
+            GSVector y(&ya[part]);
+            GSVector yp(&ypa[part]);
+            GSVector cdt(&cdta[part]);
+            GSVector dpop(&dpopa[part]);
+
+            FF_algorithm::drift_unit(x, xp, y, yp, cdt, dpop, step_length, pref, m, step_reference_cdt);
+            FF_algorithm::thin_kicker_unit(xp, yp, hk, vk);
+            FF_algorithm::drift_unit(x, xp, y, yp, cdt, dpop, step_length, pref, m, step_reference_cdt);
+
+            x.store(&xa[part]);
+            xp.store(&xpa[part]);
+            y.store(&ya[part]);
+            yp.store(&ypa[part]);
+            cdt.store(&cdta[part]);
+            dpop.store(&dpopa[part]);
+        }
+
+        for (int part = block_last; part < local_num; ++part) 
+        {
+            double x(xa[part]);
+            double xp(xpa[part]);
+            double y(ya[part]);
+            double yp(ypa[part]);
+            double cdt(cdta[part]);
+            double dpop(dpopa[part]);
+
+            FF_algorithm::drift_unit(x, xp, y, yp, cdt, dpop, step_length, pref, m, step_reference_cdt);
+            FF_algorithm::thin_kicker_unit(xp, yp, hk, vk);
+            FF_algorithm::drift_unit(x, xp, y, yp, cdt, dpop, step_length, pref, m, step_reference_cdt);
+
+            xa[part] = x;
+            xpa[part] = xp;
+            ya[part] = y;
+            ypa[part] = yp;
+            cdta[part] = cdt;
+            dpopa[part] = dpop;
+        }
+
+        bunch.get_reference_particle().increment_trajectory(length);
+    }
+}
+
+template<class Archive>
+    void
+    FF_kicker::serialize(Archive & ar, const unsigned int version)
+    {
+        ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(FF_element);
+    }
+
+template
+void
+FF_kicker::serialize<boost::archive::binary_oarchive >(
+        boost::archive::binary_oarchive & ar, const unsigned int version);
+
+template
+void
+FF_kicker::serialize<boost::archive::xml_oarchive >(
+        boost::archive::xml_oarchive & ar, const unsigned int version);
+
+template
+void
+FF_kicker::serialize<boost::archive::binary_iarchive >(
+        boost::archive::binary_iarchive & ar, const unsigned int version);
+
+template
+void
+FF_kicker::serialize<boost::archive::xml_iarchive >(
+        boost::archive::xml_iarchive & ar, const unsigned int version);
+
+FF_kicker::~FF_kicker()
+{
+
+}
+
