9a07c46a188d29435c56d4235f71389ec11a0870
Qiming Lu <qlu@fnal.gov>
2015-09-21 15:08:57 -0500

    added MIC vectorization for gsvector class

--- src/synergia/utils/gsvector.h
+++ src/synergia/utils/gsvector.h
@@ -4,6 +4,11 @@
 #include "boost/multi_array.hpp"
 typedef boost::multi_array_ref<double, 2> MArray2d_ref;
 
+#undef GSV_SSE
+#undef GSV_AVX
+#undef GSV_V4D
+#undef GSV_MIC
+
 #if defined(GSV_SSE)
 
 #include "vectorclass.h"
@@ -256,6 +261,94 @@ static inline GSVector& operator/=(GSVector& a, GSVector const& b) {
 }
 static inline GSVector sqrt(GSVector const& a) { return GSVector(sqrtd4(a.vec)); }
 static inline GSVector invsqrt(GSVector const& a) { return GSVector(rsqrtd4(a.vec)); }
+
+#elif defined(GSV_MIC)
+
+#include <mic/micvec.h>
+class GSVector {
+public:
+    static const int size = 8;
+    static const int implementation = 4;
+    F64vec8 vec;
+    GSVector(MArray2d_ref& marray, size_t index0, size_t index1) : vec(&marray[index0][index1]) { }
+    GSVector(double val) : vec(val) {}
+    GSVector(F64vec8 const& vec) : vec(vec) {}
+    GSVector(double* array_start) : vec(array_start) {  }
+    void store(MArray2d_ref& marray, size_t index0, size_t index1) const 
+    { _mm512_store_pd(&marray[index0][index1], vec); }
+    void store(double* array_start) const 
+    { _mm512_store_pd(array_start, vec); }
+#if 0
+    void wtf() {
+        std::cout << "GSVector::wtf " << vec[0] << "," << vec[1] << std::endl;
+    }
+#endif
+
+    friend std::ostream & operator << (std::ostream & out, GSVector & v);
+};
+static inline GSVector operator+(GSVector const& a, GSVector const& b) {
+    return GSVector(a.vec + b.vec);
+}
+static inline GSVector operator+(GSVector const& a, double b) {
+    return a + GSVector(b);
+}
+static inline GSVector operator+(double a, GSVector const& b) {
+    return GSVector(a) + b;
+}
+static inline GSVector& operator+=(GSVector& a, GSVector const& b) {
+    a.vec = a.vec + b.vec;
+    return a;
+}
+static inline GSVector operator-(GSVector const& a, GSVector const& b) {
+    return GSVector(a.vec - b.vec);
+}
+static inline GSVector operator-(GSVector const& a, double b) {
+    return GSVector(a.vec - b);
+}
+static inline GSVector operator-(double a, GSVector const& b) {
+    return GSVector(a - b.vec);
+}
+static inline GSVector operator-(GSVector const& a) { return GSVector(-a.vec); }
+static inline GSVector& operator-=(GSVector& a, GSVector const& b) {
+    a.vec = a.vec - b.vec;
+    return a;
+}
+static inline GSVector operator*(GSVector const& a, GSVector const& b) {
+    return GSVector(a.vec * b.vec);
+}
+static inline GSVector operator*(GSVector const& a, double b) {
+    return GSVector(a.vec * b);
+}
+static inline GSVector operator*(double a, GSVector const& b) {
+    return GSVector(a * b.vec);
+}
+static inline GSVector& operator*=(GSVector& a, GSVector const& b) {
+    a.vec = a.vec * b.vec;
+    return a;
+}
+static inline GSVector operator/(GSVector const& a, GSVector const& b) {
+    return GSVector(a.vec / b.vec);
+}
+static inline GSVector operator/(GSVector const& a, double b) {
+    return GSVector(a.vec / b);
+}
+static inline GSVector operator/(double a, GSVector const& b) {
+    return GSVector(a / b.vec);
+}
+static inline GSVector& operator/=(GSVector& a, GSVector const& b) {
+    a.vec = a.vec / b.vec;
+    return a;
+}
+static inline GSVector sqrt(GSVector const& a) { return GSVector(sqrt(a.vec)); }
+static inline GSVector invsqrt(GSVector const& a) {
+    return GSVector(1.0 / sqrt(a.vec));
+}
+
+inline std::ostream & operator << (std::ostream & out, GSVector & v)
+{
+    //out << "(" << v.vec[0] << ", " << v.vec[1] << ", " << v.vec[2] << ", " << v.vec[3] << ")";
+    return out;
+}
 
 #else  // no SIMD implementation defined
 
