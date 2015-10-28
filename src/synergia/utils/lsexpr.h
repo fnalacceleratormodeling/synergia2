a96b47515848d258b164539b832827dd7d5c6e59
James Amundson <amundson@fnal.gov>
2015-10-28 11:56:41 -0500

    accessors for Lsexpr sequences

--- src/synergia/utils/lsexpr.h
+++ src/synergia/utils/lsexpr.h
@@ -5,16 +5,21 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <stdexcept>
 
 struct Lsexpr;
 
 struct Lsexpr
 {
+    typedef std::vector<Lsexpr> sequence_t;
+    typedef sequence_t::const_iterator const_iterator_t;
+    typedef sequence_t::iterator iterator_t;
+
     bool has_label;
     std::string label;
     bool is_atom;
     std::string atom;
-    std::vector<Lsexpr> sequence;
+    sequence_t sequence;
 
     Lsexpr() :
         has_label(false)
@@ -32,20 +37,36 @@ struct Lsexpr
       , sequence()
     { }
 
-    std::string to_string(int i)
+    std::string to_string(int i) const
     {
         std::stringstream ss;
         ss << i;
         return ss.str();
     }
 
-    std::string to_string(double d)
+    int stoi(std::string const& s) const
+    {
+        int retval;
+        std::istringstream ss(s);
+        ss >> retval;
+        return retval;
+    }
+
+    std::string to_string(double d) const
     {
         std::stringstream ss;
         const int precision = 15;
         ss.precision(precision);
         ss << d;
         return(ss.str());
+    }
+
+    double stod(std::string const& s) const
+    {
+        double retval;
+        std::istringstream ss(s);
+        ss >> retval;
+        return retval;
     }
 
     Lsexpr(int atom) :
@@ -111,10 +132,109 @@ struct Lsexpr
         has_label = true;
     }
 
+    bool is_labeled() const
+    {
+        return has_label;
+    }
+
+    std::string const& get_label() const
+    {
+        return label;
+    }
+
+    bool is_atomic() const
+    {
+        return is_atom;
+    }
+
+    std::string get_string() const
+    {
+        if(!is_atom) {
+            throw std::runtime_error("Lsexpr::get_string: non-atomic Lsexpr");
+        }
+        return atom;
+    }
+
+    int get_int() const
+    {
+        if(!is_atom) {
+            throw std::runtime_error("Lsexpr::get_int: non-atomic Lsexpr");
+        }
+        return stoi(atom);
+    }
+
+    double get_double() const
+    {
+        if(!is_atom) {
+            throw std::runtime_error("Lsexpr::get_double: non-atomic Lsexpr");
+        }
+        return stod(atom);
+    }
+
     void push_back(Lsexpr const& lsexpr)
     {
         sequence.push_back(lsexpr);
     }
+
+    const_iterator_t begin() const
+    {
+        return sequence.begin();
+    }
+
+    const_iterator_t end() const
+    {
+        return sequence.end();
+    }
+
+    iterator_t begin()
+    {
+        return sequence.begin();
+    }
+
+    iterator_t end()
+    {
+        return sequence.end();
+    }
+
+    std::vector<std::string > get_string_vector() const
+    {
+        if(is_atom) {
+            throw std::runtime_error("Lsexpr::get_string_vector: atomic Lsexpr");
+        }
+        std::vector<std::string> retval;
+        for(const_iterator_t it = begin(); it!= end();
+            ++it) {
+            retval.push_back(it->get_string());
+        }
+        return retval;
+    }
+
+    std::vector<int> get_int_vector() const
+    {
+        if(is_atom) {
+            throw std::runtime_error("Lsexpr::get_int_vector: atomic Lsexpr");
+        }
+        std::vector<int> retval;
+        for(const_iterator_t it = begin(); it!= end();
+            ++it) {
+            retval.push_back(stoi(it->get_string()));
+        }
+        return retval;
+    }
+
+    std::vector<double> get_double_vector() const
+    {
+        if(is_atom) {
+            throw std::runtime_error("Lsexpr::get_double_vector: atomic Lsexpr");
+        }
+        std::vector<double> retval;
+        for(const_iterator_t it = begin(); it!= end();
+            ++it) {
+            retval.push_back(stoi(it->get_string()));
+        }
+        return retval;
+    }
+
 };
 
 Lsexpr read_lsexpr(std::istream & stream);
