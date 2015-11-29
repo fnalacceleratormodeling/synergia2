8536f748a38b12eadace03eb01f2b902cce8a744
Qiming Lu <qlu@fnal.gov>
2015-11-29 17:55:14 -0600

    cleared debug outputs from ff_rbends

--- src/synergia/libFF/ff_rbend.cc
+++ src/synergia/libFF/ff_rbend.cc
@@ -116,8 +116,6 @@ void FF_rbend::apply(Lattice_element_slice const& slice, Bunch& bunch)
 
     if (k[2] == 0.0 && k[4] == 0.0)
     {
-        std::cout << ">>>>> rbend prop libff\n";
-
         // use the exact solution for dipole
         for (int part = 0; part < local_num; ++part) 
         {
@@ -145,17 +143,6 @@ void FF_rbend::apply(Lattice_element_slice const& slice, Bunch& bunch)
 
             double tau = -x * st / bp;
 
-            std::cout << std::setprecision(10);
-
-            std::cout << "state = " << x << ", " << y << ", " << xp << ", " << yp <<"\n";
-
-#if 0
-            std::cout << "myb = " << b0 << ", " << b1 << ", " << b2 << "\n";
-            std::cout << "mybp = " << bp << "\n";
-            std::cout << "mytau = " << tau << "\n";
-            std::cout << "cos(t) = " << ct << ", sin(t) = " << st << "\n";
-            std::cout << "p = " << xp << ", " << yp << ", " << zp << "\n";
-
             r0 = x + tau * b0;
             r1 = y + tau * b1;
             r2 = 0 + tau * b2;
@@ -166,31 +153,21 @@ void FF_rbend::apply(Lattice_element_slice const& slice, Bunch& bunch)
             xp = xp * ct - zp * st;
             yp = yp;
 
-            std::cout << "px = " << xp << ", py = " << yp << "\n\n";
-#endif
+            FF_algorithm::dipole_unit(x, xp, y, yp, cdt, dpop, length, k[0]);
 
-            //FF_algorithm::dipole_unit(x, xp, y, yp, cdt, dpop, length, k[0]);
-
+#if 0
             FF_algorithm::yoshida<double, FF_algorithm::thin_rbend_unit<double>, 4, 3 >
                 ( x, xp, y, yp, cdt, dpop,
                   reference_momentum, m,
                   step_reference_cdt,
                   step_length, step_strength, steps );
-
-            std::cout << "state = " << x << ", " << y << ", " << xp << ", " << yp <<"\n";
-#if 0
+#endif
 
             b0 = xp * pref / e;
             b1 = yp * pref / e;
 
             bp = st * b0 + ct * b2;
             tau = -x * st / bp;
-
-            std::cout << "myb = " << b0 << ", " << b1 << ", " << b2 << "\n";
-            std::cout << "mybp = " << bp << "\n";
-            std::cout << "mytau = " << tau << "\n";
-            std::cout << "cos(t) = " << ct << ", sin(t) = " << st << "\n";
-            std::cout << "p = " << xp << ", " << yp << ", " << zp << "\n";
 
             r0 = x + tau * b0;
             r1 = y + tau * b1;
@@ -200,9 +177,6 @@ void FF_rbend::apply(Lattice_element_slice const& slice, Bunch& bunch)
             y = r1;
 
             xp = xp * ct - zp * st;
-
-            std::cout << "px = " << xp << ", py = " << yp << "\n";
-#endif
 
             particles[part][Bunch::x]  = x;
             particles[part][Bunch::xp] = xp;
