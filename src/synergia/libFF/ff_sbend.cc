784502d93d7cdff61e9e3194cae57cf10044fab0
Qiming Lu <qlu@fnal.gov>
2015-11-29 17:52:08 -0600

    added slots to libFF sbends

--- src/synergia/libFF/ff_sbend.cc
+++ src/synergia/libFF/ff_sbend.cc
@@ -75,6 +75,15 @@ void FF_sbend::apply(Lattice_element_slice const& slice, Bunch& bunch)
     double cos_angle = cos(angle);
     double sin_angle = sin(angle);
 
+    double e1 = 0.0;
+    double e2 = 0.0;
+
+    if (slice.get_lattice_element().has_double_attribute("e1"))
+        e1 = slice.get_lattice_element().get_double_attribute("e1");
+    if (slice.get_lattice_element().has_double_attribute("e2"))
+        e2 = slice.get_lattice_element().get_double_attribute("e2");
+
+
     int local_num = bunch.get_local_num();
     MArray2d_ref particles = bunch.get_local_particles();
 
@@ -87,9 +96,20 @@ void FF_sbend::apply(Lattice_element_slice const& slice, Bunch& bunch)
     double reference_cdt = get_reference_cdt(length, angle, strength,
                                              bunch.get_reference_particle());
 
-    std::complex<double> phase = std::exp( std::complex<double>(0.0, angle) );
+    double psi = angle - (e1 + e2);
+    std::complex<double> phase = std::exp( std::complex<double>(0.0, psi) );
     std::complex<double> term = std::complex<double>(0.0, length / angle) *
-                                std::complex<double>(1.0 - cos_angle, - sin_angle);
+                                std::complex<double>(1.0 - cos_angle, - sin_angle) *
+                                std::complex<double>(cos(e2), -sin(e2));
+
+
+    double pref = reference_momentum;
+
+    double ce1 = cos(-e1);
+    double se1 = sin(-e1);
+    double ce2 = cos(-e2);
+    double se2 = sin(-e2);
+
 
     for (int part = 0; part < local_num; ++part) {
         double x   (particles[part][Bunch::x   ]);
@@ -99,9 +119,74 @@ void FF_sbend::apply(Lattice_element_slice const& slice, Bunch& bunch)
         double cdt (particles[part][Bunch::cdt ]);
         double dpop(particles[part][Bunch::dpop]);
 
+            double r0 = x;
+            double r1 = y;
+            double r2 = 0.0;
+
+            double zp = sqrt((dpop+1)*(dpop+1) - xp * xp - yp * yp);
+
+            double p = (dpop+1) * pref;
+            double e = sqrt(p*p + m*m);
+
+            double b0 = xp * pref / e;
+            double b1 = yp * pref / e;
+            double b2 = zp * pref / e;
+
+            double bp = se1 * b0 + ce1 * b2;
+
+            double tau = -x * se1 / bp;
+
+            std::cout << std::setprecision(10);
+
+            std::cout << "state = " << x << ", " << y << ", " << xp << ", " << yp <<"\n";
+
+            std::cout << "myb = " << b0 << ", " << b1 << ", " << b2 << "\n";
+            std::cout << "mybp = " << bp << "\n";
+            std::cout << "mytau = " << tau << "\n";
+            std::cout << "cos(t) = " << ce1 << ", sin(t) = " << se2 << "\n";
+            std::cout << "p = " << xp << ", " << yp << ", " << zp << "\n";
+
+            r0 = x + tau * b0;
+            r1 = y + tau * b1;
+            r2 = 0 + tau * b2;
+
+            x = r0 * ce1 - r2 * se1;
+            y = r1;
+
+            xp = xp * ce1 - zp * se1;
+            yp = yp;
+
+            std::cout << "state after slot 1 = " << x << ", " << y << ", " << xp << ", " << yp <<"\n";
+
         sbend_unit2(x, xp, y, yp, cdt, dpop,
                    length, angle, strength,
                    reference_momentum, m, reference_cdt, phase, term);
+
+            std::cout << "state after sbend  = " << x << ", " << y << ", " << xp << ", " << yp <<"\n";
+
+            b0 = xp * pref / e;
+            b1 = yp * pref / e;
+
+            bp = se2 * b0 + ce2 * b2;
+            tau = -x * se2 / bp;
+
+            std::cout << "myb = " << b0 << ", " << b1 << ", " << b2 << "\n";
+            std::cout << "mybp = " << bp << "\n";
+            std::cout << "mytau = " << tau << "\n";
+            std::cout << "cos(t) = " << ce2 << ", sin(t) = " << se2 << "\n";
+            std::cout << "p = " << xp << ", " << yp << ", " << zp << "\n";
+
+            r0 = x + tau * b0;
+            r1 = y + tau * b1;
+            r2 = 0 + tau * b2;
+
+            x = r0 * ce2 - r2 * se2;
+            y = r1;
+
+            xp = xp * ce2 - zp * se2;
+
+            std::cout << "px = " << xp << ", py = " << yp << "\n";
+
 
         particles[part][Bunch::x]  = x;
         particles[part][Bunch::xp] = xp;
