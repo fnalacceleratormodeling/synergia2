7a05313e5954ddf617e7710ff1d2e260f2726ea1
Eric G. Stern <egstern@fnal.gov>
2016-03-04 17:15:19 -0600

    wrap new Space_charge_2d_bassetti_erskine method set_longitudinal and the constants longitudinal_uniform, longitudinal_gaussian

--- src/synergia/collective/collective_wrap.cc
+++ src/synergia/collective/collective_wrap.cc
@@ -95,11 +95,15 @@ BOOST_PYTHON_MODULE(collective)
                 .def("set_files", &Space_charge_2d_open_hockney::set_files)
         ;
 
-    class_<Space_charge_2d_bassetti_erskine, Space_charge_2d_bassetti_erskine_sptr,
+    scope Space_charge_2d_bassetti_erskine_scope =
+        class_<Space_charge_2d_bassetti_erskine, Space_charge_2d_bassetti_erskine_sptr,
         bases<Collective_operator > >("Space_charge_2d_bassetti_erskine",
                 init<>())
         .def("apply", &Space_charge_2d_bassetti_erskine::apply)
+        .def("set_longitudinal", &Space_charge_2d_bassetti_erskine::set_longitudinal)
         ;
+    scope().attr("longitudinal_uniform") = Space_charge_2d_bassetti_erskine::longitudinal_uniform;
+    scope().attr("longitudinal_gaussian") = Space_charge_2d_bassetti_erskine::longitudinal_gaussian;
 
    class_<Space_charge_rectangular, Space_charge_rectangular_sptr,
         bases<Collective_operator > >("Space_charge_rectangular",
