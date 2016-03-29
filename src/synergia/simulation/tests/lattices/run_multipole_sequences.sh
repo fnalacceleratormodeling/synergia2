450deafe4eb86b4c83248ec80e4d5b929a85c60d
Eric G. Stern <egstern@fnal.gov>
2016-03-29 16:16:21 -0500

    add k4, k5, k6 multipoles to tests

--- src/synergia/simulation/tests/lattices/run_multipole_sequences.sh
+++ src/synergia/simulation/tests/lattices/run_multipole_sequences.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-seqs="mpole_k1 mpole_k1s mpole_k1_tilt mpole_k2 mpole_k2s mpole_k2_tilt mpole_k3 mpole_k3s mpole_k3_test"
+seqs="mpole_k1 mpole_k1s mpole_k1_tilt mpole_k2 mpole_k2s mpole_k2_tilt mpole_k3 mpole_k3s mpole_k3_tilt mpole_k4 mpole_k4s mpole_k4_tilt mpole_k5 mpole_k5s mpole_k5_tilt mpole_k6 mpole_k6s mpole_k6_tilt"
 
 for s in $seqs
 do
