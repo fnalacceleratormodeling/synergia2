ebc3244f3fd7396d46b76a2eaff672032537747d
James Amundson <amundson@fnal.gov>
2016-04-08 16:44:33 -0500

    bug fix: reference_particle_ptr was not being zeroed in constructor in all cases

--- src/synergia/lattice/lattice.cc
+++ src/synergia/lattice/lattice.cc
@@ -13,14 +13,16 @@ Lattice::Lattice() :
 }
 
 Lattice::Lattice(std::string const& name) :
-        name(name), reference_particle_allocated(false), elements(), element_adaptor_map_sptr(
+        name(name), reference_particle_allocated(false), reference_particle_ptr(0),
+        elements(), element_adaptor_map_sptr(
                 new Mad8_adaptor_map)
 {
 }
 
 Lattice::Lattice(std::string const& name,
         Element_adaptor_map_sptr element_adaptor_map_sptr) :
-        name(name), reference_particle_allocated(false), elements(), element_adaptor_map_sptr(
+        name(name), reference_particle_allocated(false), reference_particle_ptr(0),
+        elements(), element_adaptor_map_sptr(
                 element_adaptor_map_sptr)
 {
 }
