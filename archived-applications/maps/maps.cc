30f60684c1b5952aaf90e7480f12908130c1b3d6
James Amundson <amundson@fnal.gov>
2017-03-21 16:29:24 -0500

    linear comparison for trigon and chef

--- archived-applications/maps/maps.cc
+++ archived-applications/maps/maps.cc
@@ -8,6 +8,7 @@
 #include "synergia/lattice/lattice_element_slice.h"
 #include "synergia/lattice/madx_reader.h"
 #include "synergia/libFF/ff_element_map.h"
+#include "synergia/utils/multi_array_print.h"
 
 // We put the actual code in a separate function so that shared_ptr's can
 // be cleanup up properly before we call MPI_Finalize.
@@ -16,10 +17,6 @@ run()
 {
     MadX_reader reader;
     Lattice_sptr lattice_sptr = reader.get_lattice_sptr("fodo", "fodo.madx");
-    //    for(auto it = lattice_sptr->get_elements().begin();
-    //        it != lattice_sptr->get_elements().end(); ++it){
-    //        std::cout << (*it)->get_name() << std::endl;
-    //    }
 
     construct_big_giant_global_ff_element_map();
     FF_element_map& element_map(the_big_giant_global_ff_element_map);
@@ -27,15 +24,35 @@ run()
     JetParticle jet_particle(reference_particle_to_chef_jet_particle(
         lattice_sptr->get_reference_particle(),
         Trigon_particle_t::Component_t::power()));
-    for (auto&& element_sptr : lattice_sptr->get_elements()) {
-        Lattice_element_slice slice(element_sptr);
-        std::cout << element_sptr->get_name() << ": "
-                  << element_sptr->get_type() << std::endl;
-        element_map.get_element_type(element_sptr->get_type())
-            ->apply(element_sptr, jet_particle);
-        std::cout << "jet_particle.jacobian() = \n";
-        std::cout << jet_particle.State().Jacobian();
+
+    double t0 = MPI_Wtime();
+    const int num_reps = 100;
+    for (int i = 0; i < num_reps; ++i) {
+        for (auto&& element_sptr : lattice_sptr->get_elements()) {
+            Lattice_element_slice slice(element_sptr);
+            element_map.get_element_type(element_sptr->get_type())
+                ->apply(element_sptr, jet_particle);
+        }
     }
+    double t1 = MPI_Wtime();
+    std::cout << "chef time: " << t1 - t0 << std::endl;
+    std::cout << "jet_particle.jacobian() = \n";
+    std::cout << jet_particle.State().Jacobian();
+
+    Trigon_particle_t trigon_particle(lattice_sptr->get_reference_particle());
+
+    t0 = MPI_Wtime();
+    for (int i = 0; i < num_reps; ++i) {
+        for (auto&& element_sptr : lattice_sptr->get_elements()) {
+            Lattice_element_slice slice(element_sptr);
+            element_map.get_element_type(element_sptr->get_type())
+                ->apply(element_sptr, trigon_particle);
+        }
+    }
+    t1 = MPI_Wtime();
+    std::cout << "trigon time: " << t1 - t0 << std::endl;
+    std::cout << "trigon_particle.jacobian() = \n";
+    multi_array_print(trigon_particle.get_jacobian(), "jacobian");
 }
 
 int
