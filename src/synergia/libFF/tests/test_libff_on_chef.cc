3e08514a4b49fe66af18df57af73fe5525523631
Qiming Lu <qlu@fnal.gov>
2017-09-19 16:18:11 -0500

    lowered the threshold of libff cf_sbend2 test (with both k1 and k2)

--- src/synergia/libFF/tests/test_libff_on_chef.cc
+++ src/synergia/libFF/tests/test_libff_on_chef.cc
@@ -391,9 +391,9 @@ BOOST_FIXTURE_TEST_CASE( test_cfsbend2, cfsbend2_fixture )
     {
         //std::cout << pcf[0][i] << " <--> " << pff[0][i] << "\n";
     }
+
     // the sextupole model in CHEF doesn't work the same way as in libff
-    element_check(pff, pcf, 0.038);
-    //element_check(pff, pcf, tolerance);
+    element_check(pff, pcf, 3e-05);
     BOOST_CHECK(true);
 }
 
