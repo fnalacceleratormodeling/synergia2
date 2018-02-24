9f0513f005435ad053552c1c46c1cef0c6d3ae1f
James Amundson <amundson@fnal.gov>
2018-02-24 15:43:31 -0600

    add run-time dependencies

--- docs/devel/spack/synergiarepo/packages/synergia2/package.py
+++ docs/devel/spack/synergiarepo/packages/synergia2/package.py
@@ -9,16 +9,27 @@ class Synergia2(CMakePackage):
             branch='devel')
     
     depends_on("cmake")
-    depends_on("mpi")
-    depends_on("chef@devel")
-    depends_on("boost@:1.62+filesystem+iostreams+python+regex+serialization+system+test")
-    depends_on("fftw+mpi")
-    depends_on("gsl")
-    depends_on("hdf5+cxx")
-    depends_on("eigen~metis~mpfr~scotch~suitesparse")
-    depends_on("python")
-    depends_on("py-mpi4py")
-    depends_on("py-nose")
-    depends_on("py-pyparsing")
-    depends_on("py-numpy")
+    depends_on("mpi", type=('build', 'run'))
+    depends_on("chef@devel", type=('build', 'run'))
+    depends_on("boost@:1.62+filesystem+iostreams+python+regex+serialization+system+test",
+            type=('build', 'run'))
+    depends_on("fftw+mpi", type=('build', 'run'))
+    depends_on("gsl", type=('build', 'run'))
+    depends_on("hdf5+cxx", type=('build', 'run'))
+    depends_on("eigen~metis~mpfr~scotch~suitesparse", type=('build', 'run'))
+    depends_on("python", type=('build', 'run'))
+    depends_on("py-mpi4py", type=('build', 'run'))
+    depends_on("py-nose", type=('build', 'run'))
+    depends_on("py-pyparsing", type=('build', 'run'))
+    depends_on("py-numpy", type=('build', 'run'))
+
+    def setup_environment(self, spack_env, run_env):
+#        site_packages_subdir = join_path( ('python' + 
+#            str(self.spec['python'].version.up_to(2))),
+#            'site-packages')
+#        site_packages_dir = join_path(self.spec.prefix.lib,
+#                site_packages_subdir)
+        site_packages_dir = self.prefix.lib
+        spack_env.prepend_path('PYTHONPATH', site_packages_dir)
+        run_env.prepend_path('PYTHONPATH', site_packages_dir)
 
