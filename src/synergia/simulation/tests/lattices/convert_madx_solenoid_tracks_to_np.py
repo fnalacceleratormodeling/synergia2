91f342d66c5bd502f96da78928a2ab938f0da475
Eric G. Stern <egstern@fnal.gov>
2018-08-06 10:11:05 -0500

    files to run solenoid test

---
+++ src/synergia/simulation/tests/lattices/convert_madx_solenoid_tracks_to_np.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+import numpy as np
+
+madxoutput = sys.argv[1]
+print "reading file: ", madxoutput
+
+basename = os.path.splitext(madxoutput)[0]
+print "writing numpy data to ", basename+".npy"
+
+f = open(madxoutput)
+lines = f.readlines()
+nlines = len(lines)
+f.close()
+
+# format after header and first observation is
+# number turn x px y py t pt s e
+trackdata = np.loadtxt(madxoutput, skiprows=nlines-16, usecols=(2,3,4,5,6,7))
+np.save(basename+".npy", trackdata)
