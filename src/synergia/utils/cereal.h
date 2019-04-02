8304c64e221883ea3f1eba888784712ba47ffb8a
Qiming Lu <qlu@fnal.gov>
2019-04-02 14:44:04 -0500

    adde cereal to the utils

---
+++ src/synergia/utils/cereal.h
@@ -0,0 +1,26 @@
+#ifndef SYNERGIA_UTILS_CEREAL_H_
+#define SYNERGIA_UTILS_CEREAL_H_
+
+#pragma clang diagnostic ignored "-Wc++11-extensions"
+
+#include <cereal/archive/binary.hpp>
+
+//#include <cereal/archive/xml_iarchive.hpp>
+//#include <cereal/archive/xml_oarchive.hpp>
+
+#if 0
+#include <cereal/types/version.hpp>
+#include <cereal/types/base_object.hpp>
+#include <cereal/types/utility.hpp>
+#include <cereal/types/assume_abstract.hpp>
+#include <cereal/types/nvp.hpp>
+#include <cereal/types/complex.hpp>
+#include <cereal/types/list.hpp>
+#include <cereal/types/array.hpp>
+#include <cereal/types/vector.hpp>
+#include <cereal/types/map.hpp>
+#include <cereal/types/shared_ptr.hpp>
+#include <cereal/types/export.hpp>
+#endif
+
+#endif /* CEREAL_H */
