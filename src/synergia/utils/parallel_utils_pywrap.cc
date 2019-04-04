567bc3c340bb3d2d7428866bf8903e0b3e280e1f
Qiming Lu <qlu@fnal.gov>
2019-04-04 10:20:48 -0500

    pybind for Logger class

--- src/synergia/utils/parallel_utils_pywrap.cc
+++ src/synergia/utils/parallel_utils_pywrap.cc
@@ -1,8 +1,9 @@
 
 #include <pybind11/pybind11.h>
 
-#include "commxx.h"
-#include "parallel_utils.h"
+#include "synergia/utils/parallel_utils.h"
+#include "synergia/utils/commxx.h"
+#include "synergia/utils/logger.h"
 
 namespace py = pybind11;
 
@@ -11,6 +12,17 @@ PYBIND11_MODULE(parallel_utils_py, m)
     m.def("generate_subcomms", generate_subcomms);
     m.def("make_optimal_spc_comm", make_optimal_spc_comm);
     m.def("decompose_1d_local", decompose_1d_local);
+
+    py::class_<Logger>(m, "Logger")
+        .def(py::init<int>())
+        .def(py::init<int, std::string const& >())
+        .def(py::init<int, std::string const&, bool >())
+        .def(py::init<int, std::string const&, bool, bool >())
+        .def(py::init<std::string const& >())
+        .def(py::init<std::string const&, bool >())
+        .def("write", &Logger::write, py::return_value_policy::reference_internal)
+        .def("flush", &Logger::flush, py::return_value_policy::reference_internal)
+        ;
 }
 
 
