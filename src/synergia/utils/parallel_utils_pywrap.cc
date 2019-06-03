bc6bf7f6da6e264ecf230d2d761f25a065995d5b
Qiming Lu <qlu@fnal.gov>
2019-06-03 15:35:17 -0500

    python wrapper for logger

--- src/synergia/utils/parallel_utils_pywrap.cc
+++ src/synergia/utils/parallel_utils_pywrap.cc
@@ -13,13 +13,23 @@ PYBIND11_MODULE(parallel_utils_py, m)
     m.def("make_optimal_spc_comm", make_optimal_spc_comm);
     m.def("decompose_1d_local", decompose_1d_local);
 
+    py::enum_<LoggerV>(m, "LoggerV", py::arithmetic())
+        .value("DEBUG",   LoggerV::DEBUG)
+        .value("DINFO",   LoggerV::DINFO)
+        .value("INFO",    LoggerV::INFO)
+        .value("WARNING", LoggerV::WARNING)
+        .value("ERROR",   LoggerV::ERROR)
+        ;
+
     py::class_<Logger>(m, "Logger")
         .def(py::init<int>())
         .def(py::init<int, std::string const& >())
-        .def(py::init<int, std::string const&, bool >())
-        .def(py::init<int, std::string const&, bool, bool >())
+        .def(py::init<int, std::string const&, LoggerV >())
+        .def(py::init<int, std::string const&, LoggerV, bool >())
+        .def(py::init<int, std::string const&, LoggerV, bool, bool >())
         .def(py::init<std::string const& >())
-        .def(py::init<std::string const&, bool >())
+        .def(py::init<std::string const&, LoggerV >())
+        .def(py::init<std::string const&, LoggerV, bool >())
         .def("write", &Logger::write, py::return_value_policy::reference_internal)
         .def("flush", &Logger::flush, py::return_value_policy::reference_internal)
         ;
