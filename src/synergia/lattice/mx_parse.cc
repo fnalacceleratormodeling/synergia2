f3f8f0e77e25e4a105a847bab8d052633fbc28e3
Qiming Lu <qlu@fnal.gov>
2019-07-10 10:54:55 -0500

    fixed the implicit casting from const char* to char* in mx_parse.cc

--- src/synergia/lattice/mx_parse.cc
+++ src/synergia/lattice/mx_parse.cc
@@ -613,9 +613,9 @@ struct synergia::madx_tree_parser
 
     flag_attr =   // flag attributes, attributes with values only, etc.
           ( -char_("-") >> name )                 [phx::bind(&set_flag_attr, _val, _1, _2)]
-        | ( dblq_str )   [phx::bind(&set_attr, _val, "default", boost::optional<char>(), _1)]
-        | ( snglq_str )  [phx::bind(&set_attr, _val, "default", boost::optional<char>(), _1)]
-        | ( expr )       [phx::bind(&set_attr, _val, "default", boost::optional<char>(), _1)]
+        | ( dblq_str )   [phx::bind(&set_attr, _val, string("default"), optional<char>(), _1)]
+        | ( snglq_str )  [phx::bind(&set_attr, _val, string("default"), optional<char>(), _1)]
+        | ( expr )       [phx::bind(&set_attr, _val, string("default"), optional<char>(), _1)]
         ;
 
     cmd_attr =
