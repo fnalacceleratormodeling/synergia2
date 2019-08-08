8df1bfaad515d2fa88de5585dd7f50865174e5c3
Qiming Lu <qlu@fnal.gov>
2019-08-08 10:34:12 -0500

    bunch test using catch2

---
+++ src/synergia/bunch/tests/test_bunch_traits.cc
@@ -0,0 +1,31 @@
+#define CATCH_CONFIG_MAIN
+#include "synergia/utils/catch.hpp"
+
+#include <Kokkos_Core.hpp>
+
+#include <map>
+#include <vector>
+#include <memory>
+#include <type_traits>
+
+struct TinyBunch
+{
+    explicit TinyBunch(int s) : size(s) { }
+
+    TinyBunch(TinyBunch const&) = delete;
+    TinyBunch(TinyBunch &&) = default;
+
+    int size;
+    Kokkos::View<double*> particles;
+    std::map<std::string, std::unique_ptr<int>> diags;
+};
+
+
+TEST_CASE("Bunch traits", "[Bunch]")
+{
+    CHECK( true );
+
+    std::vector<TinyBunch> bunches;
+    bunches.emplace_back(3);
+}
+
