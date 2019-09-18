62ec3b3a7403af021120469a7d65809cdd2e4bc0
Qiming Lu <qlu@fnal.gov>
2019-09-18 10:52:48 -0500

    independent_stepper_elements implemented

--- src/synergia/simulation/independent_stepper_elements.h
+++ src/synergia/simulation/independent_stepper_elements.h
@@ -1,5 +1,6 @@
 #ifndef INDEPENDENT_STEPPER_ELEMENTS_H_
 #define INDEPENDENT_STEPPER_ELEMENTS_H_
+
 #include "synergia/simulation/stepper.h"
 
 /// The Independent_stepper_elements class generates a constant number of
@@ -8,34 +9,17 @@
 class Independent_stepper_elements : public Stepper
 {
 private:
-    void
-    construct(int steps_per_element);
+
+    int steps_per_element;
+
+    std::vector<Step> 
+    apply_impl(Lattice const & lattice) const override;
+
 public:
-    /// Construct an Independent_stepper
-    /// @param lattice_sptr the Lattice
-    /// @param map_order order for Chef_map operations
-    /// @param steps_per_element the number of steps per thick element
-    Independent_stepper_elements(Lattice_sptr lattice_sptr, int map_order,
-            int steps_per_element);
 
-    /// Deprecated. Construct an Independent_stepper
-    /// @param lattice_simulator the Lattice_simulator for the Lattice
-    /// @param steps_per_element the number of steps per thick element
-    Independent_stepper_elements(Lattice_simulator const& lattice_simulator,
-            int steps_per_element);
-
-    /// Default constructor for serialization use only
-    Independent_stepper_elements();
-
-    template<class Archive>
-        void
-        serialize(Archive & ar, const unsigned int version);
-
-    virtual
-    ~Independent_stepper_elements();
+    Independent_stepper_elements(int steps_per_element)
+        : steps_per_element(steps_per_element)
+    { }
 };
-BOOST_CLASS_EXPORT_KEY(Independent_stepper_elements);
-typedef boost::shared_ptr<Independent_stepper_elements >
-        Independent_stepper_elements_sptr; // syndoc:include
 
 #endif /* INDEPENDENT_STEPPER_ELEMENTS_H_ */
