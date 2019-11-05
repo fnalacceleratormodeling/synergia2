8954365e9053e29a61bdaa668730a8b1ac15aed2
Qiming Lu <qlu@fnal.gov>
2019-11-05 10:14:15 -0600

    container_conversion for pybind11

--- src/synergia/lattice/lattice_pywrap.cc
+++ src/synergia/lattice/lattice_pywrap.cc
@@ -3,55 +3,19 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
+#include "synergia/utils/container_conversions.h"
 #include "synergia/simulation/propagator.h"
 
 namespace py = pybind11;
 using namespace py::literals;
 
-
-namespace pybind11
-{
-    namespace detail
-    {
-        template <> struct type_caster<std::list<Lattice_element>>
-        {
-            using value_conv = make_caster<Lattice_element>;
-
-            bool load(handle src, bool convert) 
-            { return false; }
-
-            template <typename T>
-            static handle cast(T && src, 
-                    return_value_policy policy, 
-                    handle parent) 
-            {
-                if (!std::is_lvalue_reference<T>::value)
-                    policy = return_value_policy_override<Lattice_element>::policy(policy);
-                tuple t(src.size());
-                size_t index = 0;
-
-                for (auto &&value : src) 
-                {
-                    auto value_ = reinterpret_steal<object>(
-                            value_conv::cast(forward_like<T>(value), policy, parent));
-
-                    if (!value_)
-                        return handle();
-
-                    // steals a reference
-                    PyTuple_SET_ITEM(t.ptr(), (ssize_t) index++, value_.release().ptr());
-                }
-
-                return t.release();
-            }
-
-            PYBIND11_TYPE_CASTER(std::list<Lattice_element>, _("Tuple[") + value_conv::name + _("]"));
-        };
-    }
-}
+// convert the std::list<Lattice_element> to a python tuple object
+template <> 
+struct pybind11::detail::type_caster<std::list<Lattice_element>> 
+    : pybind11::detail::py_tuple_caster<std::list<Lattice_element>, Lattice_element> { };
 
 
-
+// lattice python module
 PYBIND11_MODULE(lattice, m)
 {
     py::enum_<element_type>(m, "element_type", py::arithmetic())
