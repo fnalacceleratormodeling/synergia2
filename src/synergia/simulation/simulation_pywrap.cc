ff3fdec33f4981a057d994d3a5416f073b2a2a07
Qiming Lu <qlu@fnal.gov>
2019-11-07 14:06:49 -0600

    lattice_simulator methods wrapped in python

--- src/synergia/simulation/simulation_pywrap.cc
+++ src/synergia/simulation/simulation_pywrap.cc
@@ -1,9 +1,12 @@
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include "synergia/simulation/propagator.h"
+#include "synergia/simulation/lattice_simulator.h"
 
 namespace py = pybind11;
+using namespace py::literals;
 
 PYBIND11_MODULE(simulation, m)
 {
@@ -12,6 +15,19 @@ PYBIND11_MODULE(simulation, m)
         .def("propagate", &Propagator::propagate)
         .def("print_steps", &Propagator::print_steps)
         ;
+
+    m.def_submodule("Lattice_simulator")
+        .def( "tune_linear_lattice", 
+                &Lattice_simulator::tune_linear_lattice,
+                "Tune linear lattice.",
+                "lattice"_a )
+
+        .def( "tune_circular_lattice", 
+                &Lattice_simulator::tune_circular_lattice,
+                "Tune a circular lattice.",
+                "lattice"_a, "tolerance"_a = 1.0e-13 )
+        ;
+
 }
 
 
