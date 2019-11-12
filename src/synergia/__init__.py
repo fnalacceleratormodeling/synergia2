bea541e682941fa2fc8f8d7a470b924371e67d3c
Qiming Lu <qlu@fnal.gov>
2019-11-12 10:29:56 -0600

    automated the simulation init() and finalize() calls

--- src/synergia/__init__.py
+++ src/synergia/__init__.py
@@ -1,7 +1,7 @@
 # importing mpi4py here makes HopperII happy; there doesn't seem to be
 # anything wrong with doing so.
 #import mpi4py
-#from mpi4py import MPI
+from mpi4py import MPI
 
 #from version import __version__, version_major, version_minor, version_patch, version_tweak
 #import convertors
@@ -12,3 +12,17 @@ import lattice
 #import optics
 import simulation
 import collective
+
+# Kokkos init
+simulation.init()
+
+# Kokkos finalize
+def finalize():
+    import gc
+    gc.collect()
+    simulation.finalize()
+
+import atexit
+atexit.register(finalize)
+
+
