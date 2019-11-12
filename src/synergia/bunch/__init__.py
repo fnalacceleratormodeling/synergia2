6b7b2840ea8be543c9a07dc21a8133e72293c35d
Qiming Lu <qlu@fnal.gov>
2019-11-12 10:29:06 -0600

    added Bunch.get_particles_numpy() in python to return the particles in a numpy array

--- src/synergia/bunch/__init__.py
+++ src/synergia/bunch/__init__.py
@@ -1,1 +1,7 @@
 from bunch import *
+import numpy as np
+
+def get_particles_numpy(self):
+    return np.array(self.get_host_particles(), copy=False)
+
+setattr(Bunch, 'get_particles_numpy', get_particles_numpy)
