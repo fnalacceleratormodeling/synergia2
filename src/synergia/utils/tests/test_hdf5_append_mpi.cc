cdd538d07526569a23b0e11d34bd9c1cb3f94292
Qiming Lu <qlu@fnal.gov>
2020-03-05 09:02:26 -0600

    test of append resume

--- src/synergia/utils/tests/test_hdf5_append_mpi.cc
+++ src/synergia/utils/tests/test_hdf5_append_mpi.cc
@@ -39,14 +39,53 @@ TEST_CASE("hdf5_append_scalar", "[Hdf5_file_append]")
     }
 
     {
-        Hdf5_file file(fname, Hdf5_file::read_write, Commxx());
+        Hdf5_file file(fname, Hdf5_file::read_only, Commxx());
+
+        // v1
+        auto v1 = file.read<karray1i_row>("v1");
+        REQUIRE(v1.extent(0) == 2);
+        CHECK(v1(0) == 3);
+        CHECK(v1(1) == 4);
+
+        // v2
+        auto v2 = file.read<karray1d_row>("v2");
+        REQUIRE(v2.extent(0) == 2);
+        CHECK(v2(0) == 3.2);
+        CHECK(v2(1) == 4.2);
+    }
+}
+
+
+TEST_CASE("hdf5_append_resume", "[Hdf5_file_append]")
+{
+    int mpi_rank = Commxx::world_rank();
+    int mpi_size = Commxx::world_size();
+
+    std::stringstream ss;
+    ss << "hdf5_append_resume_" << mpi_size << ".h5";
+    std::string fname = ss.str();
+
+    {
+        // create a new file
+        Hdf5_file file(fname, Hdf5_file::truncate, Commxx());
 
         // v1: [3, 4]
+        file.append("v1", 3, false);
+        file.append("v1", 4, false);
+    }
+
+    {
+        // close and re-open to append more
+        Hdf5_file file(fname, Hdf5_file::read_write, Commxx());
+
+        // v1: [3, 4, 5, 6]
         file.append("v1", 5, false);
         file.append("v1", 6, false);
 
-        file.append("v3", 3.0, true);
-        file.append("v3", 4.0, true);
+        // v2: [5.0, 6.0, 7.0]
+        file.append("v2", 5.0, false);
+        file.append("v2", 6.0, false);
+        file.append("v2", 7.0, false);
     }
 
     {
@@ -62,9 +101,10 @@ TEST_CASE("hdf5_append_scalar", "[Hdf5_file_append]")
 
         // v2
         auto v2 = file.read<karray1d_row>("v2");
-        REQUIRE(v2.extent(0) == 2);
-        CHECK(v2(0) == 3.2);
-        CHECK(v2(1) == 4.2);
+        REQUIRE(v2.extent(0) == 3);
+        CHECK(v2(0) == 5.0);
+        CHECK(v2(1) == 6.0);
+        CHECK(v2(2) == 7.0);
     }
 }
 
