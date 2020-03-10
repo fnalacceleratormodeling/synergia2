d1eb49b6d966c2dcc1e4c25a238a659c104cc75a
Qiming Lu <qmlu79@gmail.com>
2020-03-10 12:05:23 -0500

    default virtual destructor for CO_options

--- src/synergia/simulation/collective_operator_options.h
+++ src/synergia/simulation/collective_operator_options.h
@@ -7,6 +7,7 @@ class Collective_operator;
 
 struct CO_options
 {
+    virtual ~CO_options() = default;
     virtual CO_options * clone() const = 0;
     virtual Collective_operator * create_operator() const = 0;
 
