2a246de6ea6dea402275dfa998688bd089c28751
Qiming Lu <qlu@fnal.gov>
2020-05-27 13:26:24 -0500

    distributed fft3d cuda implementation

---
+++ src/synergia/utils/distributed_fft3d_cuda.cc
@@ -0,0 +1,73 @@
+#include <cstring>
+#include <stdexcept>
+#include "distributed_fft3d_cuda.h"
+
+Distributed_fft3d::Distributed_fft3d()
+    : shape()
+    , comm(MPI_COMM_NULL)
+    , plan()
+    , invplan()
+
+    , lower(0)
+    , nz(0)
+{
+}
+
+void Distributed_fft3d::construct(std::array<int, 3> const & new_shape, MPI_Comm new_comm)
+{
+    cufftDestroy(plan);
+
+    if (new_comm == MPI_COMM_NULL)
+    {
+        comm = MPI_COMM_NULL;
+        return;
+    }
+
+    int comm_size = 0;
+    MPI_Comm_size(new_comm, &comm_size);
+
+    if (comm_size != 1)
+    {
+        throw std::runtime_error(
+                "Distributed_fft3d: number of processor must be 1 "
+                "for CUDA implementation" );
+    }
+
+    shape = new_shape;
+    comm = new_comm;
+
+    lower = 0;
+    nz = shape[2];
+
+    auto res = cufftPlan3d(&plan, shape[2], shape[1], shape[0], CUFFT_D2Z);
+    res = cufftPlan3d(&invplan, shape[2], shape[1], shape[0], CUFFT_Z2D);
+}
+
+void
+Distributed_fft3d::transform(karray1d_dev& in, karray1d_dev& out)
+{
+    auto res = cufftExecD2Z( plan, 
+            (cufftDoubleReal*)in.data(),
+            (cufftDoubleComplex*)out.data() );
+}
+
+void
+Distributed_fft3d::inv_transform(karray1d_dev& in, karray1d_dev& out)
+{
+    cufftExecZ2D( invplan, 
+            (cufftDoubleComplex*)in.data(),
+            (cufftDoubleReal*)out.data() );
+}
+
+double
+Distributed_fft3d::get_roundtrip_normalization() const
+{
+    return 1.0 / (shape[0] * shape[1] * shape[2]);
+}
+
+Distributed_fft3d::~Distributed_fft3d()
+{
+    cufftDestroy(plan);
+    cufftDestroy(invplan);
+}
+
---
+++ src/synergia/utils/distributed_fft3d_cuda.h
@@ -0,0 +1,53 @@
+#ifndef DISTRIBUTED_FFT2D_H_
+#define DISTRIBUTED_FFT2D_H_
+
+#include <vector>
+#include <string>
+
+#include <cufft.h>
+
+#include "synergia/utils/multi_array_typedefs.h"
+#include "synergia/utils/commxx.h"
+
+class Distributed_fft3d
+{
+
+private:
+
+    std::array<int, 3> shape;
+    MPI_Comm comm;
+
+    cufftHandle plan;
+    cufftHandle invplan;
+
+    int lower;
+    int nz;
+
+public:
+
+    static int get_padded_shape_real(int s)
+    { return 2*(s/2+1); }
+
+    static int get_padded_shape_cplx(int s)
+    { return s/2+1; }
+
+    Distributed_fft3d();
+    ~Distributed_fft3d();
+
+
+    int get_lower() const { return lower; }
+    int get_upper() const { return lower + nz; }
+
+    int padded_nx_real() const { return get_padded_shape_real(shape[0]); }
+    int padded_nx_cplx() const { return get_padded_shape_cplx(shape[0]); }
+
+    std::array<int, 3> const& get_shape() const { return shape; }
+
+    void construct(std::array<int, 3> const& shape, MPI_Comm comm);
+
+    void transform(karray1d_dev& in, karray1d_dev& out);
+    void inv_transform(karray1d_dev& in, karray1d_dev& out);
+    double get_roundtrip_normalization() const;
+};
+
+#endif /* DISTRIBUTED_FFT2D_H_ */
