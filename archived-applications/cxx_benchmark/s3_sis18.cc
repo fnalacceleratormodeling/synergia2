1d03034e8e6b4fa0c2bdc0b5cbd0d275d6b58917
Eric G. Stern <egstern@fnal.gov>
2020-06-17 14:41:25 -0500

    printouts

--- archived-applications/cxx_benchmark/s3_sis18.cc
+++ archived-applications/cxx_benchmark/s3_sis18.cc
@@ -30,15 +30,21 @@ void print_bunch_statistics(Bunch_sptr bunch)
          << std::showpos
          << std::ios::scientific;
 
+    std::cerr << std::endl;
+
     for(int i=0; i<6; ++i)
-        std::cerr << mean[i] << ", " << std[i] << "\n";
+        std::cerr << i << ": " << mean[i] << " \t " << std[i] << "\n";
+
+    std::cerr << std::endl;
 
     auto parts = bunch->get_local_particles();
 
+    std::cerr << "Particles 1-4" << std::endl;
     for(int p=0; p<4; ++p) 
     {
+        std::cerr << p << ":";
         for(int i=0; i<6; ++i)
-            std::cerr << parts[p][i] << ", ";
+            std::cerr << "  " << parts[p][i];
         std::cerr << "\n";
     }
 
@@ -48,14 +54,29 @@ void print_bunch_statistics(Bunch_sptr bunch)
 
 void run_and_save(std::string & prop_str, std::string & sim_str)
 {
+    Commxx_sptr comm(new Commxx());
+    const int myrank = comm->get_rank();
+    const int mpisize = comm->get_size();
+
     std::vector<int> grid_shape = {64, 64, 64};
 
-    auto lsexpr = read_lsexpr_file("sis18-6.lsx");
+    static std::string lattice_file = "sis18-6.lsx";
+    auto lsexpr = read_lsexpr_file(lattice_file);
     Lattice_sptr lattice(new Lattice(lsexpr));
 
     lattice->set_all_string_attribute("extractor_type", "libff");
 
+    if (myrank == 0) {
+        std::cout << "read lattice from file " << lattice_file << std::endl;
+        std::cout << "    length: " << lattice->get_length() << std::endl;
+        std::cout << "    # elements: " << lattice->get_elements().size() << std::endl;
+    }
+
     Lattice_simulator lsim(lattice, 2);
+
+    if (myrank == 0) {
+        std::cout << "created lattice simulator" << std::endl;
+    }
 
     // tune the lattice
     //Lattice_simulator::tune_circular_lattice(lattice);
@@ -64,32 +85,52 @@ void run_and_save(std::string & prop_str, std::string & sim_str)
     // get the reference particle
     auto const & ref = lattice->get_reference_particle();
 
-    std::cout << "reference momentum = " 
-        << ref.get_momentum() << " GeV\n";
+    if (myrank == 0) {
+        std::cout << "reference momentum = " 
+                  << ref.get_momentum() << " GeV\n";
+    }
 
     // space charge
     //Commxx_divider_sptr commxx_divider_sptr(new Commxx_divider(32, false));
     //Space_charge_2d_open_hockney_sptr sc(new Space_charge_2d_open_hockney(commxx_divider_sptr, {64, 64, 64}));
     Space_charge_3d_open_hockney_sptr sc(new Space_charge_3d_open_hockney({64, 64, 64}));
     //Dummy_collective_operator_sptr sc(new Dummy_collective_operator("dummy"));
+    if (myrank == 0) {
+        std::cout << "created space charge operator: " << sc->get_type() << ":" << sc->get_name() << std::endl;
+    }
 
     // stepper
     Split_operator_stepper_sptr stepper(
             new Split_operator_stepper(lsim, sc, 71));
 
+    if (myrank == 0) {
+        std::cout << "created stepper" << std::endl;
+    }
+
     // Propagator
     Propagator propagator(stepper);
 
     // bunch simulator
-    Commxx_sptr comm(new Commxx());
     Bunch_sptr bunch(new Bunch(ref, 4194394, 2.94e10, comm));
     bunch->set_bucket_index(0);
 
+    const std::string bunchdatafile = "/data/egstern/sis18_bigstep_generate4M.00/turn_particles_0000.h5";
+    if (myrank == 0) {
+        std::cout << "Reading bunch data from file: " << bunchdatafile << "...";
+    }
+
     // or read from file
-    bunch->read_file("turn_particles_0000_4M.h5");
+    bunch->read_file(bunchdatafile);
+    if (myrank == 0) {
+        std::cout << " read " << bunch->get_total_num() << " particles" << std::endl;
+    }
 
     // bunch statistics
-    print_bunch_statistics(bunch);
+    if (myrank == 0) {
+        std::cout << "Initial bunch statistics" << std::endl;
+        print_bunch_statistics(bunch);
+        std::cout << std::endl;
+    }
 
     // bunch simulator
     Bunch_simulator bsim(bunch);
@@ -103,7 +144,11 @@ void run_and_save(std::string & prop_str, std::string & sim_str)
     propagator.propagate(bsim, 1, 1, 2);
 
     // bunch statistics
-    print_bunch_statistics(bunch);
+    if (myrank == 0) {
+        std::cout << std::endl;
+        std::cout << "Final bunch statistics" << std::endl;
+        print_bunch_statistics(bunch);
+    }
 
     return;
 }
