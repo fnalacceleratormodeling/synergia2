f43e8fc9628edac4c2cac8b51507f509e22b1a74
Eric G. Stern <egstern@fnal.gov>
2020-06-19 19:31:03 -0500

    benchmark files in python to mirror c++ and install needed files

--- archived-applications/cxx_benchmark/benchmark_options.py
+++ archived-applications/cxx_benchmark/benchmark_options.py
@@ -4,9 +4,9 @@ from synergia_workflow import Options, Job_manager
 
 opts = Options("benchmark")
 #opts.add("spacecharge", 1, "which space charge calculation: 0=None, 1=Hockney")
-opts.add("gridx", 32, "space charge grid cells in x-direction")
-opts.add("gridy", 32, "space charge grid cells in y-direction")
-opts.add("gridz", 64, "space charge grid cells in z-direction")
+opts.add("gridx", 64, "space charge grid cells in x-direction")
+opts.add("gridy", 64, "space charge grid cells in y-direction")
+opts.add("gridz", 256, "space charge grid cells in z-direction")
 opts.add("partpercell", 10, "number of macro particles per grid cell")
 opts.add("sortperiod", 1000, "sort period")
 opts.add("autotune", True, "automatically tune communication routines")
@@ -16,7 +16,7 @@ opts.add("avoid", True, "whether to use communication avoidance")
 opts.add("diagnostics", False, "write diagnostics")
 opts.add("verbosity", 1, "verbosity (0=minimal output ... 6=detailed output)")
 job_mgr = Job_manager("benchmark", opts,
-                      ["cxx_covariance_matrix.xml", "cxx_lattice.xml",
-                       "cxx_means.xml"],
+                      ["fodo.madx", "cxx_covariance_matrix.txt",
+                       "cxx_means.txt"],
                        standalone=True)
 
