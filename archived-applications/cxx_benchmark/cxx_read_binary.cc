eb8c54c32691a3f1d19366bba5ae8e0be3b4a5c4
Eric G. Stern <egstern@fnal.gov>
2020-07-04 11:53:45 -0500

    read and write binary multiarray test

---
+++ archived-applications/cxx_benchmark/cxx_read_binary.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stdexcept>
+
+#include "synergia/lattice/lattice.h"
+#include "synergia/utils/serialization.h"
+
+#include "synergia/utils/synergia_omp.h"
+
+// We put the actual code in a separate function so that shared_ptr's can
+// be cleanup up properly before we call MPI_Finalize.
+void
+run()
+{
+    MArray2d covariances;
+    binary_load(covariances, "covariance.xml");
+    std::cout.precision(16);
+    for (int i=0; i<6; ++i) {
+        if (i != 0) {
+            std::cout << std::endl;
+        }
+        for (int j=0; j<6; ++j) {
+            if (j != 0) {
+                std::cout << " ";
+            }
+            std::cout << covariances[i][j];
+        }
+    }
+    std::cout << std::endl;
+}
+
+int
+main(int argc, char **argv)
+{
+    MPI_Init(&argc, &argv);
+    run();
+    MPI_Finalize();
+    return 0;
+}
