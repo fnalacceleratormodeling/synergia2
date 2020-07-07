28ec569c072c15ca90cca88bbc75d6ec4c5200b1
Marc Paterno <paterno@fnal.gov>
2020-07-07 09:09:26 -0700

    Add test for boost void_caster

---
+++ src/synergia/bunch/tests/test_boost_void_caster.cc
@@ -0,0 +1,32 @@
+
+#define BOOST_TEST_MAIN
+//#include "boost/serialization/void_cast.hpp"
+#include "boost/test/unit_test.hpp"
+#include <cstddef>
+
+class Base
+{
+public:
+  virtual ~Base() {}; 
+  virtual int foo() = 0;
+
+  int i() const { return i_; }
+private:
+  int i_ = 1;
+};
+
+class Derived : public Base
+{
+  int foo() override;
+};
+
+int Derived::foo() { return i() * 2; };
+
+BOOST_AUTO_TEST_CASE(test1)
+{
+ // boost::serialization::void_cast_register<Derived, Base>();
+ Base*    pb = reinterpret_cast<Base*>(1 << 20);
+ Derived* pd = static_cast<Derived*>(pb);
+ auto dist = reinterpret_cast<std::ptrdiff_t>(pd);
+ BOOST_CHECK(dist == (1 << 20));
+}
