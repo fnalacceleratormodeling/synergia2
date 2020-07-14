0ab0040a70e1ca0db7de3862da1ffd88ee6b628c
Qiming Lu <qlu@fnal.gov>
2020-07-13 23:29:04 -0500

    longitudinal periodicity

--- src/synergia/bunch/period.cc
+++ src/synergia/bunch/period.cc
@@ -14,6 +14,27 @@ namespace
            else return false;
         }
     };
+
+    struct alg_z_period
+    {
+        Particles p;
+        ConstParticleMasks masks;
+
+        double length_cdt;
+        double half_length;
+        
+        KOKKOS_INLINE_FUNCTION
+        void operator()(const int i) const
+        {
+            if (masks(i))
+            {
+                double tmp = p(i, 4) + half_length;
+                if(tmp>0) p(i, 4) = fmod(tmp, length_cdt) - half_length;
+                else      p(i, 4) = fmod(tmp, length_cdt) + half_length;
+            }
+        }
+    };
+
 }
 
 
@@ -21,25 +42,38 @@ namespace
 
 void apply_longitudinal_periodicity(Bunch& bunch, double length)
 {
-#if 0
-       Bunch::State state=bunch.get_state(); 
-       bunch.convert_to_state(Bunch::fixed_z_lab); 
-       double beta=bunch. get_reference_particle().get_beta();      
-       double  length_cdt=length/beta;
-       double half_length=0.5*length_cdt;
+    //Bunch::State state=bunch.get_state(); 
+    //bunch.convert_to_state(Bunch::fixed_z_lab); 
 
-        MArray2d_ref particles(bunch.get_local_particles());
-        int local_num = bunch.get_local_num();
-        for (int part = 0; part < local_num; ++part) {
-            double tmp = particles[part][Bunch::cdt]+ half_length;
-            if (tmp > 0) {
-                particles[part][Bunch::cdt] = fmod(tmp, length_cdt) -half_length ;
-            } else {
-                particles[part][Bunch::cdt] = fmod(tmp, length_cdt) + half_length;
-            }
-        }
-        bunch.convert_to_state(state); 
-#endif
+    double beta = bunch.get_reference_particle().get_beta();      
+    double length_cdt = length / beta;
+    double half_length = 0.5 * length_cdt;
+
+    if (bunch.get_local_num(ParticleGroup::regular))
+    {
+        alg_z_period alg{
+            bunch.get_local_particles(ParticleGroup::regular),
+            bunch.get_local_particle_masks(ParticleGroup::regular),
+            length_cdt, half_length
+        };
+
+        Kokkos::parallel_for(
+                bunch.get_local_num(ParticleGroup::regular), alg);
+    }
+
+    if (bunch.get_local_num(ParticleGroup::spectator))
+    {
+        alg_z_period alg{
+            bunch.get_local_particles(ParticleGroup::spectator),
+            bunch.get_local_particle_masks(ParticleGroup::spectator),
+            length_cdt, half_length
+        };
+
+        Kokkos::parallel_for(
+                bunch.get_local_num(ParticleGroup::spectator), alg);
+    }
+
+    //bunch.convert_to_state(state); 
 }
 
 void apply_longitudinal_bucket_barrier(Bunch& bunch, double length)
@@ -51,9 +85,11 @@ void apply_zcut(Bunch& bunch, double length)
 {
     //Bunch::State state = bunch.get_state(); 
     //bunch.convert_to_state(Bunch::fixed_z_lab); 
+
     double beta = bunch.get_reference_particle().get_beta();
     double half_length = 0.5 * length / beta;	
     bunch.apply_zcut(Zcut_aperture{half_length});
+
     //bunch.convert_to_state(state); 
 }
 
