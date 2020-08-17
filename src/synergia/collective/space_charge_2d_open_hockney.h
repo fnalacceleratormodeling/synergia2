72949d28a4f16d05474569d4da63fffc1f9f7890
Qiming Lu <qlu@fnal.gov>
2020-08-17 13:49:04 -0500

    clean up unused code blocks in sc2d

--- src/synergia/collective/space_charge_2d_open_hockney.h
+++ src/synergia/collective/space_charge_2d_open_hockney.h
@@ -58,15 +58,6 @@ CEREAL_REGISTER_TYPE(Space_charge_2d_open_hockney_options)
 class Space_charge_2d_open_hockney : public Collective_operator
 {
 
-public:
-
-#if 0
-    enum Green_fn_type
-    {
-        pointlike = 1, bruteforce = 2
-    };
-#endif
-
 private:
 
     const Space_charge_2d_open_hockney_options options;
@@ -83,49 +74,8 @@ private:
     karray1d_dev phi2;
     karray1d_dev g2;
 
-#if 0
-    karray1d_dev rho2hat;
-    karray1d_dev phi2hat;
-    karray1d_dev g2hat;
-#endif
-
     karray1d_hst h_rho2;
     karray1d_hst h_phi2;
-
-
-#if 0
-    std::vector<int > grid_shape, doubled_grid_shape;
-    Rectangular_grid_domain_sptr domain_sptr, doubled_domain_sptr;
-    bool periodic_z;
-    double z_period;
-    bool grid_entire_period;
-    Green_fn_type green_fn_type;
-    Distributed_fft2d_sptr distributed_fft2d_sptr;
-
-    Commxx_divider_sptr commxx_divider_sptr;
-    Commxx_sptr comm2_sptr, comm1_sptr;
-
-    std::vector<int > lowers1, lengths1;
-    int real_lower, real_upper, real_length;
-    std::vector<int > real_lengths;
-    std::vector<int > real_lengths_1d;
-
-    int doubled_lower, doubled_upper;
-    int real_doubled_lower, real_doubled_upper;
-
-    double n_sigma;
-    double bunch_particle_charge, bunch_total_num;
-    double beta, gamma;
-    bool use_cell_coords;
-    bool need_state_conversion;
-    bool domain_fixed;
-    bool have_domains;
-
-    void setup_communication(Commxx_sptr const& bunch_comm_sptr);
-    void setup_derived_communication();
-    void setup_default_options();
-    void set_doubled_domain();
-#endif
 
     void construct_workspaces(std::array<int, 3> const& s);
 
@@ -160,48 +110,6 @@ public:
 
     Space_charge_2d_open_hockney(Space_charge_2d_open_hockney_options const & ops);
 
-#if 0
-    bool get_need_state_conversion();
-    double get_n_sigma() const;
-
-    void set_fixed_domain(Rectangular_grid_domain_sptr domain_sptr);
-    void update_domain(Bunch const& bunch);
-
-    Rectangular_grid_domain const& get_domain() const
-    { return *domain_sptr; }
-
-    /// Returns local charge density on doubled grid in [C/m^3]
-    Rectangular_grid_sptr
-    get_local_charge_density(Bunch const& bunch);
-
-    /// Returns global charge density on doubled grid in [C/m^3]
-    Distributed_rectangular_grid_sptr
-    get_global_charge_density2(
-            Rectangular_grid const& local_charge_density,
-            Commxx_sptr comm_sptr);
-
-    /// Returns Green function on the doubled grid in [1/m^3]
-    Distributed_rectangular_grid_sptr
-    get_green_fn2_pointlike();
-
-    Distributed_rectangular_grid_sptr
-    get_green_fn2_brute_force();
-
-    Distributed_rectangular_grid_sptr
-    get_local_force2(Distributed_rectangular_grid & charge_density2,
-            Distributed_rectangular_grid & green_fn2);
-
-    Rectangular_grid_sptr
-    get_global_electric_force2(
-            Distributed_rectangular_grid const& dist_force);
-
-    void
-    apply_kick(Bunch & bunch, Distributed_rectangular_grid const& rho2_1d,
-            Rectangular_grid const& Fn, double delta_tau);
-
-    virtual void
-    apply(Bunch & bunch, double time_step, Step & step, int verbosity, Logger & logger);
-#endif
 };
 
 inline Collective_operator * 
