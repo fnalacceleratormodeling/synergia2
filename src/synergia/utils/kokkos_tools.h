b0a054b42db38f34bf2e265f9984ace21bf5b537
Qiming Lu <qlu@fnal.gov>
2020-09-03 14:44:17 -0500

    added print_arr_sum() to kokkos tools

--- src/synergia/utils/kokkos_tools.h
+++ src/synergia/utils/kokkos_tools.h
@@ -91,6 +91,34 @@ namespace kt
         alg_zeroer alg{arr};
         Kokkos::parallel_for(arr.extent(0), alg);
     }
+
+    inline void
+    print_arr_sum( Logger& logger,
+                   karray1d_hst const& arr,
+                   int offset = 0,
+                   int length = -1 )
+    {
+        double sum = 0;
+        for(int i=offset; i<offset+length; ++i) sum += arr(i);
+
+        logger << std::setprecision(12);
+        logger << "      " << arr.label() << " = " << sum << "\n";
+    }
+
+    inline void
+    print_arr_sum( Logger& logger,
+                   karray1d_dev const& arr,
+                   int offset = 0,
+                   int length = -1 )
+    {
+        if (length<0) length = arr.extent(0);
+
+        karray1d_hst harr = Kokkos::create_mirror_view(arr);
+        Kokkos::deep_copy(harr, arr);
+
+        print_arr_sum(logger, harr, offset, length);
+    }
+
 }
 
 #endif
