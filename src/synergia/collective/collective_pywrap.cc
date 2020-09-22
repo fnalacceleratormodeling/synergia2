3d35d33bf330c665a6942d1bce824fc9fc450761
Qiming Lu <qlu@fnal.gov>
2020-09-22 13:16:44 -0500

    python wrappers for more space charge solvers

--- src/synergia/collective/collective_pywrap.cc
+++ src/synergia/collective/collective_pywrap.cc
@@ -5,6 +5,8 @@
 
 #include "dummy_collective_operator.h"
 #include "space_charge_2d_open_hockney.h"
+#include "space_charge_3d_open_hockney.h"
+#include "space_charge_rectangular.h"
 
 namespace py = pybind11;
 using namespace py::literals;
@@ -20,7 +22,29 @@ PYBIND11_MODULE(collective, m)
                 "gridx"_a, "gridy"_a, "gridz"_a )
         .def_readwrite( "comm_group_size", 
                 &Space_charge_2d_open_hockney_options::comm_group_size,
-                "Communication group size (must be 1 when running on GPUs)." )
+                "Communication group size (must be 1 on GPUs)." )
+        ;
+
+    py::class_<Space_charge_3d_open_hockney_options, CO_options>
+        (m, "Space_charge_3d_open_hockney_options")
+        .def( py::init<int, int, int>(),
+                "Construct the space charge 3d open hockney solver.",
+                "gridx"_a, "gridy"_a, "gridz"_a )
+        .def_readwrite( "comm_group_size", 
+                &Space_charge_3d_open_hockney_options::comm_group_size,
+                "Communication group size (must be 1 on GPUs)." )
+        ;
+
+ 
+    py::class_<Space_charge_rectangular_options, CO_options>
+        (m, "Space_charge_rectangular_options")
+        .def( py::init<std::array<int, 3> const&, 
+                std::array<double, 3> const& >(),
+                "Construct the rectangular space charge solver.",
+                "grid_shape"_a, "pipe_size"_a )
+        .def_readwrite( "comm_group_size", 
+                &Space_charge_rectangular_options::comm_group_size,
+                "Communication group size (must be 1 on GPUs)." )
         ;
 
     py::class_<Dummy_CO_options, CO_options>
