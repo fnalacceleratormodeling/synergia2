22762578ab85485ccab4b08d686509531aea32dd
Qiming Lu <qlu@fnal.gov>
2020-10-20 15:52:10 -0500

    print simple timer results after resume and propagate

--- examples/s3_sis18/resume.py
+++ examples/s3_sis18/resume.py
@@ -6,8 +6,13 @@ def resume():
     simlog = syn.utils.parallel_utils.Logger(0, 
             syn.utils.parallel_utils.LoggerV.INFO_STEP)
 
+    screen = syn.utils.parallel_utils.Logger(0, 
+            syn.utils.parallel_utils.LoggerV.DEBUG)
+
     [prop, sim] = syn.simulation.checkpoint_load()
     prop.propagate(sim, simlog, 1)
+
+    syn.utils.parallel_utils.simple_timer_print(screen)
 
 print("resuming sis_18")
 resume()
