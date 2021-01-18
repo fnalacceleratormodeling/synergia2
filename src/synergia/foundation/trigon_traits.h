3e1b25eb4cbe6f4876fb17458d892e6174314839
Qiming Lu <qlu@fnal.gov>
2021-01-18 13:04:57 -0600

    type traits for trigons

---
+++ src/synergia/foundation/trigon_traits.h
@@ -0,0 +1,12 @@
+#ifndef SYN_FOUNDATION_TRIGON_TRAITS_H
+#define SYN_FOUNDATION_TRIGON_TRAITS_H
+
+#include <type_traits>
+
+// general is_trigon type trait
+template<typename>
+struct is_trigon : std::false_type { };
+
+#endif
+
+
