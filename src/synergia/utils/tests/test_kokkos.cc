692debde7ad0cafec273b58cb24ad11c8a0e25f1
Qiming Lu <qlu@fnal.gov>
2021-03-04 13:49:28 -0600

    added message for the AllowPadding kokkos test

--- src/synergia/utils/tests/test_kokkos.cc
+++ src/synergia/utils/tests/test_kokkos.cc
@@ -7,6 +7,10 @@ TEST_CASE("kokkos_padding", "[BunchParticles]")
     auto darr = Kokkos::View<double**, Kokkos::LayoutLeft>(alloc, 11, 7);
     auto harr = Kokkos::create_mirror_view(darr);
 
+    INFO("This is a test for Kokkos::AllowPadding issue on the "
+            "device memory. If it fails, meaning the issue hasnt "
+            "been fixed in Kokkos yet");
+
     REQUIRE(darr.stride(0) == harr.stride(0));
     REQUIRE(darr.stride(1) == harr.stride(1));
 
