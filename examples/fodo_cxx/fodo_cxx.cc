e43c7b8a4b938f9722a3ed976e53aaecad4433eb
Eric G. Stern <egstern@fnal.gov>
2021-06-03 16:09:15 -0500

    Added fodo_cxx, standalone executable to run a fodo

---
+++ examples/fodo_cxx/fodo_cxx.cc
@@ -0,0 +1,148 @@
+// standalone c++ version of the fodo.py example
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "synergia/simulation/propagator.h"
+#include "synergia/simulation/split_operator_stepper_elements.h"
+#include "synergia/foundation/physical_constants.h"
+#include "synergia/bunch/populate.h"
+#include "synergia/bunch/core_diagnostics.h"
+#include "synergia/bunch/diagnostics_particles.h"
+#include "synergia/bunch/diagnostics_bulk_track.h"
+#include "synergia/bunch/diagnostics_full2.h"
+#include "synergia/lattice/madx_reader.h"
+
+#include "synergia/collective/space_charge_3d_open_hockney.h"
+
+Lattice get_lattice()
+{
+    static std::string fodo_madx(R"foo(
+beam, particle=proton,pc=3.0;
+
+o: drift, l=8.0;
+f: quadrupole, l=2.0, k1=0.071428571428571425;
+d: quadrupole, l=2.0, k1=-0.071428571428571425;
+
+fodo: sequence, l=20.0, refer=entry;
+fodo_1: f, at=0.0;
+fodo_2: o, at=2.0;
+fodo_3: d, at=10.0;
+fodo_4: o, at=12.0;
+endsequence;
+)foo");
+
+    MadX_reader reader;
+    reader.parse(fodo_madx);
+    return reader.get_lattice("fodo");
+}
+
+void print_bunch_statistics(Bunch const& bunch, Logger &logger)
+{
+    karray1d bunch_means(Core_diagnostics::calculate_mean(bunch));
+    karray1d_row bunch_stds(Core_diagnostics::calculate_std(bunch, bunch_means));
+
+    logger << "bunch means" << std::endl;
+    for (int i=0; i<6; ++i) {
+        logger << i << ":  " << std::setprecision(15) << bunch_means(i) << std::endl;
+    }
+    logger << "bunch stds" << std::endl;
+    for (int i=0; i<6; ++i) {
+        logger << i << ":  " << std::setprecision(15) << bunch_stds(i) << std::endl;
+    }
+}
+
+int run()
+{
+    Logger screen(0, LoggerV::INFO_TURN);
+    //Logger screen(0, LoggerV::DEBUG);
+
+    Lattice lattice = get_lattice();
+    screen << "Read lattice, length: " << lattice.get_length() << ", " << lattice.get_elements().size() << " elements" << std::endl;
+
+    Four_momentum four_momentum(pconstants::mp);
+    four_momentum.set_momentum(3.0);
+    Reference_particle refpart(1, four_momentum);
+    lattice.set_reference_particle(refpart);
+
+    // space charge
+    Space_charge_3d_open_hockney_options sc_ops(32, 32, 64);
+    sc_ops.comm_group_size = 1;
+
+    // stepper
+    Split_operator_stepper_elements stepper(sc_ops, 4);
+
+    // Propagator
+    Propagator propagator(lattice, stepper);
+    // propagator.print_steps(screen);
+
+    // bunch simulator
+    auto sim = Bunch_simulator::create_single_bunch_simulator(
+            //lattice.get_reference_particle(), 524288, 1e13,
+            lattice.get_reference_particle(), 65536, 1e13,
+                                                              
+            Commxx() );
+
+    karray1d means("means", 6);
+    for (int i=0; i<6; ++i) means(i) = 0.0;
+
+    static const double covariance_matrix[6][6] = 
+        {{3.0509743977035345e-05, 2.2014134466660509e-06, 0, 0, 0, 0},
+         {2.2014134466660509e-06, 1.9161816525115869e-07, 0, 0, 0, 0},
+         {0, 0, 7.5506914064526925e-06, -6.6846812465678249e-07, 0, 0},
+         {0, 0, -6.6846812465678249e-07, 1.9161816525115867e-07, 0, 0},
+         {0, 0, 0, 0, 0.00016427607645871527, 0},
+         {0, 0, 0, 0, 0, 1e-08}};
+
+    
+    karray2d_row covariances("covariances", 6, 6);
+    for (int i=0; i<6; ++i)
+        for (int j=0; j<6; ++j)
+            covariances(i, j) = covariance_matrix[i][j];
+
+    Random_distribution dist(1234567, Commxx());
+
+    auto & bunch = sim.get_bunch();
+    populate_6d(dist, bunch, means, covariances);
+
+    screen << "Statistics before propagation" << std::endl;
+        
+    bunch.checkout_particles();
+    print_bunch_statistics(bunch, screen);
+
+    // diagnostics
+    Diagnostics_bulk_track diag_bulk_track("tracks.h5", 100, 0);
+    sim.reg_diag_per_turn(diag_bulk_track);
+
+    Diagnostics_particles diag_particles("particles.h5", 1000);
+    sim.reg_diag_per_turn(diag_particles);
+
+    Diagnostics_full2 diag_full2("diag.h5");
+    sim.reg_diag_per_turn(diag_full2);
+
+    screen << "Statistics before propagation" << std::endl;
+        
+    bunch.checkout_particles();
+    print_bunch_statistics(bunch, screen);
+    // propagate
+    propagator.propagate(sim, screen, 10);
+
+    bunch.checkout_particles();
+    screen << "Statistics after propagate" << std::endl;
+    print_bunch_statistics(bunch, screen);
+    
+    return 0;
+}
+
+int main(int argc, char ** argv)
+{
+    MPI_Init(&argc, &argv);
+    Kokkos::initialize(argc, argv);
+
+    //layout_test();
+    run();
+
+    Kokkos::finalize();
+    MPI_Finalize();
+    return 0;
+}
