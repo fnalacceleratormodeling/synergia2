123953e5a7bd2f579e99c8f199cb443713d497c8
Marc Paterno <paterno@fnal.gov>
2021-06-15 00:41:45 +0000

    Minor cleanup in MPI support, handle oversubscribing

--- CMake/MPITest.cmake
+++ CMake/MPITest.cmake
@@ -1,6 +1,6 @@
 macro(add_mpi_test tname np)
 
     add_test(NAME ${tname}_${np}
-        COMMAND ${MPIEXEC} ${MPIEXEC_NUMPROC_FLAG} ${np} $<TARGET_FILE:${tname}>)
-
+	COMMAND ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${np}
+	${MPIEXEC_PREFLAGS} $<TARGET_FILE:${tname}> ${MPIEXEC_POSTFLAGS})
 endmacro()
