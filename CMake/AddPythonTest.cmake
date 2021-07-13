83a3c9092cd069249af0f17ccb702fc99c76fa11
Qiming Lu <qmlu79@gmail.com>
2021-07-13 14:59:37 -0500

    setup structure and tools for python tests

---
+++ CMake/AddPythonTest.cmake
@@ -0,0 +1,14 @@
+macro(add_py_test tname)
+
+    add_custom_target(pytest_${tname})
+
+    add_test(NAME ${tname}
+        COMMAND ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} 1
+          ${MPIEXEC_PREFLAGS} ${PYTHON_EXECUTABLE} -m pytest 
+          ${CMAKE_CURRENT_BINARY_DIR}/${tname} ${MPIEXEC_POSTFLAGS}
+        WORKING_DIRECTORY ${SYNERGIA2_BINARY_DIR}/src
+        )
+
+    copy_file(${tname} pytest_${tname})
+
+endmacro()
---
+++ src/synergia/foundation/tests/test_simple.py
@@ -0,0 +1,6 @@
+import synergia
+
+def test_simple():
+    assert True
+
+
