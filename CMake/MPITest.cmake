c9a812f9769e0deddcd84332868a6970fef66802
Qiming Lu <qmlu79@gmail.com>
2021-07-26 15:48:20 -0500

    set the OMP environment variables (OMP_NUM_THREADS, OMP_PROC_BIND, and OMP_PLACES) when running tests if the openmp backend is turned on

--- CMake/MPITest.cmake
+++ CMake/MPITest.cmake
@@ -1,6 +1,18 @@
 macro(add_mpi_test tname np)
 
-    add_test(NAME ${tname}_${np}
-	COMMAND ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${np}
-	${MPIEXEC_PREFLAGS} $<TARGET_FILE:${tname}> ${MPIEXEC_POSTFLAGS})
+    if (${Kokkos_ENABLE_OPENMP})
+        add_test(NAME ${tname}_${np}
+            COMMAND ${CMAKE_COMMAND} -E env 
+                OMP_NUM_THREADS=1
+                OMP_PROC_BIND=spread
+                OMP_PLACES=threads
+                ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${np}
+                ${MPIEXEC_PREFLAGS} $<TARGET_FILE:${tname}> ${MPIEXEC_POSTFLAGS})
+    else()
+        add_test(NAME ${tname}_${np}
+            COMMAND ${CMAKE_COMMAND} -E env
+                ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${np}
+                ${MPIEXEC_PREFLAGS} $<TARGET_FILE:${tname}> ${MPIEXEC_POSTFLAGS})
+    endif()
+
 endmacro()
