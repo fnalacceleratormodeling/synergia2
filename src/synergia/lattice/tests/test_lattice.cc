e1a1be010c2b02186490ccb7c902bd91bbb87a0c
Qiming Lu <qmlu79@gmail.com>
2021-09-01 15:16:07 -0500

    test lattice C++ tests

--- src/synergia/lattice/tests/test_lattice.cc
+++ src/synergia/lattice/tests/test_lattice.cc
@@ -1,12 +1,12 @@
-#define BOOST_TEST_MAIN
-#include <boost/test/unit_test.hpp>
+#include "synergia/utils/catch.hpp"
+
 #include "synergia/lattice/lattice.h"
 #include "synergia/lattice/madx_reader.h"
 #include "synergia/foundation/math_constants.h"
-#include "synergia/utils/serialization.h"
-#include "synergia/utils/serialization_files.h"
-#include "synergia/utils/boost_test_mpi_fixture.h"
-BOOST_GLOBAL_FIXTURE(MPI_fixture);
+
+#include "synergia/utils/cereal_files.h"
+
+#include "synergia/utils/boost_test_to_catch2_macros.h"
 
 const std::string name("foo");
 const int charge = -1;
@@ -30,15 +30,6 @@ BOOST_AUTO_TEST_CASE(set_reference_particle)
     Lattice lattice(name);
     Reference_particle reference_particle(charge, mass, total_energy);
     lattice.set_reference_particle(reference_particle);
-}
-
-BOOST_AUTO_TEST_CASE(has_reference_particle)
-{
-    Lattice lattice(name);
-    BOOST_CHECK_EQUAL(lattice.has_reference_particle(), false);
-    Reference_particle reference_particle(charge, mass, total_energy);
-    lattice.set_reference_particle(reference_particle);
-    BOOST_CHECK_EQUAL(lattice.has_reference_particle(), true);
 }
 
 BOOST_AUTO_TEST_CASE(get_reference_particle)
@@ -68,28 +59,29 @@ BOOST_AUTO_TEST_CASE(append_fodo)
     lattice.append(d);
     lattice.append(o);
 
-    Lattice_elements::const_iterator it = (lattice.get_elements()).begin();
+    auto it = (lattice.get_elements()).begin();
     BOOST_CHECK(it != lattice.get_elements().end());
-    BOOST_CHECK_EQUAL((*it)->get_name(), "f");
-    BOOST_CHECK((*it)->get_type() == "quadrupole");
-    BOOST_CHECK_CLOSE((*it)->get_double_attribute("l"), quad_length, tolerance);
+    BOOST_CHECK_EQUAL(it->get_name(), "f");
+    BOOST_CHECK(it->get_type_name() == "quadrupole");
+    BOOST_CHECK_CLOSE(it->get_double_attribute("l"), quad_length, tolerance);
     ++it;
 
-    BOOST_CHECK((*it)->get_name() == "o");
-    BOOST_CHECK((*it)->get_type() == "drift");
-    BOOST_CHECK_CLOSE((*it)->get_double_attribute("l"), drift_length, tolerance);
+    BOOST_CHECK(it->get_name() == "o");
+    BOOST_CHECK(it->get_type_name() == "drift");
+    BOOST_CHECK_CLOSE(it->get_double_attribute("l"), drift_length, tolerance);
     ++it;
 
-    BOOST_CHECK((*it)->get_name() == "d");
-    BOOST_CHECK((*it)->get_type() == "quadrupole");
-    BOOST_CHECK_CLOSE((*it)->get_double_attribute("l"), quad_length, tolerance);
+    BOOST_CHECK(it->get_name() == "d");
+    BOOST_CHECK(it->get_type_name() == "quadrupole");
+    BOOST_CHECK_CLOSE(it->get_double_attribute("l"), quad_length, tolerance);
     ++it;
 
-    BOOST_CHECK((*it)->get_name() == "o");
-    BOOST_CHECK((*it)->get_type() == "drift");
-    BOOST_CHECK_CLOSE((*it)->get_double_attribute("l"), drift_length, tolerance);
+    BOOST_CHECK(it->get_name() == "o");
+    BOOST_CHECK(it->get_type_name() == "drift");
+    BOOST_CHECK_CLOSE(it->get_double_attribute("l"), drift_length, tolerance);
 }
 
+#if 0
 BOOST_AUTO_TEST_CASE(derive_internal_attributes)
 {
     Lattice_element b("rbend", "b");
@@ -106,14 +98,15 @@ BOOST_AUTO_TEST_CASE(derive_internal_attributes)
             * std::sin(bend_angle / 2));
     BOOST_CHECK_CLOSE(lattice.get_length(), arc_length, tolerance);
 }
+#endif
 
 BOOST_AUTO_TEST_CASE(get_length)
 {
-    Lattice_element f("f", "quadrupole");
+    Lattice_element f("quadrupole", "f");
     f.set_double_attribute("l", quad_length);
-    Lattice_element o("o", "drift");
+    Lattice_element o("drift", "o");
     o.set_double_attribute("l", drift_length);
-    Lattice_element d("d", "quadrupole");
+    Lattice_element d("quadrupole", "d");
     d.set_double_attribute("l", quad_length);
 
     Lattice lattice(name);
@@ -127,11 +120,11 @@ BOOST_AUTO_TEST_CASE(get_length)
 
 BOOST_AUTO_TEST_CASE(get_total_angle1)
 {
-    Lattice_element f("f", "quadrupole");
+    Lattice_element f("quadrupole", "f");
     f.set_double_attribute("l", quad_length);
-    Lattice_element o("o", "drift");
+    Lattice_element o("drift", "o");
     o.set_double_attribute("l", drift_length);
-    Lattice_element d("d", "quadrupole");
+    Lattice_element d("quadrupole", "d");
     d.set_double_attribute("l", quad_length);
 
     Lattice lattice(name);
@@ -149,14 +142,14 @@ BOOST_AUTO_TEST_CASE(get_total_angle2)
     const int n_cells = 8;
     double bend_angle = 2 * pi / (2 * n_cells);
 
-    Lattice_element f("f", "quadrupole");
+    Lattice_element f("quadrupole", "f");
     f.set_double_attribute("l", quad_length);
-    Lattice_element o("o", "drift");
+    Lattice_element o("drift", "o");
     o.set_double_attribute("l", drift_length);
-    Lattice_element b("b", "sbend");
+    Lattice_element b("sbend", "b");
     b.set_double_attribute("l", bend_length);
     b.set_double_attribute("angle", bend_angle);
-    Lattice_element d("d", "quadrupole");
+    Lattice_element d("quadrupole", "d");
     d.set_double_attribute("l", quad_length);
 
     Lattice lattice(name);
@@ -176,19 +169,19 @@ BOOST_AUTO_TEST_CASE(get_total_angle2)
 
 BOOST_AUTO_TEST_CASE(lattice_from_lattice_element)
 {
-    Lattice_element f("footype", "foo");
+    Lattice_element f("drift", "foo");
     Lattice lattice(name);
     lattice.append(f);
-    for(Lattice_elements::const_iterator it = lattice.get_elements().begin();
+    for(auto it = lattice.get_elements().begin();
         it != lattice.get_elements().end(); ++it) {
-        BOOST_CHECK((*it)->has_lattice());
-        BOOST_CHECK_EQUAL(&((*it)->get_lattice()), &lattice);
+        BOOST_CHECK(it->has_lattice());
+        BOOST_CHECK_EQUAL(&(it->get_lattice()), &lattice);
     }
 }
 
 BOOST_AUTO_TEST_CASE(copy_lattice)
 {
-    Lattice_element f("footype", "foo");
+    Lattice_element f("drift", "foo");
     const double foo_length = 1.0;
     f.set_double_attribute("l", foo_length);
     Lattice lattice(name);
@@ -200,29 +193,28 @@ BOOST_AUTO_TEST_CASE(copy_lattice)
 
     BOOST_CHECK_EQUAL(lattice.get_elements().size(),
             copied_lattice.get_elements().size());
-    BOOST_CHECK_EQUAL((*lattice.get_elements().begin())->get_name(),
-            (*copied_lattice.get_elements().begin())->get_name());
+    BOOST_CHECK_EQUAL(lattice.get_elements().begin()->get_name(),
+            copied_lattice.get_elements().begin()->get_name());
 
-    BOOST_CHECK_CLOSE((*lattice.get_elements().begin())->get_length(),
+    BOOST_CHECK_CLOSE(lattice.get_elements().begin()->get_length(),
             foo_length, tolerance);
-    BOOST_CHECK_CLOSE((*copied_lattice.get_elements().begin())->get_length(),
+    BOOST_CHECK_CLOSE(copied_lattice.get_elements().begin()->get_length(),
             foo_length, tolerance);
 
-    for(Lattice_elements::const_iterator it = copied_lattice.get_elements().begin();
+    for(auto it = copied_lattice.get_elements().begin();
         it != copied_lattice.get_elements().end(); ++it) {
-        BOOST_CHECK((*it)->has_lattice());
-        BOOST_CHECK_EQUAL(&((*it)->get_lattice()), &copied_lattice);
+        BOOST_CHECK(it->has_lattice());
+        BOOST_CHECK_EQUAL(&(it->get_lattice()), &copied_lattice);
     }
 
     const double new_length = 2.0;
-    (*copied_lattice.get_elements().begin())->set_double_attribute("l", new_length);
+    copied_lattice.get_elements().begin()->set_double_attribute("l", new_length);
 
-    BOOST_CHECK_CLOSE((*lattice.get_elements().begin())->get_length(),
+    BOOST_CHECK_CLOSE(lattice.get_elements().begin()->get_length(),
             foo_length, tolerance);
-    BOOST_CHECK_CLOSE((*copied_lattice.get_elements().begin())->get_length(),
+    BOOST_CHECK_CLOSE(copied_lattice.get_elements().begin()->get_length(),
             new_length, tolerance);
 
-    BOOST_CHECK_EQUAL(copied_lattice.has_reference_particle(), true);
     const double new_energy = 2*total_energy;
     Reference_particle new_reference_particle(charge, mass, new_energy);
     copied_lattice.set_reference_particle(new_reference_particle);
@@ -237,78 +229,44 @@ BOOST_AUTO_TEST_CASE(copy_lattice2)
     Lattice lattice(name);
     Lattice copied_lattice(lattice);
 
-    BOOST_CHECK_EQUAL(copied_lattice.has_reference_particle(), false);
     Reference_particle reference_particle(charge, mass, total_energy);
     copied_lattice.set_reference_particle(reference_particle);
 }
 
 BOOST_AUTO_TEST_CASE(copy_lattice_from_lattice_sptr)
 {
-    Lattice_element f("footype", "foo");
+    Lattice_element f("drift", "foo");
     const double foo_length = 1.0;
     f.set_double_attribute("l", foo_length);
-    Lattice_sptr lattice_sptr(new Lattice(name));
-    lattice_sptr->append(f);
+
+    Lattice lattice(name);
+    lattice.append(f);
+
     Reference_particle reference_particle(charge, mass, total_energy);
-    lattice_sptr->set_reference_particle(reference_particle);
+    lattice.set_reference_particle(reference_particle);
 
-    Lattice_sptr copied_lattice_sptr(new Lattice(*lattice_sptr));
+    Lattice copied_lattice = lattice;
 
-    BOOST_CHECK_EQUAL(lattice_sptr->get_elements().size(),
-            copied_lattice_sptr->get_elements().size());
-    BOOST_CHECK_EQUAL((*lattice_sptr->get_elements().begin())->get_name(),
-            (*copied_lattice_sptr->get_elements().begin())->get_name());
+    BOOST_CHECK_EQUAL(lattice.get_elements().size(),
+            copied_lattice.get_elements().size());
+    BOOST_CHECK_EQUAL(lattice.get_elements().begin()->get_name(),
+            copied_lattice.get_elements().begin()->get_name());
 
-    BOOST_CHECK_CLOSE((*lattice_sptr->get_elements().begin())->get_length(),
+    BOOST_CHECK_CLOSE(lattice.get_elements().begin()->get_length(),
             foo_length, tolerance);
-    BOOST_CHECK_CLOSE((*copied_lattice_sptr->get_elements().begin())->get_length(),
+    BOOST_CHECK_CLOSE(copied_lattice.get_elements().begin()->get_length(),
             foo_length, tolerance);
 
-    for(Lattice_elements::const_iterator it = copied_lattice_sptr->get_elements().begin();
-        it != copied_lattice_sptr->get_elements().end(); ++it) {
-        BOOST_CHECK((*it)->has_lattice());
-        BOOST_CHECK_EQUAL(&((*it)->get_lattice()), copied_lattice_sptr.get());
+    for(auto it = copied_lattice.get_elements().begin();
+        it != copied_lattice.get_elements().end(); ++it) {
+        BOOST_CHECK(it->has_lattice());
+        BOOST_CHECK_EQUAL(&(it->get_lattice()), &copied_lattice);
     }
-}
-
-BOOST_AUTO_TEST_CASE(test_lattice_has_element_adaptor_map)
-{
-#if 0
-    Lattice_sptr lattice_sptr(MadX_reader().get_lattice_sptr("fodo", "lattices/fodo.madx"));
-#endif
-    Lattice_element f("footype", "foo");
-    const double foo_length = 1.0;
-    f.set_double_attribute("l", foo_length);
-    Lattice_sptr lattice_sptr(new Lattice(name));
-    lattice_sptr->append(f);
-    Reference_particle reference_particle(charge, mass, total_energy);
-    lattice_sptr->set_reference_particle(reference_particle);
-    std::cout << "egs: element_adaptor_map label: " << lattice_sptr->get_element_adaptor_map_sptr()->get_label() << std::endl;
-    BOOST_CHECK(lattice_sptr->get_element_adaptor_map_sptr());
-}
-
-BOOST_AUTO_TEST_CASE(test_copied_lattice_has_element_adaptor_map)
-{
-#if 0
-    Lattice_sptr orig_lattice_sptr(MadX_reader().get_lattice_sptr("fodo", "lattices/fodo.madx"));
-#else
-    Lattice_element f("footype", "foo");
-    const double foo_length = 1.0;
-    f.set_double_attribute("l", foo_length);
-    Lattice_sptr orig_lattice_sptr(new Lattice(name));
-    orig_lattice_sptr->append(f);
-    Reference_particle reference_particle(charge, mass, total_energy);
-    orig_lattice_sptr->set_reference_particle(reference_particle);
-#endif
-    std::cout << "egs: element_adaptor_map label: " << orig_lattice_sptr->get_element_adaptor_map_sptr()->get_label() << std::endl;
-    BOOST_CHECK(orig_lattice_sptr->get_element_adaptor_map_sptr());
-    Lattice_sptr lattice_sptr(new Lattice(*orig_lattice_sptr));
-    std::cout << "egs: element_adaptor_map label: " << lattice_sptr->get_element_adaptor_map_sptr()->get_label() << std::endl;
-    BOOST_CHECK(lattice_sptr->get_element_adaptor_map_sptr());
 }
 
 BOOST_AUTO_TEST_CASE(test_lsexpr)
 {
+#if 0
     Lattice_element f("quadrupole", "f");
     f.set_double_attribute("l", quad_length);
     Lattice_element o("drift", "o");
@@ -327,27 +285,27 @@ BOOST_AUTO_TEST_CASE(test_lsexpr)
     Lsexpr lsexpr(lattice.as_lsexpr());
     Lattice loaded(lsexpr);
 
-    BOOST_CHECK(loaded.has_reference_particle());
-    Lattice_elements::const_iterator it = (loaded.get_elements()).begin();
+    auto it = (loaded.get_elements()).begin();
     BOOST_CHECK(it != loaded.get_elements().end());
-    BOOST_CHECK_EQUAL((*it)->get_name(), "f");
-    BOOST_CHECK((*it)->get_type() == "quadrupole");
-    BOOST_CHECK_CLOSE((*it)->get_double_attribute("l"), quad_length, tolerance);
+    BOOST_CHECK_EQUAL(it->get_name(), "f");
+    BOOST_CHECK(it->get_type_name() == "quadrupole");
+    BOOST_CHECK_CLOSE(it->get_double_attribute("l"), quad_length, tolerance);
     ++it;
 
-    BOOST_CHECK((*it)->get_name() == "o");
-    BOOST_CHECK((*it)->get_type() == "drift");
-    BOOST_CHECK_CLOSE((*it)->get_double_attribute("l"), drift_length, tolerance);
+    BOOST_CHECK(it->get_name() == "o");
+    BOOST_CHECK(it->get_type_name() == "drift");
+    BOOST_CHECK_CLOSE(it->get_double_attribute("l"), drift_length, tolerance);
     ++it;
 
-    BOOST_CHECK((*it)->get_name() == "d");
-    BOOST_CHECK((*it)->get_type() == "quadrupole");
-    BOOST_CHECK_CLOSE((*it)->get_double_attribute("l"), quad_length, tolerance);
+    BOOST_CHECK(it->get_name() == "d");
+    BOOST_CHECK(it->get_type_name() == "quadrupole");
+    BOOST_CHECK_CLOSE(it->get_double_attribute("l"), quad_length, tolerance);
     ++it;
 
-    BOOST_CHECK((*it)->get_name() == "o");
-    BOOST_CHECK((*it)->get_type() == "drift");
-    BOOST_CHECK_CLOSE((*it)->get_double_attribute("l"), drift_length, tolerance);
+    BOOST_CHECK(it->get_name() == "o");
+    BOOST_CHECK(it->get_type_name() == "drift");
+    BOOST_CHECK_CLOSE(it->get_double_attribute("l"), drift_length, tolerance);
+#endif
 }
 
 BOOST_AUTO_TEST_CASE(test_serialize1)
@@ -364,32 +322,31 @@ BOOST_AUTO_TEST_CASE(test_serialize1)
     lattice.append(o);
     lattice.append(d);
     lattice.append(o);
-    xml_save<Lattice > (lattice, "lattice1.xml");
+    json_save<Lattice>(lattice, "lattice1.json");
 
     Lattice loaded;
-    xml_load<Lattice > (loaded, "lattice1.xml");
+    json_load<Lattice>(loaded, "lattice1.json");
 
-    BOOST_CHECK(!loaded.has_reference_particle());
-    Lattice_elements::const_iterator it = (loaded.get_elements()).begin();
+    auto it = loaded.get_elements().begin();
     BOOST_CHECK(it != loaded.get_elements().end());
-    BOOST_CHECK_EQUAL((*it)->get_name(), "f");
-    BOOST_CHECK((*it)->get_type() == "quadrupole");
-    BOOST_CHECK_CLOSE((*it)->get_double_attribute("l"), quad_length, tolerance);
+    BOOST_CHECK_EQUAL(it->get_name(), "f");
+    BOOST_CHECK(it->get_type_name() == "quadrupole");
+    BOOST_CHECK_CLOSE(it->get_double_attribute("l"), quad_length, tolerance);
     ++it;
 
-    BOOST_CHECK((*it)->get_name() == "o");
-    BOOST_CHECK((*it)->get_type() == "drift");
-    BOOST_CHECK_CLOSE((*it)->get_double_attribute("l"), drift_length, tolerance);
+    BOOST_CHECK(it->get_name() == "o");
+    BOOST_CHECK(it->get_type_name() == "drift");
+    BOOST_CHECK_CLOSE(it->get_double_attribute("l"), drift_length, tolerance);
     ++it;
 
-    BOOST_CHECK((*it)->get_name() == "d");
-    BOOST_CHECK((*it)->get_type() == "quadrupole");
-    BOOST_CHECK_CLOSE((*it)->get_double_attribute("l"), quad_length, tolerance);
+    BOOST_CHECK(it->get_name() == "d");
+    BOOST_CHECK(it->get_type_name() == "quadrupole");
+    BOOST_CHECK_CLOSE(it->get_double_attribute("l"), quad_length, tolerance);
     ++it;
 
-    BOOST_CHECK((*it)->get_name() == "o");
-    BOOST_CHECK((*it)->get_type() == "drift");
-    BOOST_CHECK_CLOSE((*it)->get_double_attribute("l"), drift_length, tolerance);
+    BOOST_CHECK(it->get_name() == "o");
+    BOOST_CHECK(it->get_type_name() == "drift");
+    BOOST_CHECK_CLOSE(it->get_double_attribute("l"), drift_length, tolerance);
 }
 
 BOOST_AUTO_TEST_CASE(test_serialize2)
@@ -397,11 +354,10 @@ BOOST_AUTO_TEST_CASE(test_serialize2)
     Lattice lattice(name);
     Reference_particle reference_particle(charge, mass, total_energy);
     lattice.set_reference_particle(reference_particle);
-    xml_save<Lattice > (lattice, "lattice2.xml");
+    json_save<Lattice>(lattice, "lattice2.json");
 
     Lattice loaded;
-    xml_load<Lattice > (loaded, "lattice2.xml");
+    json_load<Lattice>(loaded, "lattice2.json");
 
-    BOOST_CHECK(loaded.has_reference_particle());
     BOOST_CHECK(loaded.get_reference_particle().equal(reference_particle, tolerance));
 }
