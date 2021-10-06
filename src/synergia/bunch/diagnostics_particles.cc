57ed7df3cd4f6acbd40984b8d9cbe1b97b4aaa93
Qiming Lu <qmlu79@gmail.com>
2021-10-06 13:22:42 -0500

    added more quantities to the particles diagnostics

--- src/synergia/bunch/diagnostics_particles.cc
+++ src/synergia/bunch/diagnostics_particles.cc
@@ -14,10 +14,19 @@ Diagnostics_particles::Diagnostics_particles(
     , num_spec_part(num_spec_part), spec_offset(spec_offset)
 { }
 
-
 void Diagnostics_particles::do_write(Hdf5_file& file)
 { 
     assert(bunch_ptr != nullptr);
+    auto const& ref = bunch_ptr->get_reference_particle();
+
+    file.write("charge", ref.get_charge());
+    file.write("mass", ref.get_four_momentum().get_mass());
+
+    file.write("s", ref.get_s());
+    file.write("s_n", ref.get_s_n());
+    file.write("repetition", ref.get_repetition());
+    file.write("pz", ref.get_momentum());
+
     bunch_ptr->write_file(file, num_part, offset, num_spec_part, spec_offset); 
     bunch_ptr = nullptr;
 }
