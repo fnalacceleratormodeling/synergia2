fd515ec5adc5e3b9d0bc3d3c406bf50784a38914
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2022-03-16 14:51:17 -0500

    ensure portability of mpiexec arguments for both mpich and openmpi
    	modified:   CMake/MPITest.cmake
    	modified:   src/synergia/utils/tests/test_distributed_fft2d.cc

--- CMake/MPITest.cmake
+++ CMake/MPITest.cmake
@@ -6,7 +6,7 @@ macro(add_mpi_test tname np)
                 OMP_NUM_THREADS=1
                 OMP_PROC_BIND=spread
                 OMP_PLACES=cores
-                ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${np} --bind-to-core
+                ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${np}
                 ${MPIEXEC_PREFLAGS} $<TARGET_FILE:${tname}> ${MPIEXEC_POSTFLAGS})
     else()
         add_test(NAME ${tname}_${np}
