929ef62f7926211f289da6d40169dafd1c9da359
Marc Paterno <paterno@fnal.gov>
2022-03-19 15:26:02 -0500

    Add tests of factorial

--- src/synergia/foundation/tests/test_trigon.cc
+++ src/synergia/foundation/tests/test_trigon.cc
@@ -6,6 +6,26 @@
 
 template <unsigned int P> using Trig = Trigon<std::complex<double>, P, 1>;
 
+TEST_CASE("factorials")
+{
+  static_assert(factorial(0) == 1);
+  static_assert(factorial(1) == 1);
+  static_assert(factorial(2) == 2);
+  static_assert(factorial(3) == 6);
+  static_assert(factorial(4) == 24);
+  static_assert(factorial(5) == 120);
+  static_assert(factorial(6) == 720);
+  static_assert(factorial(7) == 5040);
+  static_assert(factorial(8) == 40320);
+  static_assert(factorial(9) == 362880);
+  static_assert(factorial(10) == 3628800);
+  static_assert(factorial(11) == 39916800);
+  static_assert(factorial(12) == 479001600);
+  // factorial(13) is too large to represent in a 32-bit integer, and so
+  // it returns 0.
+  static_assert(factorial(13) == 0);
+}
+
 TEST_CASE("Power 0")
 {
   using trig_t = Trig<0>;
