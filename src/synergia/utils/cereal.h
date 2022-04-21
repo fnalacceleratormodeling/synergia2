13863ff07331700f2bb936b03b92aa1b341396b5
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2022-04-21 14:00:29 -0500

    bump cereal/pybind11, move to cmake fetch content, allow external installations to be used
    	modified:   .gitmodules
    	modified:   CMake/AddPythonTest.cmake
    	modified:   CMake/FindConsistentPython.cmake
    	deleted:    CMake/FindMPI4PY.cmake
    	deleted:    CMake/FindNUMPY.cmake
    	modified:   CMakeLists.txt
    	modified:   src/synergia/foundation/CMakeLists.txt
    	modified:   src/synergia/foundation/tests/CMakeLists.txt
    	modified:   src/synergia/utils/CMakeLists.txt
    	deleted:    src/synergia/utils/cereal
    	modified:   src/synergia/utils/cereal.h
    	deleted:    src/synergia/utils/pybind11

--- src/synergia/utils/cereal.h
+++ src/synergia/utils/cereal.h
@@ -4,31 +4,13 @@
 #pragma clang diagnostic ignored "-Wc++11-extensions"
 
 #include <cereal/archives/binary.hpp>
-#include <cereal/archives/xml.hpp>
 #include <cereal/archives/json.hpp>
+#include <cereal/archives/xml.hpp>
 
-//#include <cereal/archive/xml_iarchive.hpp>
-//#include <cereal/archive/xml_oarchive.hpp>
-
-#include <cereal/types/map.hpp>
-#include <cereal/types/vector.hpp>
-#include <cereal/types/string.hpp>
 #include <cereal/types/complex.hpp>
+#include <cereal/types/map.hpp>
 #include <cereal/types/memory.hpp>
-
-#if 0
-#include <cereal/types/version.hpp>
-#include <cereal/types/base_object.hpp>
-#include <cereal/types/utility.hpp>
-#include <cereal/types/assume_abstract.hpp>
-#include <cereal/types/nvp.hpp>
-#include <cereal/types/complex.hpp>
-#include <cereal/types/list.hpp>
-#include <cereal/types/array.hpp>
+#include <cereal/types/string.hpp>
 #include <cereal/types/vector.hpp>
-#include <cereal/types/map.hpp>
-#include <cereal/types/shared_ptr.hpp>
-#include <cereal/types/export.hpp>
-#endif
 
 #endif /* CEREAL_H */
