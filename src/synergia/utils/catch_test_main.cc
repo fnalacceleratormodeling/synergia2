e249ea1696c95e452e6d5c98bd49e5a2940fb736
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2022-08-15 11:55:20 -0500

    Add point charge test for space-charge-3d-fd/rectangular

--- src/synergia/utils/catch_test_main.cc
+++ src/synergia/utils/catch_test_main.cc
@@ -4,15 +4,34 @@
 #include <Kokkos_Core.hpp>
 #include <mpi.h>
 
+#if defined BUILD_FD_SPACE_CHARGE_SOLVER
+#include <petscsys.h>
+static std::string help = "This is a synergia2 test program!\n\n";
+#endif
+
 int
 main(int argc, char* argv[])
 {
+
+#if defined BUILD_FD_SPACE_CHARGE_SOLVER
+  PetscErrorCode ierr;
+  ierr = PetscInitialize(&argc, &argv, (char*)0, help.c_str());
+  auto initargs =
+    Kokkos::InitializationSettings{}; /* use default constructor */
+  Kokkos::initialize(initargs);
+#else
   MPI_Init(&argc, &argv);
   Kokkos::initialize(argc, argv);
+#endif
 
   int result = Catch::Session().run(argc, argv);
 
   Kokkos::finalize();
+#if defined BUILD_FD_SPACE_CHARGE_SOLVER
+  ierr = PetscFinalize();
+#else
   MPI_Finalize();
+#endif
+
   return result;
 }
