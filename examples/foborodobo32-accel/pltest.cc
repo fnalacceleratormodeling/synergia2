5298e093141fdb11a455fb8535a0a0b8a24d124f
Sajid Ali <sasyed@fnal.gov>
2022-12-12 15:04:20 -0600

    temporarily run pltest.py a couple of times on ubuntu-clang/gcc

--- examples/foborodobo32-accel/pltest.cc
+++ examples/foborodobo32-accel/pltest.cc
@@ -30,7 +30,7 @@ get_lattice()
 
     lattice.set_all_string_attribute("extractor_type", "libff");
 
-    Reference_particle ref(1, pconstants::mp, pconstants::mp+0.8);
+    Reference_particle ref(1, pconstants::mp, pconstants::mp + 0.8);
 
     lattice.set_reference_particle(ref);
 
@@ -48,7 +48,7 @@ run()
     Logger simlog(0, LV::INFO_STEP);
 
     Lattice lattice(get_lattice());
-   
+
     // get the reference particle
     auto const& ref = lattice.get_reference_particle();
 
@@ -58,62 +58,81 @@ run()
     screen(LV::INFO) << "gamma: " << ref.get_gamma() << "\n";
     screen(LV::INFO) << "beta: " << ref.get_beta() << "\n";
 
-   // stepper
+    // stepper
     Split_operator_stepper_elements stepper(Dummy_CO_options(), 1);
 
     // Propagator
     Propagator propagator(lattice, stepper);
 
-    screen(LV::INFO) << "lattice.get_elements() first element orig a1 parameter: " << 
-        lattice.get_elements().front().get_double_attribute("a1") << std::endl;
+    screen(LV::INFO)
+        << "lattice.get_elements() first element orig a1 parameter: "
+        << lattice.get_elements().front().get_double_attribute("a1")
+        << std::endl;
     lattice.get_elements().front().set_double_attribute("a1", 9999.0);
-    screen(LV::INFO) << "lattice.get_elements() first element orig a1 parameter: " << 
-        lattice.get_elements().front().get_double_attribute("a1") << std::endl;
-    screen(LV::INFO) << "demonstrated that I can control lattice element attributes from lattice.get_elements()\n" << std::endl;
+    screen(LV::INFO)
+        << "lattice.get_elements() first element orig a1 parameter: "
+        << lattice.get_elements().front().get_double_attribute("a1")
+        << std::endl;
+    screen(LV::INFO) << "demonstrated that I can control lattice element "
+                        "attributes from lattice.get_elements()\n"
+                     << std::endl;
 
-
-    screen(LV::INFO) << 
-    "Initial a1 param propagator.get_lattice_elements().front(): " <<
-        propagator.get_lattice_elements().front().get_double_attribute("a1") <<
-        std::endl;
-    screen(LV::INFO) << "propagator.get_lattice_elements() returns a const reference to lattice elements that are different than the original lattice used in propagator.  You can't modify any elements through this reference.\n" << std::endl;
+    screen(LV::INFO)
+        << "Initial a1 param propagator.get_lattice_elements().front(): "
+        << propagator.get_lattice_elements().front().get_double_attribute("a1")
+        << std::endl;
+    screen(LV::INFO) << "propagator.get_lattice_elements() returns a const "
+                        "reference to lattice elements that are different than "
+                        "the original lattice used in propagator.  You can't "
+                        "modify any elements through this reference.\n"
+                     << std::endl;
 
     // Doesn't work because of const
-    // propagator.get_lattice_elements().front().set_double_attribute("a1", -888.0);
-    // screen(LV::INFO) << 
-    // "After mod a1 param propagator.get_lattice_elements().front()" <<
-    //     propagator.get_lattice_elements().front().get_double_attribute("a1") <<
-    //     std::endl;
+    // propagator.get_lattice_elements().front().set_double_attribute("a1",
+    // -888.0); screen(LV::INFO) << "After mod a1 param
+    // propagator.get_lattice_elements().front()" <<
+    //     propagator.get_lattice_elements().front().get_double_attribute("a1")
+    //     << std::endl;
 
-    Propagator::Lattice_element_slices les = propagator.get_lattice_element_slices();
+    Propagator::Lattice_element_slices les =
+        propagator.get_lattice_element_slices();
     auto& first_slice = *les.begin();
 
-    // By the way, Lattice_element_slice::get_lattice_element is a const& so can't
-    // modify element?
-    screen(LV::INFO) << "First slice:\n" << 
-        first_slice.get_lattice_element().get_name() << ": a1: " <<
-        first_slice.get_lattice_element().get_double_attribute("a1") <<
-        std::endl;
-    
-    // Doesn't work because of const    
+    // By the way, Lattice_element_slice::get_lattice_element is a const& so
+    // can't modify element?
+    screen(LV::INFO) << "First slice:\n"
+                     << first_slice.get_lattice_element().get_name() << ": a1: "
+                     << first_slice.get_lattice_element().get_double_attribute(
+                            "a1")
+                     << std::endl;
+
+    // Doesn't work because of const
     // first_slice.get_lattice_element().set_double_attribute("a1", -222.0);
 
     Lattice& l(propagator.get_lattice());
-    screen(LV::INFO) << "propagator.get_lattice().get_elements().front() initial a1 param l.get_elements().front(): " <<
-        propagator.get_lattice().get_elements().front().get_double_attribute("a1") << std::endl;
-    propagator.get_lattice().get_elements().front().set_double_attribute("a1", -555.0);
-    screen(LV::INFO) << "propagator.get_lattice().get_elements().front() after mod a1 param l.get_elements().front(): " <<
-        propagator.get_lattice().get_elements().front().get_double_attribute("a1") << std::endl;
+    screen(LV::INFO)
+        << "propagator.get_lattice().get_elements().front() initial a1 param "
+           "l.get_elements().front(): "
+        << propagator.get_lattice().get_elements().front().get_double_attribute(
+               "a1")
+        << std::endl;
+    propagator.get_lattice().get_elements().front().set_double_attribute(
+        "a1", -555.0);
+    screen(LV::INFO)
+        << "propagator.get_lattice().get_elements().front() after mod a1 param "
+           "l.get_elements().front(): "
+        << propagator.get_lattice().get_elements().front().get_double_attribute(
+               "a1")
+        << std::endl;
 
-    screen(LV::INFO) << "First slice after mod:\n" << 
-        first_slice.get_lattice_element().get_name() << ": a1: " <<
-        first_slice.get_lattice_element().get_double_attribute("a1") <<
-        std::endl;
-
+    screen(LV::INFO) << "First slice after mod:\n"
+                     << first_slice.get_lattice_element().get_name() << ": a1: "
+                     << first_slice.get_lattice_element().get_double_attribute(
+                            "a1")
+                     << std::endl;
 
     return;
 }
-
 
 int
 main(int argc, char** argv)
