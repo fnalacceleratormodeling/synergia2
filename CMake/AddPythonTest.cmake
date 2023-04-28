5dcf21e04097a91edcabac5f274fecc4c91c3014
Sajid Ali <sasyed@fnal.gov>
2023-04-28 12:56:36 -0500

    Add OpenMPI oversubscribe env vars to Python tests
    	modified:   CMake/AddPythonTest.cmake
    	modified:   src/synergia/foundation/tests/CMakeLists.txt
    	modified:   src/synergia/utils/tests/CMakeLists.txt

--- CMake/AddPythonTest.cmake
+++ CMake/AddPythonTest.cmake
@@ -5,9 +5,10 @@ macro(add_py_test tname)
   add_test(
     NAME ${tname}
     COMMAND
-      ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} 1 ${MPIEXEC_PREFLAGS}
-      ${Python_EXECUTABLE} -m pytest -vv ${CMAKE_CURRENT_BINARY_DIR}/${tname}
-      ${MPIEXEC_POSTFLAGS}
+      ${CMAKE_COMMAND} -E env OMPI_MCA_rmaps_base_oversubscribe=true
+      PRTE_MCA_rmaps_default_mapping_policy=:oversubscribe ${MPIEXEC_EXECUTABLE}
+      ${MPIEXEC_NUMPROC_FLAG} 1 ${MPIEXEC_PREFLAGS} ${Python_EXECUTABLE} -m
+      pytest -vv ${CMAKE_CURRENT_BINARY_DIR}/${tname} ${MPIEXEC_POSTFLAGS}
     WORKING_DIRECTORY ${SYNERGIA2_BINARY_DIR}/src)
 
   copy_file(${tname} pytest_${tname})
