958c03e90a09612efb52628140ccf02ea07e19a6
Eric G. Stern <egstern@fnal.gov>
2023-06-02 09:55:28 -0500

    Test for off-axis kicked orbit with zero and nonzero length kickers including that particles don't wobble longitudinally.

--- src/synergia/simulation/tests/test_kicked_orbit_timing2.py
+++ src/synergia/simulation/tests/test_kicked_orbit_timing2.py
@@ -14,9 +14,9 @@ def prop_fixture():
     # booster-like lattice
     booster_madx = """
 beam, particle=proton,energy=pmass+0.8;
-k1: vkicker, vkick=0.01;
-k2: vkicker, vkick=-0.02;
-k3: vkicker, vkick=0.01;
+k1: vkicker, vkick=0.01, l=1.0;
+k2: vkicker, vkick=-0.02, l=1.0;
+k3: vkicker, vkick=0.01, l=1.0;
 rfc: rfcavity, l=0.0, volt=0.2, harmon=1;
 channel: sequence, l=15, refer=entry;
 k1, at=0.0;
@@ -60,7 +60,49 @@ def test_proper_frequency(prop_fixture):
     lattice = prop_fixture.get_lattice()
     refpart = lattice.get_reference_particle()
     beta = refpart.get_beta()
-    pathlength = 2*np.sqrt(5**2 + 0.05**2) + 5
+
+    # To calculate the pathlength through the kicker, assume a circular
+    # path. The pathlength is the same for the outer kickers but not the
+    # inner kicker. Get the elements.
+    kick1elem = None
+    kick2elem = None
+    for elem in lattice.get_elements():
+        if elem.get_name() == "k1":
+            kick1elem = elem
+        elif elem.get_name() == "k2":
+            kick2elem = elem
+            break
+    if not kick1elem and not kick2elem:
+        raise RuntimeError("Did not find kicker elements")
+
+    # first and third kickers have asymmetric orbit
+    tantheta1 = kick1elem.get_double_attribute('vkick')
+    sintheta1 = tantheta1/np.sqrt(1+tantheta1**2)
+    costheta1 = 1/np.sqrt(1+tantheta1**2)
+    R1 = kick1elem.get_length()/sintheta1
+    pl1 = R1*np.arctan(tantheta1)
+    tanhalftheta1 = sintheta1/(1+costheta1)
+    # height1 is the distance from the bottom where the particle exits the kicker
+    height1 = kick1elem.get_length()*tanhalftheta1/2
+
+    # second kicker has symmetric orbit
+    tanhalftheta2 = -kick2elem.get_double_attribute('vkick')/2
+    sinhalftheta2 = tanhalftheta2/np.sqrt(1+tanhalftheta2**2)
+    coshalftheta2 = 1/np.sqrt(1+tanhalftheta2**2)
+    R2 = kick2elem.get_length()/(2*sinhalftheta2)
+    pl2 = R2*2*np.arctan(tanhalftheta2)
+    height2 = tanhalftheta2*kick2elem.get_length()/2
+
+    # piece together the pathlength
+    pathlength = pl1 # first kicker
+    pathlength = pathlength + 4/costheta1 # 4 is distance between kicker1 and kicker2
+    pathlength = pathlength*2 # same on both sides of kicker 2
+    pathlength = pathlength + 4
+
+    pathlength = pathlength + pl2 # path through kicker 2
+
+    zpathlength = 2*np.sqrt(5**2 + 0.05**2) + 5 # pathlength for 0 length kickers to check
+
     assert pathlength != lattice.get_length()
 
     # find the rf cavity
@@ -73,10 +115,13 @@ def test_proper_frequency(prop_fixture):
         raise RuntimeError('no cavity found')
     cav_freq = rfc.get_double_attribute('freq')*1.0e6
 
-    print('0 length kick calculated pathlength: ', pathlength)
+    print('zero length kicker pathlength: ', zpathlength)
+    print('geometric calculated pathlength: ', pathlength)
     print('freq calculated pathlength: ', beta*synergia.foundation.pconstants.c/cav_freq)
 
     assert beta*synergia.foundation.pconstants.c/lattice.get_length() != pytest.approx(cav_freq)
+
+    assert beta*synergia.foundation.pconstants.c/zpathlength != pytest.approx(cav_freq)
 
     assert beta*synergia.foundation.pconstants.c/pathlength == pytest.approx(cav_freq)
 
@@ -92,7 +137,6 @@ def test_proper_frequency(prop_fixture):
 
     assert lp[0, 4] == pytest.approx(0.0)
     assert lp[0, 5] == pytest.approx(0.0)
-
 
 def main():
     pf = prop_fixture()
