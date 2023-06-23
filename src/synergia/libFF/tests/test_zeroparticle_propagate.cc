da7ad32923230e20fd06fb3f32f878ce872f6353
Eric G. Stern <egstern@fnal.gov>
2023-06-23 10:35:43 -0500

    Add test zeroparticle_propagate which checks that a particle with 0 transverse coordinates propagates to something close to 0.

--- src/synergia/libFF/tests/test_zeroparticle_propagate.cc
+++ src/synergia/libFF/tests/test_zeroparticle_propagate.cc
@@ -13,230 +13,125 @@
 
 struct propagator_fixture
 {
-    std::string test_elem;
     Logger screen;
-    std::unique_ptr<Lattice> lattice;
-    std::unique_ptr<Propagator> propagator;
+    Lattice lattice;
+    Propagator propagator;
     std::unique_ptr<Bunch_simulator> sim;
 
-	static constexpr std::string lattice_head(R"foo(
-beam, particle=proton, energy=0.8+pmass;
-a: )foo");
-	static constexpr std::string lattice_tail(R"foo(
-
-channel: sequence, refer=entry, l=0;
-a, at=0.0;
-endsequence;
-)foo");
 	
     propagator_fixture(std::string const& elem_def)
-        : test_elem(elem_def)
+        : lattice(propagator_fixture::construct_test_lattice(elem_def))
         , screen(0, LoggerV::INFO_TURN)
-        , propagator()
-        , lattice()
+        , propagator(Propagator(lattice, Independent_stepper_elements(1)))
         , sim()
     {
-        // construct the lattice from the given element
-	    MadX_reader reader;
-	    reader.parse(lattice_head + test_elem + lattice_tail);
-	    lattice = reader.parse("channel");
   
-	    Reference_particle refpart = lattice->get_reference_particle();
-	    
-	    propagator = Propagator(*lattice, Independent_stepper_elements(1));
+	    const Reference_particle& refpart = lattice.get_reference_particle();	    
 
         sim = std::make_unique<Bunch_simulator>(
                 Bunch_simulator::create_single_bunch_simulator(
                     refpart, 8, 1e09, Commxx()));
+    }
 
+    Lattice
+    construct_test_lattice(const std::string elem_def)
+    {
+        std::string lattice_head(R"foo(
+beam, particle=proton, energy=0.8+pmass;
+a: )foo");
+        std::string lattice_tail(R"foo(
+;
+channel: sequence, refer=entry, l=0;
+a, at=0.0;
+endsequence;
+)foo");
+
+        // construct the lattice from the given element by sandwiching
+        // it between the lattice_head and lattice_tail
+	    MadX_reader reader;
+	    reader.parse(lattice_head + elem_def + lattice_tail);
+	    Lattice lattice(reader.get_lattice("channel"));
+
+        return lattice;
     }
 
     void propagate()
-    { *propagator.propagate(*sim, screen, 1); }
+    { propagator.propagate(*sim, screen, 1); }
 
     Bunch& bunch()
     { return sim->get_bunch(); }
 
     void print_lattice()
-    { Logger l(0, LoggerV::DEBUG); lattice->print(l); }
+    { Logger l(0, LoggerV::DEBUG); lattice.print(l); }
 };
 
-void propagate_libff(std::string const& seq)
+void propagate_test_elem(std::string const& elem_def)
 {
-    std::cout << "libff propagate " << seq << "\n";
+    std::cout << "propagate test element " << elem_def << "\n";
 
-    propagator_fixture pf(seq);
+    propagator_fixture pf(elem_def);
     pf.print_lattice();
     auto & b = pf.bunch();
 
     b.checkout_particles();
     auto parts = b.get_host_particles();
-    for (int i=0; i<6; ++i) parts(0, i) = 0.1;
+    for (int i=0; i<6; ++i) parts(0, i) = 0.0;
     b.checkin_particles();
 
     pf.propagate();
 
     b.checkout_particles();
     parts = b.get_host_particles();
-    std::cout << std::setprecision(16);
+    std::cout << std::scientific << std::setprecision(16);
     for (int i=0; i<6; ++i) std::cout << parts(0, i) << "\n";
     std::cout << "\n";
+
+    // For propagating particle at 0, all the transverse elements should
+    // remain at 0
+
+    // on Ryzen 7, sbends and CFsbends have a momentum of 1.45e-17
+    for(int i=0; i<4; ++i) {
+        CHECK (std::abs(parts(0, i)) < 2.0e-17);
+    }
 }
 
-
-TEST_CASE("sbend", "[libFF][Elements]")
+TEST_CASE("sbend")
 {
-    CHECK( true );
-    propagate_libff("seq_sbend");
+    propagate_test_elem("sbend, l=2, angle=pi/24");
 }
 
-TEST_CASE("drift", "[libFF][Elements]")
+TEST_CASE("CFsbend")
 {
-    CHECK( true );
-    propagate_libff("seq_drift");
+    propagate_test_elem("sbend, l=2, angle=pi/24");
 }
 
-TEST_CASE("quadrupole", "[libFF][Elements]")
+TEST_CASE("drift")
 {
-    CHECK( true );
-    propagate_libff("seq_quadrupole");
+    propagate_test_elem("drift, l=10");
 }
 
-TEST_CASE("sbend cf", "[libFF][Elements]")
+TEST_CASE("quadrupole")
 {
-    CHECK( true );
-    propagate_libff("seq_cfsbend");
+    propagate_test_elem("quadrupole, l=4, k1=1/10");
 }
 
-TEST_CASE("sbend cf 2", "[libFF][Elements]")
+TEST_CASE("sextupole")
 {
-    CHECK( true );
-    propagate_libff("seq_cfsbend2");
+    propagate_test_elem("sextupole, l=0.5, k2=0.25");
 }
 
-TEST_CASE("octupole", "[libFF][Elements]")
+TEST_CASE("octupole")
 {
-    CHECK( true );
-    propagate_libff("seq_octupole");
+    propagate_test_elem("octupole, l=0.5, k3=0.25");
 }
 
-TEST_CASE("octupole with tilt", "[libFF][Elements]")
+TEST_CASE("skew-quadrupole")
 {
-    CHECK( true );
-    propagate_libff("seq_octupole2");
+    propagate_test_elem("quadrupole, l=4, k1=1/10, tilt=pi/4");
 }
 
-TEST_CASE("sextupole", "[libFF][Elements]")
+TEST_CASE("rfcavity")
 {
-    CHECK( true );
-    propagate_libff("seq_sextupole");
+    propagate_test_elem("rfcavity, l=2, volt=0.05");
 }
 
-TEST_CASE("sextupole with tilt", "[libFF][Elements]")
-{
-    CHECK( true );
-    propagate_libff("seq_sextupole2");
-}
-
-TEST_CASE("rfcavity", "[libFF][Elements]")
-{
-    CHECK( true );
-    propagate_libff("seq_rfc");
-}
-
-TEST_CASE("thin multipole 1 -- mad8 format", "[libFF][Elements]")
-{
-    CHECK( true );
-    propagate_libff("seq_mp1");
-}
-
-TEST_CASE("thin multipole 2 -- mad8 format with tilts", "[libFF][Elements]")
-{
-    CHECK( true );
-    propagate_libff("seq_mp2");
-}
-
-TEST_CASE("thin multipole 3 -- madx format", "[libFF][Elements]")
-{
-    CHECK( true );
-    propagate_libff("seq_mp3");
-}
-
-
-TEST_CASE("thin multipole 4 -- madx format with tilts", "[libFF][Elements]")
-{
-    CHECK( true );
-    propagate_libff("seq_mp4");
-}
-
-TEST_CASE("hkicker", "[libFF][Elements]")
-{
-    CHECK( true );
-    propagate_libff("seq_hkicker");
-}
-
-TEST_CASE("vkicker", "[libFF][Elements]")
-{
-    CHECK( true );
-    propagate_libff("seq_vkicker");
-}
-
-TEST_CASE("kicker", "[libFF][Elements]")
-{
-    CHECK( true );
-    propagate_libff("seq_kicker");
-}
-
-TEST_CASE("long hkicker", "[libFF][Elements]")
-{
-    CHECK( true );
-    propagate_libff("seq_long_hkicker");
-}
-
-TEST_CASE("long hkicker simple", "[libFF][Elements]")
-{
-    CHECK( true );
-    propagate_libff("seq_long_hkicker_simple");
-}
-
-TEST_CASE("long vkicker", "[libFF][Elements]")
-{
-    CHECK( true );
-    propagate_libff("seq_long_vkicker");
-}
-
-TEST_CASE("long vkicker simple", "[libFF][Elements]")
-{
-    CHECK( true );
-    propagate_libff("seq_long_vkicker_simple");
-}
-
-TEST_CASE("long kicker", "[libFF][Elements]")
-{
-    CHECK( true );
-    propagate_libff("seq_long_kicker");
-}
-
-TEST_CASE("long kicker simple", "[libFF][Elements]")
-{
-    CHECK( true );
-    propagate_libff("seq_long_kicker_simple");
-}
-
-
-
-#if 0
-int main(int argc, char** argv)
-{
-    MPI_Init(&argc, &argv);
-    Kokkos::initialize(argc, argv);
-
-    int result = Catch::Session().run(argc, argv);
-
-    Kokkos::finalize();
-    MPI_Finalize();
-
-    return result;
-}
-
-#endif
