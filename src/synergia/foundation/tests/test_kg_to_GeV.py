be607aac1efdbb34589dc314fcd81d5a89d87c59
Eric G. Stern <egstern@fnal.gov>
2023-11-06 09:08:43 -0600

    the actual test

---
+++ src/synergia/foundation/tests/test_kg_to_GeV.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import synergia.foundation
+import pytest
+
+def test_kg_to_Gev():
+    kg_to_GeV_derived = 1.0e-9 * synergia.foundation.pconstants.c**2/synergia.foundation.pconstants.e
+    print('kg_to_GeV_derived: ', kg_to_GeV_derived)
+    print('pconstants.kg_to_GeV: ', synergia.foundation.pconstants.kg_to_GeV)
+    assert kg_to_GeV_derived == pytest.approx(synergia.foundation.pconstants.kg_to_GeV, rel=1.0e-15)
+
+if __name__ == "__main__":
+    test_kg_to_Gev()
