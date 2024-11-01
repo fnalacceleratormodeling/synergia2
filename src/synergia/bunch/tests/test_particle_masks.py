58495101e04bcfcdc52a8f6756c7b082b5ac3421
Eric G. Stern <egstern@fnal.gov>
2024-10-31 20:05:49 -0500

    pulled this file out of devel3

--- src/synergia/bunch/tests/test_particle_masks.py
+++ src/synergia/bunch/tests/test_particle_masks.py
@@ -13,7 +13,7 @@ macroparticles=16
 realparticles=5e10
 
 
-#@pytest.fixture
+@pytest.fixture
 def bunch_fixture():
     refpart = synergia.foundation.Reference_particle(1, mass, energy)
     bunch = synergia.bunch.Bunch(refpart, macroparticles, realparticles, synergia.utils.Commxx())
@@ -30,9 +30,51 @@ def test_mask_initial_state(bunch_fixture):
     masks = bunch_fixture.get_particle_masks_numpy()
     assert masks.sum() == macroparticles
 
+def test_mask_after_cuts():
+    refpart = synergia.foundation.Reference_particle(1, mass, energy)
+    sim = synergia.simulation.Bunch_simulator.create_single_bunch_simulator(refpart, macroparticles, realparticles, synergia.utils.Commxx())
+    bunch = sim.get_bunch()
+    lp = bunch.get_particles_numpy()
+    # populate between -2mm to +2mm
+
+    lp[:, 0] = 0.002 * (np.arange(macroparticles, dtype='d')/macroparticles - 0.5)
+    bunch.checkin_particles()
+
+    # create a lattice with an aperture
+    lattice = synergia.lattice.Lattice("foo")
+    m = synergia.lattice.Lattice_element("marker", "m")
+    m.set_string_attribute("aperture_type", "rectangular")
+    m.set_double_attribute("rectangular_aperture_width", 0.00030*2)
+    m.set_double_attribute("rectangular_aperture_height", 0.002)
+    lattice.append(m)
+    lattice.set_reference_particle(refpart)
+
+    stepper = synergia.simulation.Independent_stepper_elements(1)
+    propagator = synergia.simulation.Propagator(lattice, stepper)
+    
+    simlog = synergia.utils.parallel_utils.Logger(0, synergia.utils.parallel_utils.LoggerV.INFO_TURN, True)
+
+    propagator.propagate(sim, simlog, 1)
+    bunch.checkout_particles()
+    masks = bunch.get_particle_masks_numpy()
+    assert masks.sum() == 5
+
+# test access mask
+def run_test1():
+    bf = bunch_fixture()
+    test_mask_access(bf)
+
+# test initial state of mask
+def run_test2():
+    bf = bunch_fixture()
+    test_mask_initial_state(bf)
+
+# test masks after cutting some particles in aperture
+def run_test3():
+    test_mask_after_cuts()
+
 # for running from the command line, comment out pytest.fixture
 if __name__ == "__main__":
-    bf = bunch_fixture()
-
-    test_mask_access(bf)
-    test_mask_initial_state(bf)
+    run_test1()
+    run_test2()
+    run_test3()
