a7882e89e3012ae8f4812452f62e24fce1af1299
Eric G. Stern <egstern@fnal.gov>
2025-07-28 16:24:32 -0500

    The calculation of kicks with regular sized parameters for regular sbends results in really small vertical kicks, so switch to parallel sbends where it is really obvious. Add CMakeLists entry to new test.

--- src/synergia/simulation/tests/test_bend_edge_kill.py
+++ src/synergia/simulation/tests/test_bend_edge_kill.py
@@ -12,15 +12,18 @@ nturns = 1
 
 mp = synergia.foundation.pconstants.mp
 xoffs = 0.0005
-xpoffs = 0.0001 # need an xp offset to generate edge kick
+#xpoffs = 0.0001 # need an xp offset to generate edge kick
+xpoffs = 0.0
 yoffs = 0.0005
 
+##########################################################################################
+
 # prop_fixture is a propagator
-#@pytest.fixture
+@pytest.fixture
 def prop_fixture_edgekick():
     channel_madx = """
 beam, particle=proton,energy=0.8+pmass;
-b: sbend, angle=pi/3.0, l=3.0;
+b: sbend, angle=pi/30.0, l=3.0, e1=pi/60, e2=pi/60;
 channel: sequence, l=3.0, refer=entry;
 b, at=0.0;
 endsequence;
@@ -33,12 +36,14 @@ endsequence;
     propagator = synergia.simulation.Propagator(lattice, stepper)
 
     return propagator
+
+##########################################################################################
 
 #@pytest.fixture
 def prop_fixture_noentrykick():
     channel_madx = """
 beam, particle=proton,energy=0.8+pmass;
-b: sbend, angle=pi/3.0, l=3.0, kill_entry_kick=1.0;
+b: sbend, angle=pi/30.0, l=3.0, e1=pi/60.0, e2=pi/60.0, kill_entry_kick=1.0;
 channel: sequence, l=3.0, refer=entry;
 b, at=0.0;
 endsequence;
@@ -51,12 +56,14 @@ endsequence;
     propagator = synergia.simulation.Propagator(lattice, stepper)
 
     return propagator
+
+##########################################################################################
 
 #@pytest.fixture
 def prop_fixture_noexitkick():
     channel_madx = """
 beam, particle=proton,energy=0.8+pmass;
-b: sbend, angle=pi/3.0, l=3.0, kill_exit_kick=1.0;
+b: sbend, angle=pi/30.0, l=3.0, e1=pi/60.0, e2=pi/60.0, kill_exit_kick=1.0;
 channel: sequence, l=3.0, refer=entry;
 b, at=0.0;
 endsequence;
@@ -70,11 +77,13 @@ endsequence;
 
     return propagator
 
-#@pytest.fixture
+##########################################################################################
+
+@pytest.fixture
 def prop_fixture_nokick():
     channel_madx = """
 beam, particle=proton,energy=0.8+pmass;
-b: sbend, angle=pi/30.0, l=3.0, kill_entry_kick=1.0, kill_exit_kick=1.0;
+b: sbend, angle=pi/30.0, l=3.0, e1=pi/60.0, e2=pi/60.0, kill_entry_kick=1.0, kill_exit_kick=1.0;
 channel: sequence, l=3.0, refer=entry;
 b, at=0.0;
 endsequence;
@@ -87,6 +96,8 @@ endsequence;
     propagator = synergia.simulation.Propagator(lattice, stepper)
 
     return propagator
+
+##########################################################################################
 
 def create_simulator(ref_part):
     sim = synergia.simulation.Bunch_simulator.create_single_bunch_simulator(
@@ -107,36 +118,27 @@ def create_simulator(ref_part):
     bunch.checkin_particles()
     return sim
 
-def test_edge1(prop_fixture_edgekick, prop_fixture_nokick):
-    # print('lattice: ')
-    # print(prop_fixture.get_lattice())
+##########################################################################################
+
+def test_edge1(prop_fixture_edgekick):
 
     sim1 = create_simulator(prop_fixture_edgekick.get_lattice().get_reference_particle())
-    sim2 = create_simulator(prop_fixture_nokick.get_lattice().get_reference_particle())
     bunch1 = sim1.get_bunch()
-    bunch2 = sim2.get_bunch()
 
     bunch1.checkout_particles()
     lp1 = bunch1.get_particles_numpy()
     lpinit = lp1.copy()
-    for ix in range(5):
-        for iy in range(5):
-            print(f'{ix}, {iy}: {lpinit[5*ix+iy, 0]}, {lpinit[5*ix+iy, 2]}')
-
-    bunch2.checkout_particles()
-    lp2 = bunch2.get_particles_numpy()
-
+    # for ix in range(5):
+    #     for iy in range(5):
+    #         print(f'{ix}, {iy}: {lpinit[5*ix+iy, 0]}, {lpinit[5*ix+iy, 2]}')
 
     simlog = synergia.utils.parallel_utils.Logger(
         0, synergia.utils.parallel_utils.LoggerV.INFO_TURN, True
     )
     prop_fixture_edgekick.propagate(sim1, simlog, nturns)
-    prop_fixture_nokick.propagate(sim2, simlog, nturns)
-
+    
     bunch1.checkout_particles()
     lp1 = bunch1.get_particles_numpy()
-    bunch2.checkout_particles()
-    lp2 = bunch2.get_particles_numpy()
 
     # particles started at y = 0 should have no py with edge kicks
     # all the particles with y != 0 should have nonzero py with edge kicks
@@ -144,20 +146,49 @@ def test_edge1(prop_fixture_edgekick, prop_fixture_nokick):
     # y = 0 is iy = 2
     for ix in range(5):
         for iy in range(5):
-            print(f'{ix}, {iy} ({lpinit[5*ix+iy, 0]}, {lpinit[5*ix+iy, 2]}): lp1[{5*ix+iy}, 3]: ', lp1[5*ix+iy, 3])
+            # print(f'{ix}, {iy} ({lpinit[5*ix+iy, 0]}, {lpinit[5*ix+iy, 2]}): lp1[{5*ix+iy}, 3]: ', lp1[5*ix+iy, 3])
             if iy == 2:
-                assert lp1[5*ix+2, 3] == pytest.approx(0.0, abs=1.0e-12)
+                assert lp1[5*ix+2, 3] == pytest.approx(0.0, abs=1.0e-15)
             else:
-                pass
-                #assert abs(lp1[5*ix+iy, 3]) > 1.0e-12
+                assert abs(lp1[5*ix+iy, 3]) > 1.0e-12
 
+##########################################################################################
+
+def test_edge2(prop_fixture_nokick):
+
+    sim1 = create_simulator(prop_fixture_nokick.get_lattice().get_reference_particle())
+    bunch1 = sim1.get_bunch()
+
+    bunch1.checkout_particles()
+    lp1 = bunch1.get_particles_numpy()
+    lpinit = lp1.copy()
+    # for ix in range(5):
+    #     for iy in range(5):
+    #         print(f'{ix}, {iy}: {lpinit[5*ix+iy, 0]}, {lpinit[5*ix+iy, 2]}')
+
+    simlog = synergia.utils.parallel_utils.Logger(
+        0, synergia.utils.parallel_utils.LoggerV.INFO_TURN, True
+    )
+    prop_fixture_nokick.propagate(sim1, simlog, nturns)
+    
+    bunch1.checkout_particles()
+    lp1 = bunch1.get_particles_numpy()
+
+    # with no edge effects, there should be no particles with significant py
+    for ix in range(5):
+        for iy in range(5):
+            #print(f'{ix}, {iy} ({lpinit[5*ix+iy, 0]}, {lpinit[5*ix+iy, 2]}): lp1[{5*ix+iy}, 3]: ', lp1[5*ix+iy, 3])
+            assert abs(lp1[5*ix+iy, 3]) == pytest.approx(0.0, abs=1.0e-15)
+
+
+##########################################################################################
 ### it seems like the current implementation of bends does not perform edge kicks$%^&&*!!
 
 def main():
     pf1 = prop_fixture_edgekick()
     pf2 = prop_fixture_nokick()
-    test_edge1(pf1, pf2)
-
+    test_edge1(pf1)
+    test_edge2(pf2)
 
 if __name__ == "__main__":
     main()
